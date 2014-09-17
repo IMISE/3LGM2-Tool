994722232361f2229ccbae031489fea27fa24a75
astruebi <a.leipzig@gmx.de>
2014-09-17 10:36:16 +0200

    Expand FileFilter to filter Directories if necessary

--- axsutils/src/main/java/de/imise/util/io/FileNameExtensionFilterAndFileFilter.java
+++ axsutils/src/main/java/de/imise/util/io/FileNameExtensionFilterAndFileFilter.java
@@ -7,109 +7,121 @@ import java.io.FilenameFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
- * Ein {@link FileFilter} und {@link FilenameFilter}, der intern einen {@link FileNameExtensionFilter}
- * hält
+ * Ein {@link FileFilter} und {@link FilenameFilter}, der intern einen {@link FileNameExtensionFilter} hält
  * 
  * @author AXS
  * @create 06.07.2012
  */
 public class FileNameExtensionFilterAndFileFilter implements FileFilter {
 
-	/** Der eigentliche FileNameExtensionFilter */
-	private FileNameExtensionFilter fileNameExtensionFilter;
+    /** Der eigentliche FileNameExtensionFilter */
+    private FileNameExtensionFilter fileNameExtensionFilter;
 
-	/**
+    /** Wenn <code>true</code> werden auch Verzeichnisse zurück gegeben, sonst nur Dateien */
+    private final boolean acceptDirectories;
+
+    /**
      * Creates a {@code FileNameExtensionFilterAndFileFilter} with the specified
-     * description and file name extensions. The returned {@code
-     * FileNameExtensionFilterAndFileFilter} will accept all directories and any
+     * description and file name extensions. The returned {@code FileNameExtensionFilterAndFileFilter} will accept all directories and any
      * file with a file name extension contained in {@code extensions}.
-     *
-     * @param description textual description for the filter, may be
-     *                    {@code null}
+     * 
+     * @param description textual description for the filter, may be {@code null}
      * @param extensions the accepted file name extensions
      * @throws IllegalArgumentException if extensions is {@code null}, empty,
-     *         contains {@code null}, or contains an empty string
+     *             contains {@code null}, or contains an empty string
      * @see #accept
      * @see javax.swing.filechooser.FileNameExtensionFilter
      */
-    public FileNameExtensionFilterAndFileFilter(String description, String... extensions) {
-    	this(new FileNameExtensionFilter(description, extensions));
-	}
+    public FileNameExtensionFilterAndFileFilter(final String description, final String... extensions) {
+        this(new FileNameExtensionFilter(description, extensions));
+    }
 
     /**
      * @param fileNameExtensionFilter
      */
-    public FileNameExtensionFilterAndFileFilter(FileNameExtensionFilter fileNameExtensionFilter) {
-    	super();
-    	this.fileNameExtensionFilter = fileNameExtensionFilter;
-	}
-    
+    public FileNameExtensionFilterAndFileFilter(final FileNameExtensionFilter fileNameExtensionFilter) {
+        this(fileNameExtensionFilter, true);
+    }
+
     /**
-	 * @return the fileNameExtensionFilter
-	 */
-	public final FileNameExtensionFilter getFileNameExtensionFilter() {
-		return fileNameExtensionFilter;
-	}
+     * @param fileNameExtensionFilter
+     * @param acceptDirectories
+     */
+    public FileNameExtensionFilterAndFileFilter(final FileNameExtensionFilter fileNameExtensionFilter, final boolean acceptDirectories) {
+        super();
+        this.fileNameExtensionFilter = fileNameExtensionFilter;
+        this.acceptDirectories = acceptDirectories;
+    }
 
-	/**
-	 * @param fileNameExtensionFilter the fileNameExtensionFilter to set
-	 */
-	public final void setFileNameExtensionFilter(FileNameExtensionFilter fileNameExtensionFilter) {
-		this.fileNameExtensionFilter = fileNameExtensionFilter;
-	}
+    /**
+     * @return the fileNameExtensionFilter
+     */
+    public final FileNameExtensionFilter getFileNameExtensionFilter() {
+        return fileNameExtensionFilter;
+    }
 
-	/**
-	 * @return
-	 * @see FileNameExtensionFilter#hashCode();
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public int hashCode() {
-		return fileNameExtensionFilter.hashCode();
-	}
+    /**
+     * @param fileNameExtensionFilter the fileNameExtensionFilter to set
+     */
+    public final void setFileNameExtensionFilter(final FileNameExtensionFilter fileNameExtensionFilter) {
+        this.fileNameExtensionFilter = fileNameExtensionFilter;
+    }
 
-	/**
-	 * @param obj
-	 * @return
-	 * @see FileNameExtensionFilter#equals(Object);
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		return fileNameExtensionFilter.equals(obj);
-	}
+    /**
+     * @return
+     * @see FileNameExtensionFilter#hashCode();
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public int hashCode() {
+        return fileNameExtensionFilter.hashCode();
+    }
 
-	/**
-	 * @return
-	 * @see javax.swing.filechooser.FileNameExtensionFilter#getDescription()
-	 */
-	public String getDescription() {
-		return fileNameExtensionFilter.getDescription();
-	}
+    /**
+     * @param obj
+     * @return
+     * @see FileNameExtensionFilter#equals(Object);
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        return fileNameExtensionFilter.equals(obj);
+    }
 
-	/**
-	 * @return
-	 * @see javax.swing.filechooser.FileNameExtensionFilter#getExtensions()
-	 */
-	public String[] getExtensions() {
-		return fileNameExtensionFilter.getExtensions();
-	}
+    /**
+     * @return
+     * @see javax.swing.filechooser.FileNameExtensionFilter#getDescription()
+     */
+    public String getDescription() {
+        return fileNameExtensionFilter.getDescription();
+    }
 
-	/**
-	 * @return
-	 * @see javax.swing.filechooser.FileNameExtensionFilter#toString()
-	 */
-	@Override
-	public String toString() {
-		return fileNameExtensionFilter.toString();
-	}
+    /**
+     * @return
+     * @see javax.swing.filechooser.FileNameExtensionFilter#getExtensions()
+     */
+    public String[] getExtensions() {
+        return fileNameExtensionFilter.getExtensions();
+    }
 
-	/* (non-Javadoc)
-	 * @see java.io.FileFilter#accept(java.io.File)
-	 */
-	@Override
-	public boolean accept(File pathname) {
-		return fileNameExtensionFilter.accept(pathname);
-	}
+    /**
+     * @return
+     * @see javax.swing.filechooser.FileNameExtensionFilter#toString()
+     */
+    @Override
+    public String toString() {
+        return fileNameExtensionFilter.toString();
+    }
 
+    /*
+     * (non-Javadoc)
+     * @see java.io.FileFilter#accept(java.io.File)
+     */
+    @Override
+    public boolean accept(final File f) {
+        if (!acceptDirectories && f != null && f.isDirectory()) {
+            return false;
+        }
+        return fileNameExtensionFilter.accept(f);
+    }
 }
