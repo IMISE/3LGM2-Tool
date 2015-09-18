aaae68b3c3125174e6940574b73aed008b94a050
astruebi <a.leipzig@gmx.de>
2015-09-18 11:48:00 +0200

    LimitedSizeScrollTextPane: addDocumentListener and removeDocumentListener

--- axsutils/src/main/java/de/imise/util/swing/component/LimitedSizeScrollTextPane.java
+++ axsutils/src/main/java/de/imise/util/swing/component/LimitedSizeScrollTextPane.java
@@ -1,7 +1,6 @@
 /*
  * Created on 02.09.2003
- *
- * To change this generated comment go to 
+ * To change this generated comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
 package de.imise.util.swing.component;
@@ -14,161 +13,157 @@ import javax.swing.JComponent;
 import javax.swing.JScrollPane;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
+import javax.swing.event.DocumentListener;
 import javax.swing.text.JTextComponent;
 
 import de.imise.util.swing.component.text.ExtendedTextPane;
+
 /**
- * Wird maxLines >0 angegeben, dann wird das TextPane nie größer als diese Zeilenanzahl 
+ * Wird maxLines >0 angegeben, dann wird das TextPane nie größer als diese Zeilenanzahl
  * sondern es erscheint ein ScrollPane. Bei maxLines kleiner 1 vergrößert sich das Pane
- * beliebig, ohne dass ein ScrollPane erscheint. 
+ * beliebig, ohne dass ein ScrollPane erscheint.
  */
 public class LimitedSizeScrollTextPane extends JScrollPane {
 
-	/**
-	 * Maximale Zeilenanzahl, auf die sich die Komponente vergrößert, bevor das ScrollPane angezeigt wird.
-	 */
-	private int maxLines;
-	
-	/**
-	 * Das eigentliche Textpane
-	 */
-	private JTextComponent textPane;
+    /**
+     * Maximale Zeilenanzahl, auf die sich die Komponente vergrößert, bevor das ScrollPane angezeigt wird.
+     */
+    private final int maxLines;
 
-	/**
+    /**
+     * Das eigentliche Textpane
+     */
+    private final JTextComponent textPane;
+
+    /**
 	 * 
 	 */
-	public LimitedSizeScrollTextPane() {
-		this(-1);
-	}
+    public LimitedSizeScrollTextPane() {
+        this(-1);
+    }
 
-	/**
-	 * @param maxLines
-	 */
-	public LimitedSizeScrollTextPane(int maxLines) {
-		super();
-		textPane = new ExtendedTextPane();
-		this.setViewportView(textPane);
-		this.maxLines = maxLines;
-//		if (maxLines>1){
-			textPane.addCaretListener(new CaretListener() {
-				@Override
-				public void caretUpdate(CaretEvent e) {
-					setSize(new Dimension(getSize().width, getPreferredSize().height));
-					Component comp = getParent();
-					if (comp != null)
-						((JComponent) getParent()).revalidate();
-				}
-			});
-//		}
-	}
+    /**
+     * @param maxLines
+     */
+    public LimitedSizeScrollTextPane(final int maxLines) {
+        super();
+        textPane = new ExtendedTextPane();
+        setViewportView(textPane);
+        this.maxLines = maxLines;
+        //		if (maxLines>1){
+        textPane.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(final CaretEvent e) {
+                setSize(new Dimension(getSize().width, getPreferredSize().height));
+                Component comp = getParent();
+                if (comp != null) {
+                    ((JComponent) getParent()).revalidate();
+                }
+            }
+        });
+        //		}
+    }
 
-	public void setCaretPosition(int position) {
-		textPane.setCaretPosition(position);
-		textPane.setSize(getSize());
+    public void setCaretPosition(final int position) {
+        textPane.setCaretPosition(position);
+        textPane.setSize(getSize());
 
-	}
+    }
 
-	public int getCaretPosition() {
-		return textPane.getCaretPosition();
+    public int getCaretPosition() {
+        return textPane.getCaretPosition();
 
-	}
+    }
 
-	public String getText() {
-		return textPane.getText().replaceAll("\r", "");
-	}
+    public String getText() {
+        return textPane.getText().replaceAll("\r", "");
+    }
 
-	public void setText(String text) {
-		textPane.setText(text);
-	}
+    public void setText(final String text) {
+        textPane.setText(text);
+    }
 
-	/* (non-Javadoc)
-	 * @see javax.swing.JComponent#getMinimumSize()
-	 */
-	@Override
-	public Dimension getMinimumSize() {
-		return getPreferredSize();
-	}
+    @Override
+    public Dimension getMinimumSize() {
+        return getPreferredSize();
+    }
 
-	/* (non-Javadoc)
-	 * @see javax.swing.JComponent#getPreferredSize()
-	 */
-	@Override
-	public Dimension getPreferredSize() {
-		if (maxLines > 0) {
-			if (super.getPreferredSize().height > getMaximumSize().height) {
-				return getMaximumSize();
-			}
-		}
-		return super.getPreferredSize();
-	}
+    @Override
+    public Dimension getPreferredSize() {
+        if (maxLines > 0) {
+            if (super.getPreferredSize().height > getMaximumSize().height) {
+                return getMaximumSize();
+            }
+        }
+        return super.getPreferredSize();
+    }
 
-	/* (non-Javadoc)
-	 * @see javax.swing.JComponent#getMaximumSize()
-	 */
-	@Override
-	public Dimension getMaximumSize() {
-		if (maxLines > 0)
-			return new Dimension(textPane.getMaximumSize().width, (textPane.getFontMetrics(textPane.getFont()).getHeight() + 3) * maxLines + 5);
-		return super.getMaximumSize();
-	}
+    @Override
+    public Dimension getMaximumSize() {
+        if (maxLines > 0) {
+            return new Dimension(textPane.getMaximumSize().width, (textPane.getFontMetrics(textPane.getFont()).getHeight() + 3) * maxLines + 5);
+        }
+        return super.getMaximumSize();
+    }
 
-	/**
-	 * @return
-	 */
-	public JTextComponent getTextPane() {
-		return (JTextComponent) getViewport().getView();
-	}
+    /**
+     * @return
+     */
+    public JTextComponent getTextPane() {
+        return (JTextComponent) getViewport().getView();
+    }
 
+    /**
+     * @param b
+     */
+    public void setEditable(final boolean b) {
+        textPane.setEditable(b);
+    }
 
-	/**
-	 * @param b
-	 */
-	public void setEditable(boolean b) {
-		textPane.setEditable(b);
-	}
-
-	/**
+    /**
 	 * 
 	 */
-	public void selectAll() {
-		textPane.selectAll();
-	}
+    public void selectAll() {
+        textPane.selectAll();
+    }
 
-	/**
-	 * @return
-	 */
-	public String getSelectedText() {
-		return textPane.getSelectedText();
-	}
+    /**
+     * @return
+     */
+    public String getSelectedText() {
+        return textPane.getSelectedText();
+    }
 
-	/**
-	 * @return
-	 */
-	public int getSelectionStart() {
-		return textPane.getSelectionStart();
-	}
+    /**
+     * @return
+     */
+    public int getSelectionStart() {
+        return textPane.getSelectionStart();
+    }
 
-	/**
-	 * @return
-	 */
-	public int getSelectionEnd() {
-		return textPane.getSelectionEnd();
-	}
+    /**
+     * @return
+     */
+    public int getSelectionEnd() {
+        return textPane.getSelectionEnd();
+    }
 
-	/* (non-Javadoc)
-	 * @see java.awt.Component#addKeyListener(java.awt.event.KeyListener)
-	 */
-	@Override
-	public synchronized void addKeyListener(final KeyListener listener) {
-		textPane.addKeyListener(listener);
-	}
+    @Override
+    public synchronized void addKeyListener(final KeyListener listener) {
+        textPane.addKeyListener(listener);
+    }
 
-	/* (non-Javadoc)
-	 * @see java.awt.Component#removeKeyListener(java.awt.event.KeyListener)
-	 */
-	@Override
-	public synchronized void removeKeyListener(final KeyListener listener) {
-		textPane.removeKeyListener(listener);
-	}
+    @Override
+    public synchronized void removeKeyListener(final KeyListener listener) {
+        textPane.removeKeyListener(listener);
+    }
+
+    public void addDocumentListener(final DocumentListener listener) {
+        textPane.getDocument().addDocumentListener(listener);
+    }
+
+    public void removeDocumentListener(final DocumentListener listener) {
+        textPane.getDocument().removeDocumentListener(listener);
+    }
 
 }
