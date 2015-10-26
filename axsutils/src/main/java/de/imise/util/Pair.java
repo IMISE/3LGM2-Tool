c2ac9e1d529d5d23cee08d59d14912af5b28ace0
astruebi <a.leipzig@gmx.de>
2015-10-26 08:58:01 +0100

    Pair: equals() and hashCode()

--- axsutils/src/main/java/de/imise/util/Pair.java
+++ axsutils/src/main/java/de/imise/util/Pair.java
@@ -7,79 +7,95 @@ package de.imise.util;
  * @author AXS
  */
 public class Pair<T, S> {
-	/**
-	 * COMMENTME
-	 */
-	protected T firstObject;
-	
-	/**
-	 * COMMENTME
-	 */
-	protected S secondObject;
+    /**
+     * COMMENTME
+     */
+    protected T firstObject;
 
-	/**
-	 * @param o
-	 */
-	public void setFirstItem(T o) {
-		firstObject = o;
-	}
+    /**
+     * COMMENTME
+     */
+    protected S secondObject;
 
-	/**
-	 * @param o
-	 */
-	public void setSecondItem(S o) {
-		secondObject = o;
-	}
+    /**
+     * @param o
+     */
+    public void setFirstItem(final T o) {
+        firstObject = o;
+    }
 
-	/**
-	 * @param o1
-	 * @param o2
-	 * @param i
-	 */
-	public Pair(T o1, S o2) {
-		firstObject = o1;
-		secondObject = o2;
-	}
+    /**
+     * @param o
+     */
+    public void setSecondItem(final S o) {
+        secondObject = o;
+    }
 
-	/**
-	 * @return
-	 */
-	public T getFirstItem() {
-		return firstObject;
-	}
+    /**
+     * @param o1
+     * @param o2
+     * @param i
+     */
+    public Pair(final T o1, final S o2) {
+        firstObject = o1;
+        secondObject = o2;
+    }
 
-	/**
-	 * @return
-	 */
-	public S getSecondItem() {
-		return secondObject;
-	}
+    /**
+     * @return
+     */
+    public T getFirstItem() {
+        return firstObject;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-    public boolean equals(Object obj) {
-		if (obj == this)
-			return true;
-		if (!(obj instanceof Pair))
-			return false;
-		Pair<?, ?> op = (Pair<?, ?>) obj;
-		if (op.firstObject == firstObject && op.secondObject == secondObject)
-			return true;
-		if (firstObject == null || secondObject == null)
-			return false;
-		return firstObject.equals(op.firstObject) && secondObject.equals(op.secondObject);
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "firstObject:" + firstObject.toString() + ";secondObject:" + secondObject.toString();
-	}
+    /**
+     * @return
+     */
+    public S getSecondItem() {
+        return secondObject;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (firstObject == null ? 0 : firstObject.hashCode());
+        result = prime * result + (secondObject == null ? 0 : secondObject.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Pair<?, ?> other = (Pair<?, ?>) obj;
+        if (firstObject == null) {
+            if (other.firstObject != null) {
+                return false;
+            }
+        } else if (!firstObject.equals(other.firstObject)) {
+            return false;
+        }
+        if (secondObject == null) {
+            if (other.secondObject != null) {
+                return false;
+            }
+        } else if (!secondObject.equals(other.secondObject)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "firstObject:" + firstObject.toString() + ";secondObject:" + secondObject.toString();
+    }
 
 }
