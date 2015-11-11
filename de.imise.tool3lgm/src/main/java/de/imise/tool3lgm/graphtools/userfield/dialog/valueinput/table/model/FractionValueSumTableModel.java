f0f982f83820127e741fc3940b882878e8d476e8
astruebi <a.leipzig@gmx.de>
2015-11-11 21:30:09 +0100

    FractionValueSumPanel: added with TableModel

---
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/userfield/dialog/valueinput/table/model/FractionValueSumTableModel.java
@@ -0,0 +1,119 @@
+package de.imise.tool3lgm.graphtools.userfield.dialog.valueinput.table.model;
+
+import java.util.List;
+import java.util.Vector;
+
+import de.imise.tool3lgm.graphtools.GraphDocument;
+import de.imise.tool3lgm.graphtools.elements.Doppelkante;
+import de.imise.tool3lgm.graphtools.elements.Kante;
+import de.imise.tool3lgm.graphtools.elements.ModelElement;
+import de.imise.tool3lgm.graphtools.userfield.UserField;
+import de.imise.tool3lgm.graphtools.userfield.calculator.PartValueSumFunction;
+import de.imise.tool3lgm.graphtools.userfield.calculator.PartValueSumFunction.TWSumArguments;
+import de.imise.util.NamedObjectContainer;
+
+public class FractionValueSumTableModel extends AbstractUserFieldTableModel {
+
+    public FractionValueSumTableModel(final GraphDocument doc) {
+        super(doc);
+    }
+
+    public FractionValueSumTableModel(final ModelElement me, final Class<? extends Kante> edgeClass, final boolean edgeForwardDirection, final boolean showTopLevel, final boolean showInner, final boolean showLeafs) {
+        super(me.getCollection().getMainGraphDocument());
+        setData(me, edgeClass, edgeForwardDirection, showTopLevel, showInner, showLeafs);
+    }
+
+    /**
+     * Liefert eine Liste aller UserFields mit Teilwertsummenformeln und den aus der Formel geparsten Argumenten, die
+     * für die übergebene Element- und Kantenklasse definiert sind.
+     * 
+     * @param me
+     * @param edgeClass
+     * @return
+     */
+    private List<UserField> getFractionValueSumUserFields(final Class<? extends ModelElement> elementClass, final Class<? extends Kante> edgeClass) {
+        List<UserField> userFieldList = definitions.getUserFields(elementClass, UserField.Style.CLASSIFICATION_NUMBER_FORMULA);
+        for (int i = userFieldList.size() - 1; i >= 0; i--) {
+            UserField userField = userFieldList.get(i);
+            if (userField.isSimplePartValueSumFormula()) {
+                TWSumArguments arguments = PartValueSumFunction.getTWSumArguments(userField);
+                if (!edgeClass.isAssignableFrom(arguments.edgeClass)) {
+                    userFieldList.remove(i);
+                }
+            }
+        }
+        return userFieldList;
+    }
+
+    /**
+     * Entfernt je nach Werten der übergebenen boolean-Varibalen alle Elemente, die nicht dazu passen aus der übergebenen Liste
+     * 
+     * @param elements
+     */
+    private void removeNotVisibleHierarchyElements(final List<? extends ModelElement> elements, final boolean showTopLevel, final boolean showInner, final boolean showLeafs) {
+        for (int i = elements.size(); i <= 0; i--) {
+            ModelElement other = elements.get(i);
+            boolean stay = false;
+            if (showTopLevel && !other.hasDirectParentContainer(doc)) { // Top-Level-E. anfügen
+                stay = true;
+            } else if (showInner && other.hasDirectParentContainer(doc) && other.hasDirectPartContainer(doc)) { // Innere E. anfügen
+                stay = true;
+            } else if (showLeafs && !other.hasDirectPartContainer(doc)) { // Blatt-E. anfügen
+                stay = true;
+            }
+            if (!stay) {
+                elements.remove(i);
+            }
+        }
+    }
+
+    /**
+     * Erstellt und setzt Kennzahlen-Modeldaten
+     */
+    private void setData(final ModelElement me, final Class<? extends Kante> edgeClass, final boolean edgeForwardDirection, final boolean showTopLevel, final boolean showInner, final boolean showLeafs) {
+        //alle Teilwersummen Userfields des Elementes holen
+        // Ermitteln der UserFields zu elementClass
+        Class<? extends ModelElement> elementClass = me.getClass();
+        List<UserField> fractionValueSumUserFields = getFractionValueSumUserFields(elementClass, edgeClass);
+
+        Class<? extends ModelElement> fractionValueSumSourceClass = edgeForwardDirection ? Kante.getEndClass(edgeClass) : Kante.getStartClass(edgeClass);
+
+        int direction = edgeForwardDirection ? Doppelkante.FORWARD : Doppelkante.BACKWARD;
+        //alle mit dem Element über die Kante verbundenen Elemente holen
+        List<ModelElement> modelElements = me.getConnectedElements(fractionValueSumSourceClass, edgeClass, direction);
+
+        removeNotVisibleHierarchyElements(modelElements, showTopLevel, showInner, showLeafs);
+
+        if (modelElements.size() == 0 || fractionValueSumUserFields.size() == 0) {
+            modelElements.clear();
+            fractionValueSumUserFields.clear();
+        }
+
+        Vector<NamedObjectContainer<?>> rowIdentifiers = new Vector<NamedObjectContainer<?>>(modelElements.size());
+        // RowHeader aufbauen
+        for (ModelElement rowElement : modelElements) {
+            NamedObjectContainer<ModelElement> rowElementContainer = new NamedObjectContainer<ModelElement>(rowElement, rowElement.getName());
+            rowIdentifiers.add(rowElementContainer);
+        }
+
+        Vector<NamedObjectContainer<?>> columnIdentifiers = new Vector<NamedObjectContainer<?>>(fractionValueSumUserFields.size());
+        // ColumnHeader aufbauen
+        for (UserField userField : fractionValueSumUserFields) {
+            NamedObjectContainer<UserField> columnUserFieldContainer = new NamedObjectContainer<UserField>(userField, userField.getName());
+            columnIdentifiers.add(columnUserFieldContainer);
+        }
+
+        //DataVector aufbauen
+        Object[][] data = new Object[modelElements.size()][fractionValueSumUserFields.size()];
+        for (int i = 0; i < data.length; i++) {
+            ModelElement fractionValueSource = modelElements.get(i);
+            for (int j = 0; j < data[0].length; j++) {
+                UserField userField = fractionValueSumUserFields.get(j);
+                String value = definitions.getPartValueSumSinglePartResults().get(me, userField, fractionValueSource);
+                data[i][j] = new NamedObjectContainer<UserField>(userField, value);
+            }
+        }
+        //Daten setzen
+        setDataVector(data, columnIdentifiers, rowIdentifiers);
+    }
+}
