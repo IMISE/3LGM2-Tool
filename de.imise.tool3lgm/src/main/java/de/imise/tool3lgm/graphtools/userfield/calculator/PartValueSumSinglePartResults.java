8cefd1a8515737e7df5f0f834484635e09ed0a99
astruebi <a.leipzig@gmx.de>
2015-11-11 21:21:25 +0100

    PartValueSumSinglePartResults: complete rewritten: now there is only an add and get function and all values will be stored in a map from fraction value sum target to a table with the fraction value sum userField and the  fraction value sum source to the value

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/userfield/calculator/PartValueSumSinglePartResults.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/userfield/calculator/PartValueSumSinglePartResults.java
@@ -1,16 +1,13 @@
 package de.imise.tool3lgm.graphtools.userfield.calculator;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Map;
 
 import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Table;
 
 import de.imise.tool3lgm.graphtools.elements.ModelElement;
 import de.imise.tool3lgm.graphtools.userfield.UserField;
-import de.imise.util.Pair;
 
 /**
  * Hält für alle ModelElemente, denen eine einfache Teilwertsummenformel als UserField zugeordnet ist,
@@ -22,7 +19,7 @@ import de.imise.util.Pair;
  */
 public class PartValueSumSinglePartResults {
 
-    private final Table<ModelElement, UserField, ResultList> results = HashBasedTable.create();
+    private final Map<ModelElement, Table<UserField, ModelElement, String>> results = Maps.newHashMap();
 
     public PartValueSumSinglePartResults() {
     }
@@ -32,85 +29,26 @@ public class PartValueSumSinglePartResults {
     }
 
     public void add(final ModelElement fullResultElement, final UserField partValueSumField, final ModelElement valueSourceElement, final String partResult) {
-        ResultList currentList = results.get(fullResultElement, partValueSumField);
-        if (currentList == null) {
-            currentList = new ResultList();
-            results.put(fullResultElement, partValueSumField, currentList);
+        Table<UserField, ModelElement, String> resultTable = results.get(fullResultElement);
+        if (resultTable == null) {
+            resultTable = HashBasedTable.create();
+            results.put(fullResultElement, resultTable);
         }
-        currentList.add(valueSourceElement, partResult);
+        resultTable.put(partValueSumField, valueSourceElement, partResult);
     }
 
-    public Iterable<Pair<ModelElement, String>> getIterable(final ModelElement fullResultElement, final UserField partValueSumField) {
-        ResultList result = results.get(fullResultElement, partValueSumField);
-        if (result == null) {
-            result = new ResultList();
+    public String get(final ModelElement fullResultElement, final UserField partValueSumField, final ModelElement valueSourceElement) {
+        Table<UserField, ModelElement, String> resultTable = results.get(fullResultElement);
+        if (resultTable == null) {
+            return null;
         }
+        String result = resultTable.get(partValueSumField, valueSourceElement);
         return result;
     }
 
     @Override
     public String toString() {
         return results.toString();
-    }
-
-    /**
-     * Liste von Paaren aus einem Element und einem Ergenis. Dabei ist das Ergebnis der Anteil, der einem
-     * anderen Element für das im Paar angegebene Element in einer Teilwertsumme zugerechnet wird.
-     * 
-     * @author Ich
-     * @create 26.10.2015
-     */
-    public class ResultList implements Iterable<Pair<ModelElement, String>> {
-
-        private final List<Pair<ModelElement, String>> internalResults = Lists.newArrayList();
-
-        private void clear() {
-            internalResults.clear();
-        }
-
-        private void add(final ModelElement valueSourceElement, final String partResult) {
-            Pair<ModelElement, String> resultPair = new Pair<ModelElement, String>(valueSourceElement, partResult);
-            internalResults.add(resultPair);
-        }
-
-        @Override
-        public Iterator<Pair<ModelElement, String>> iterator() {
-            return new MyIterator();
-        }
-
-        private class MyIterator implements Iterator<Pair<ModelElement, String>> {
-            private int cursor;
-
-            public MyIterator() {
-                cursor = 0;
-            }
-
-            @Override
-            public boolean hasNext() {
-                return cursor < internalResults.size();
-            }
-
-            @Override
-            public Pair<ModelElement, String> next() {
-                if (hasNext()) {
-                    Pair<ModelElement, String> current = internalResults.get(cursor);
-                    cursor++;
-                    return current;
-                }
-                throw new NoSuchElementException();
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        }
-
-        @Override
-        public String toString() {
-            return internalResults.toString();
-        }
-
     }
 
 }
