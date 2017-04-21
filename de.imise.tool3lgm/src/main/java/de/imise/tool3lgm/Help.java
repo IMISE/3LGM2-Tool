cf3d55e18aa1f8c215b1ab2ce9eaed613eaff881
astruebi <a.leipzig@gmx.de>
2017-04-21 19:40:03 +0200

    Help.java: Alle Verweise auf konkrete Modellelemente aukommentiert, die man anscheinend schon lange nicht mehr brauchte, weil diese Hilfe schon seit Ewigkeiten so nicht funktionert haben kann. Irgendwie fehlen fast alle Hilfeseiten der Docs

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Help.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Help.java
@@ -6,7 +6,7 @@ package de.imise.tool3lgm;
 /**
  * Diese Klasse stellt den HelpBroker und das HelpSet für die gesamte
  * Anwendung zur Verfügung.
- * 
+ *
  * @author Sebp
  */
 
@@ -21,34 +21,6 @@ import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
-import de.imise.tool3lgm.graphtools.elements.ModelElement;
-import de.imise.tool3lgm.graphtools.elements.node.Anwendungsbaustein;
-import de.imise.tool3lgm.graphtools.elements.node.Anwendungsprogramm;
-import de.imise.tool3lgm.graphtools.elements.node.Aufgabe;
-import de.imise.tool3lgm.graphtools.elements.node.Bausteinschnittstelle;
-import de.imise.tool3lgm.graphtools.elements.node.Bausteintyp;
-import de.imise.tool3lgm.graphtools.elements.node.Benutzungsschnittstelle;
-import de.imise.tool3lgm.graphtools.elements.node.DBVerwaltungssystem;
-import de.imise.tool3lgm.graphtools.elements.node.Datenbanksystem;
-import de.imise.tool3lgm.graphtools.elements.node.Datensatztyp;
-import de.imise.tool3lgm.graphtools.elements.node.Dokumentensammlung;
-import de.imise.tool3lgm.graphtools.elements.node.Dokumententyp;
-import de.imise.tool3lgm.graphtools.elements.node.Ereignistyp;
-import de.imise.tool3lgm.graphtools.elements.node.EtntEtdtKombination;
-import de.imise.tool3lgm.graphtools.elements.node.Kommunikationsprozess;
-import de.imise.tool3lgm.graphtools.elements.node.Kommunikationsstandard;
-import de.imise.tool3lgm.graphtools.elements.node.KonAnwendungsbaustein;
-import de.imise.tool3lgm.graphtools.elements.node.Nachrichtentyp;
-import de.imise.tool3lgm.graphtools.elements.node.Netzprotokoll;
-import de.imise.tool3lgm.graphtools.elements.node.Netztyp;
-import de.imise.tool3lgm.graphtools.elements.node.Objekttyp;
-import de.imise.tool3lgm.graphtools.elements.node.Organisationseinheit;
-import de.imise.tool3lgm.graphtools.elements.node.Organisationsplan;
-import de.imise.tool3lgm.graphtools.elements.node.PhysischerDVBaustein;
-import de.imise.tool3lgm.graphtools.elements.node.RechAnwendungsbaustein;
-import de.imise.tool3lgm.graphtools.elements.node.Softwareprodukt;
-import de.imise.tool3lgm.graphtools.elements.node.Standort;
-import de.imise.tool3lgm.graphtools.elements.node.Subnetz;
 import de.imise.tool3lgm.log.Log;
 import de.imise.tool3lgm.userproperties.UserProperties;
 
@@ -85,7 +57,7 @@ public class Help {
 
     /**
      * Gibt die Instanz dieser Klasse zurück.
-     * 
+     *
      * @return eine Instanz der Klasse Help.
      */
     public static Help getHelp() {
@@ -98,7 +70,7 @@ public class Help {
     /**
      * Ermöglicht die Verwendung der durch <code>id</code> spezifizierten Hilfe innerhalb einer Komponente wie
      * z.B. innerhalb eines {@link JFrame}s.
-     * 
+     *
      * @param comp
      *            Komponente innerhalb der die Hilfe aktiviert werden soll (z.B. {@link JFrame})
      * @param id
@@ -113,7 +85,7 @@ public class Help {
     /**
      * Ermöglicht das Öffnen der durch <code>id</code> spezifizierten Hilfe durch Click auf die spezifizierte
      * Komponente.
-     * 
+     *
      * @param comp
      *            Komponente die die Hilfe öffnet (z.B. {@link JMenuItem})
      * @param id
@@ -127,7 +99,7 @@ public class Help {
     /**
      * Gibt einen {@link ActionListener} zurück, der bei Anfügen die Direkthilfe für einer Swing-Komponente
      * aktiviert.
-     * 
+     *
      * @see CSH.DisplayHelpAfterTracking
      */
     public CSH.DisplayHelpAfterTracking getDisplayHelpAfterTracking() {
@@ -139,7 +111,7 @@ public class Help {
      * muß diesem mittels CSH.setHelpIDString() eine HelpID zugewiesen werden.
      * (Die HelpID muß in der Datei map-Datei über eine mapID mit der URL der
      * entsprechenden Hilfe Seite verlinkt sein.)
-     * 
+     *
      * @return die HelpID für das übergebene Object.
      */
     public String getHelpID(final Object o) {
@@ -150,92 +122,98 @@ public class Help {
         // Wenn ein ElementPropertyDialog übergeben wurde, wird die Help der
         // Komponente zurück gegeben, die im ElementPropertyDialog angezeigt wird.
         if (o instanceof ElementPropertyDialog) {
-            ModelElement me = ((ElementPropertyDialog) o).getModelElement();
-            // SEBP anwendungsbausteinkonfiguration
-            if (me instanceof Anwendungsbaustein) {
-                return "dialoge_gemischt_ab";
-            }
-            if (me instanceof Anwendungsprogramm) {
-                return "dialoge_anwendungsprogramm";
-            }
-            if (me instanceof Aufgabe) {
-                return "dialoge_aufgabe";
-            }
-            // SEBP auforgkombination
-            if (me instanceof Bausteinschnittstelle) {
-                return "dialoge_baustein_s";
-            }
-            if (me instanceof Bausteintyp) {
-                return "dialoge_bausteintyp";
-            }
-            if (me instanceof Benutzungsschnittstelle) {
-                return "dialoge_benutzer_s";
-            }
-            if (me instanceof Datenbanksystem) {
-                return "dialoge_dbs";
-            }
-            if (me instanceof Datensatztyp) {
-                return "dialoge_datensatztyp";
-            }
-            // SEBP dbkonfiguration
-            if (me instanceof DBVerwaltungssystem) {
-                return "dialoge_db_system";
-            }
-            if (me instanceof Dokumentensammlung) {
-                return "dialoge_dokumentensammlung";
-            }
-            if (me instanceof Dokumententyp) {
-                return "dialoge_dokumententyp";
-            }
-            if (me instanceof Ereignistyp) {
-                return "dialoge_ereignistyp";
-            }
-            if (me instanceof EtntEtdtKombination) {
-                return "dialoge_etnt_kombi";
-            }
-            if (me instanceof Kommunikationsprozess) {
-                return "dialoge_kom_prozess";
-            }
-            if (me instanceof Kommunikationsstandard) {
-                return "dialoge_komm_standard";
-            }
-            if (me instanceof KonAnwendungsbaustein) {
-                return "dialoge_papier_ab";
-            }
-            if (me instanceof Nachrichtentyp) {
-                return "dialoge_nachrichtentyp";
-            }
-            if (me instanceof Netzprotokoll) {
-                return "dialoge_netzprotokoll";
-            }
-            if (me instanceof Netztyp) {
-                return "dialoge_netztyp";
-            }
-            if (me instanceof Objekttyp) {
-                return "dialoge_objekttyp";
-            }
-            if (me instanceof Organisationseinheit) {
-                return "dialoge_organisationseinheit";
-            }
-            if (me instanceof Organisationsplan) {
-                return "dialoge_organisationsplan";
-            }
-            if (me instanceof PhysischerDVBaustein) {
-                return "dialoge_dv_baustein";
-            }
-            // SEBP Prozess
-            if (me instanceof RechAnwendungsbaustein) {
-                return "dialoge_rechner_ab";
-            }
-            if (me instanceof Softwareprodukt) {
-                return "dialoge_softwareprodukt";
-            }
-            if (me instanceof Standort) {
-                return "dialoge_standort";
-            }
-            if (me instanceof Subnetz) {
-                return "dialoge_subnetz";
-            }
+
+            //AXS: 21.04.2017: Das hier scheint nie oder schon sehr lange nicht mehr funktioniert zu haben.
+            //Abgesehen davon ist es totaler Schrott. So ein Mapping wie hier sollte natürlich generisch
+            //funktionieren und diese komischen return-Strings sollten immer so etwas heißen wie:
+            //"dialoge_" + Anwendungsbaustein.class.getSimpleName();
+
+            //            ModelElement me = ((ElementPropertyDialog) o).getModelElement();
+            //            // SEBP anwendungsbausteinkonfiguration
+            //            if (me instanceof Anwendungsbaustein) {
+            //                return "dialoge_gemischt_ab";
+            //            }
+            //            if (me instanceof Anwendungsprogramm) {
+            //                return "dialoge_anwendungsprogramm";
+            //            }
+            //            if (me instanceof Aufgabe) {
+            //                return "dialoge_aufgabe";
+            //            }
+            //            // SEBP auforgkombination
+            //            if (me instanceof Bausteinschnittstelle) {
+            //                return "dialoge_baustein_s";
+            //            }
+            //            if (me instanceof Bausteintyp) {
+            //                return "dialoge_bausteintyp";
+            //            }
+            //            if (me instanceof Benutzungsschnittstelle) {
+            //                return "dialoge_benutzer_s";
+            //            }
+            //            if (me instanceof Datenbanksystem) {
+            //                return "dialoge_dbs";
+            //            }
+            //            if (me instanceof Datensatztyp) {
+            //                return "dialoge_datensatztyp";
+            //            }
+            //            // SEBP dbkonfiguration
+            //            if (me instanceof DBVerwaltungssystem) {
+            //                return "dialoge_db_system";
+            //            }
+            //            if (me instanceof Dokumentensammlung) {
+            //                return "dialoge_dokumentensammlung";
+            //            }
+            //            if (me instanceof Dokumententyp) {
+            //                return "dialoge_dokumententyp";
+            //            }
+            //            if (me instanceof Ereignistyp) {
+            //                return "dialoge_ereignistyp";
+            //            }
+            //            if (me instanceof EtntEtdtKombination) {
+            //                return "dialoge_etnt_kombi";
+            //            }
+            //            if (me instanceof Kommunikationsprozess) {
+            //                return "dialoge_kom_prozess";
+            //            }
+            //            if (me instanceof Kommunikationsstandard) {
+            //                return "dialoge_komm_standard";
+            //            }
+            //            if (me instanceof KonAnwendungsbaustein) {
+            //                return "dialoge_papier_ab";
+            //            }
+            //            if (me instanceof Nachrichtentyp) {
+            //                return "dialoge_nachrichtentyp";
+            //            }
+            //            if (me instanceof Netzprotokoll) {
+            //                return "dialoge_netzprotokoll";
+            //            }
+            //            if (me instanceof Netztyp) {
+            //                return "dialoge_netztyp";
+            //            }
+            //            if (me instanceof Objekttyp) {
+            //                return "dialoge_objekttyp";
+            //            }
+            //            if (me instanceof Organisationseinheit) {
+            //                return "dialoge_organisationseinheit";
+            //            }
+            //            if (me instanceof Organisationsplan) {
+            //                return "dialoge_organisationsplan";
+            //            }
+            //            if (me instanceof PhysischerDVBaustein) {
+            //                return "dialoge_dv_baustein";
+            //            }
+            //            // SEBP Prozess
+            //            if (me instanceof RechAnwendungsbaustein) {
+            //                return "dialoge_rechner_ab";
+            //            }
+            //            if (me instanceof Softwareprodukt) {
+            //                return "dialoge_softwareprodukt";
+            //            }
+            //            if (me instanceof Standort) {
+            //                return "dialoge_standort";
+            //            }
+            //            if (me instanceof Subnetz) {
+            //                return "dialoge_subnetz";
+            //            }
         }
 
         return null;
