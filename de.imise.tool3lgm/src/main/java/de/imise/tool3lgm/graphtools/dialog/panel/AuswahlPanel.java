30fc14ff72b0b9ba71d91a033b6736bf0f7df6f4
astruebi <a.leipzig@gmx.de>
2017-04-26 10:16:00 +0200

    AbstractPathConnectionPanel reagiert für die Anzeigekomponente auf Mausaktionen: Doppelklick = Öffne Eigenschaftsdialog des angezeigten Elementes, Rechte Maustaste = Kontextmenü AuswahlPanel damit verknüpft

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/panel/AuswahlPanel.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/panel/AuswahlPanel.java
@@ -3,22 +3,20 @@ package de.imise.tool3lgm.graphtools.dialog.panel;
 import java.awt.BorderLayout;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.MouseEvent;
 import java.util.EventObject;
 import java.util.List;
 
 import javax.swing.JLabel;
 
-import de.imise.tool3lgm.Tool3lgm;
 import de.imise.tool3lgm.Tool3lgmConstants;
 import de.imise.tool3lgm.graphtools.GDCollection;
 import de.imise.tool3lgm.graphtools.GraphDocument;
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
 import de.imise.tool3lgm.graphtools.dialog.action.LGMAction;
 import de.imise.tool3lgm.graphtools.dialog.action.LGMItemListener;
-import de.imise.tool3lgm.graphtools.dialog.action.LGMMouseListener;
 import de.imise.tool3lgm.graphtools.dialog.dragdrop.DragNDropInitializer.DragNDropActionChain;
 import de.imise.tool3lgm.graphtools.dialog.dragdrop.LGMDragNDropTree;
+import de.imise.tool3lgm.graphtools.elements.Composition;
 import de.imise.tool3lgm.graphtools.elements.Kante;
 import de.imise.tool3lgm.graphtools.elements.ModelConstants;
 import de.imise.tool3lgm.graphtools.elements.ModelElement;
@@ -29,6 +27,11 @@ import de.imise.util.swing.component.AlphabeticalComboBox;
 
 /**
  * @author AXS
+ *         Dieses Panel stellt in einer Combobox ein einzelne Element zur Verknüpfung mit dem
+ *         ModelElement des Dialoges zur Auswahl. Je nachdem ob die Verknüpfung über eine normale
+ *         {@link Kante} oder eine {@link Composition} läuft, werden andere im Modell befindliche
+ *         Elemente zur Verknüpfung angeboten {@link Kante}) oder nicht ({@link Composition}).
+ *         Die Verknüpfung kann über einen Pfad erfolgen, d.h. es gehen nicht nur direkte Verbindungen.
  */
 public class AuswahlPanel extends AbstractPathConnectionPanel {
 
@@ -60,8 +63,8 @@ public class AuswahlPanel extends AbstractPathConnectionPanel {
         super(dialog, edgeClasses);
         setLayout(new BorderLayout());
         box = new AlphabeticalComboBox();
-        // Action erstell und Listener an Panel und Box anhängen
-        box.addMouseListener(new LGMMouseListener(null, null, null, getMouseAction(), null));
+        // Action erstellen und Listener an Panel und Box anhängen
+        addMouseActions(box);
 
         itemListener = new LGMItemListener(getItemStateChangedAction(this, searchElementClass));
         box.addItemListener(itemListener);
@@ -74,18 +77,6 @@ public class AuswahlPanel extends AbstractPathConnectionPanel {
         westLabel.setText(Tool3lgmConstants.getResString(searchElementClass.getSimpleName()));
         createNew = new NamedObjectContainer<Object>(this, Tool3lgmConstants.getResString("auswahlPanel_neu") + " " + ModelConstants.getDisplayableName(searchElementClass));
         init();
-    }
-
-    /**
-     * @param dialog
-     * @param addLabel
-     * @param edgeClasses
-     */
-    public AuswahlPanel(final ElementPropertyDialog dialog, final boolean addLabel, final Class<? extends Kante>... edgeClasses) {
-        this(dialog, edgeClasses);
-        if (addLabel == false) {
-            remove(westLabel);
-        }
     }
 
     /**
@@ -121,13 +112,6 @@ public class AuswahlPanel extends AbstractPathConnectionPanel {
     @Override
     protected void showFullDialog() {
         super.showFullDialog();
-    }
-
-    /**
-     * @return
-     */
-    public AlphabeticalComboBox getBox() {
-        return box;
     }
 
     /**
@@ -180,32 +164,6 @@ public class AuswahlPanel extends AbstractPathConnectionPanel {
         };
     }
 
-    /**
-     * Methode liefert eine <code>LGMAction</code> zurück, die auf Mouse-Aktionen in Panels
-     * reagiert.
-     *
-     * @param edp
-     */
-    private final LGMAction getMouseAction() {
-        final AuswahlPanel panel = this;
-        final GraphDocument doc = panel.getGraphDocument();
-        final ElementPropertyDialog dialog = panel.getDialog();
-        return new LGMAction() {
-            @Override
-            public void execute(final EventObject eo) {
-                MouseEvent e = (MouseEvent) eo;
-                if (Tool3lgmConstants.isPopupTrigger(e)) {
-                    Object item = panel.getBox().getSelectedItem();
-                    if (item != null && item instanceof NodeContainer) {
-                        NodeContainer knot = (NodeContainer) item;
-                        doc.select(knot, dialog.getTransactionID());
-                        Tool3lgm.getContextGenerator().getTreeKnotContextMenu().show(e.getComponent(), e.getX() + 3, e.getY() + 3);
-                    }
-                }
-            }
-        };
-    }
-
     @Override
     protected DragNDropActionChain[] collectDragNDropActionChains() {
         return new DragNDropActionChain[] {};
@@ -214,6 +172,11 @@ public class AuswahlPanel extends AbstractPathConnectionPanel {
     @Override
     public LGMDragNDropTree[] getAllDragNDropTrees() {
         return new LGMDragNDropTree[] {};
+    }
+
+    @Override
+    protected Object getMouseSelectedItem() {
+        return box.getSelectedItem();
     }
 
 }
