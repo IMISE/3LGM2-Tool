30cb3b9a339ea885bf36320c1c28fb9d431cbdaa
Alexander Strübing <astruebi@regebtropfen.org>
2017-05-22 08:18:57 +0200

    Sys: Syste.out.println() und System.err.println() mit zugehörigem StackTrace ausgeben

---
+++ axsutils/src/main/java/de/imise/util/Sys.java
@@ -0,0 +1,52 @@
+package de.imise.util;
+
+public class Sys {
+
+    /** Anzahl der StackTrace-Zeilen, die uasgegeben werden sollen */
+    public static int traceSteps = 10;
+
+    public static boolean insertBlankLineAfterOutput;
+
+    public static void out(final String message) {
+        System.out.println(message);
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (int i = 2; i < 10; i++) {
+            System.out.println(stackTrace[i]);
+        }
+        if (insertBlankLineAfterOutput) {
+            System.out.println();
+        }
+    }
+
+    public static void err(final String message) {
+        System.err.println(message);
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (int i = 2; i < 10; i++) {
+            System.err.println(stackTrace[i]);
+        }
+        if (insertBlankLineAfterOutput) {
+            System.err.println();
+        }
+    }
+
+    public static void out1(String message) {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        System.out.println(message);
+        System.out.print("      ");
+        System.out.println(stackTrace[2]);
+        if (insertBlankLineAfterOutput) {
+            System.out.println();
+        }
+    }
+
+    public static void err1(String message) {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        System.err.println(message);
+        System.err.print("      ");
+        System.err.println(stackTrace[2]);
+        if (insertBlankLineAfterOutput) {
+            System.err.println();
+        }
+    }
+
+}
