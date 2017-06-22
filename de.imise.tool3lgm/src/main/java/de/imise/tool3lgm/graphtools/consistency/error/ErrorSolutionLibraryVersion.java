ce4d735aa0ad53b885bd258b2f67b638e3acde9e
astruebi <a.leipzig@gmx.de>
2017-06-22 05:20:29 +0200

    ErrorSolutionLibraryVersion: alles MetaModell-spezifische raus

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/consistency/error/ErrorSolutionLibraryVersion.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/consistency/error/ErrorSolutionLibraryVersion.java
@@ -3,43 +3,35 @@
  */
 package de.imise.tool3lgm.graphtools.consistency.error;
 
-import de.imise.tool3lgm.graphtools.dialog.panel.AwbAufPanel;
-import de.imise.tool3lgm.graphtools.dialog.panel.PathConnectionPanel;
-import de.imise.tool3lgm.graphtools.elements.edge.AufAufOrgVerbindung;
-import de.imise.tool3lgm.graphtools.elements.edge.AwbAwbkVerbindung;
-import de.imise.tool3lgm.graphtools.elements.edge.AwbkAufOrgVerbindung;
-import de.imise.tool3lgm.graphtools.elements.edge.PdvbPdvbkVerbindung;
-import de.imise.tool3lgm.graphtools.elements.edge.PdvbkAwbVerbindung;
-import de.imise.tool3lgm.graphtools.elements.node.ABKonfiguration;
-import de.imise.tool3lgm.graphtools.elements.node.Anwendungsbaustein;
-import de.imise.tool3lgm.graphtools.elements.node.Aufgabe;
-import de.imise.tool3lgm.graphtools.elements.node.DBKonfiguration;
-import de.imise.tool3lgm.graphtools.path.MetaPath;
-
 /**
+ * Diese Datei ist aktuell überflüssig und nur noch drin, damit man erkennen kann, wie die ErrorSolutions funktionieren.
+ * Im Grunde ist das nur dazu da, im Fehlerfall an einen besseren Ort (Tab in einem Eigenschaftsdialog) zu lenken,
+ * als den Dialog des Elementes, bei dem bei einer Kante ein Kardinalitätsfehler besteht, direkt den Reiter für diese
+ * fehlerhafte Kante zu öffnen.
+ *
  * @author AXS
  */
 public class ErrorSolutionLibraryVersion {
 
-    @SuppressWarnings("rawtypes")
-    private static Class[][] abKonfigToAufgabeEdgeClasses = {
-            {
-                    AwbkAufOrgVerbindung.class,
-                    AufAufOrgVerbindung.class
-            }
-    };
-    @SuppressWarnings("rawtypes")
-    private static Class[][] abKonfigToAnwendungsbausteinEdgeClasses = {
-            {
-                    AwbAwbkVerbindung.class
-            }
-    };
-    @SuppressWarnings("rawtypes")
-    private static Class[][] dbKonfigToAnwendungsbausteinEdgeClasses = {
-            {
-                    PdvbkAwbVerbindung.class
-            }
-    };
+    // @SuppressWarnings("rawtypes")
+    // private static Class[][] abKonfigToAufgabeEdgeClasses = {
+    // {
+    // AwbkAufOrgVerbindung.class,
+    // AufAufOrgVerbindung.class
+    // }
+    // };
+    // @SuppressWarnings("rawtypes")
+    // private static Class[][] abKonfigToAnwendungsbausteinEdgeClasses = {
+    // {
+    // AwbAwbkVerbindung.class
+    // }
+    // };
+    // @SuppressWarnings("rawtypes")
+    // private static Class[][] dbKonfigToAnwendungsbausteinEdgeClasses = {
+    // {
+    // PdvbkAwbVerbindung.class
+    // }
+    // };
 
     /**
      * Falls man mal verschiedene Metamodelle gleichzeitug nutzen will, muss man für jede
@@ -51,16 +43,16 @@ public class ErrorSolutionLibraryVersion {
     }
 
     // wenn man keine Error-Solution für einen Min oder Max-Fehler findet, wird ein neues
-    // OneToNUndirectedConnectionPanel angefügt, das den Fehler anzeigt
+    // PathConnectionPanel angefügt, das den Fehler anzeigt
     // wenn die minimale Kardinalität eines initalSubTypes unterschritten ist, dann einfach einen
     // neuen anlegen (Anwendungsparogramm bei AWB, Orgplan bei KAWB)
-    // Kanten ohne start und end einfach löschen
+    // Kanten ohne start und end einfach löschen (das macht der ModelCleaner beim Einlesen und Speichern)
 
     @SuppressWarnings("unchecked")
     private final ErrorSolution[] CARDINALITY_ERROR_SOLUTIONS = {
-            new ErrorSolution(ABKonfiguration.class, AwbAwbkVerbindung.class, new MetaPath(ABKonfiguration.class, Aufgabe.class, abKonfigToAufgabeEdgeClasses), PathConnectionPanel.class, "ABKonfiguration"),
-            new ErrorSolution(ABKonfiguration.class, AwbkAufOrgVerbindung.class, new MetaPath(ABKonfiguration.class, Anwendungsbaustein.class, abKonfigToAnwendungsbausteinEdgeClasses), AwbAufPanel.class, "Aufgabe_p"),
-            new ErrorSolution(DBKonfiguration.class, PdvbPdvbkVerbindung.class, new MetaPath(DBKonfiguration.class, Anwendungsbaustein.class, dbKonfigToAnwendungsbausteinEdgeClasses), PathConnectionPanel.class, "PhysischerDVBaustein_p"),
+            //            new ErrorSolution(ABKonfiguration.class, AwbAwbkVerbindung.class, new MetaPath(ABKonfiguration.class, Aufgabe.class, abKonfigToAufgabeEdgeClasses), PathConnectionPanel.class, "ABKonfiguration"),
+            //            new ErrorSolution(ABKonfiguration.class, AwbkAufOrgVerbindung.class, new MetaPath(ABKonfiguration.class, Anwendungsbaustein.class, abKonfigToAnwendungsbausteinEdgeClasses), PathConnectionPanel.class, "Aufgabe_p"),
+            //            new ErrorSolution(DBKonfiguration.class, PdvbPdvbkVerbindung.class, new MetaPath(DBKonfiguration.class, Anwendungsbaustein.class, dbKonfigToAnwendungsbausteinEdgeClasses), PathConnectionPanel.class, "PhysischerDVBaustein_p"),
     };
 
     /**
@@ -82,13 +74,5 @@ public class ErrorSolutionLibraryVersion {
         }
         return null;
     }
-
-    // /**
-    // * @return
-    // */
-    // public final Pair<Class<? extends ModelElement>, Class<? extends Kante>>[]
-    // getMinCardinalityNoSolutuinErrors(){
-    // return MINCARDINALITY_NO_SOLUTION_ERRORS;
-    // }
 
 }
