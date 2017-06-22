4209c0075d3b303220348454cacc3fb901305fa3
astruebi <a.leipzig@gmx.de>
2017-06-22 05:10:19 +0200

    NConnectionsPanel: komplett entfernt (endlich alle Verwenundungsorte durch PathConnectionPanel ersetzt und getestet)

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/consistency/error/ErrorSolutionLibraryVersion.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/consistency/error/ErrorSolutionLibraryVersion.java
@@ -4,27 +4,16 @@
 package de.imise.tool3lgm.graphtools.consistency.error;
 
 import de.imise.tool3lgm.graphtools.dialog.panel.AwbAufPanel;
-import de.imise.tool3lgm.graphtools.dialog.panel.NConnectionPanel;
 import de.imise.tool3lgm.graphtools.dialog.panel.PathConnectionPanel;
 import de.imise.tool3lgm.graphtools.elements.edge.AufAufOrgVerbindung;
 import de.imise.tool3lgm.graphtools.elements.edge.AwbAwbkVerbindung;
 import de.imise.tool3lgm.graphtools.elements.edge.AwbkAufOrgVerbindung;
-import de.imise.tool3lgm.graphtools.elements.edge.DbsDatVerbindung;
-import de.imise.tool3lgm.graphtools.elements.edge.DoksDokVerbindung;
-import de.imise.tool3lgm.graphtools.elements.edge.EtntKommstVerbindung;
-import de.imise.tool3lgm.graphtools.elements.edge.OrgAufOrgVerbindung;
 import de.imise.tool3lgm.graphtools.elements.edge.PdvbPdvbkVerbindung;
 import de.imise.tool3lgm.graphtools.elements.edge.PdvbkAwbVerbindung;
-import de.imise.tool3lgm.graphtools.elements.edge.RawbAwpVerbindung;
 import de.imise.tool3lgm.graphtools.elements.node.ABKonfiguration;
 import de.imise.tool3lgm.graphtools.elements.node.Anwendungsbaustein;
-import de.imise.tool3lgm.graphtools.elements.node.Anwendungsprogramm;
-import de.imise.tool3lgm.graphtools.elements.node.AufOrgKombination;
 import de.imise.tool3lgm.graphtools.elements.node.Aufgabe;
 import de.imise.tool3lgm.graphtools.elements.node.DBKonfiguration;
-import de.imise.tool3lgm.graphtools.elements.node.Datensatztyp;
-import de.imise.tool3lgm.graphtools.elements.node.Dokumententyp;
-import de.imise.tool3lgm.graphtools.elements.node.EtntEtdtKombination;
 import de.imise.tool3lgm.graphtools.path.MetaPath;
 
 /**
@@ -69,18 +58,8 @@ public class ErrorSolutionLibraryVersion {
 
     @SuppressWarnings("unchecked")
     private final ErrorSolution[] CARDINALITY_ERROR_SOLUTIONS = {
-            new ErrorSolution(AufOrgKombination.class, AufAufOrgVerbindung.class, NConnectionPanel.class, "Aufgabe"),
             new ErrorSolution(ABKonfiguration.class, AwbAwbkVerbindung.class, new MetaPath(ABKonfiguration.class, Aufgabe.class, abKonfigToAufgabeEdgeClasses), PathConnectionPanel.class, "ABKonfiguration"),
             new ErrorSolution(ABKonfiguration.class, AwbkAufOrgVerbindung.class, new MetaPath(ABKonfiguration.class, Anwendungsbaustein.class, abKonfigToAnwendungsbausteinEdgeClasses), AwbAufPanel.class, "Aufgabe_p"),
-            new ErrorSolution(Anwendungsprogramm.class, RawbAwpVerbindung.class, NConnectionPanel.class, "Anwendungsbaustein"),
-            // new ErrorSolution(Bausteinschnittstelle.class, AwbKommssVerbindung.class, ???),
-            // new ErrorSolution(Anwendungsprogramm.class, AwpSwpVerbindung.class) nur max
-            // BssKommstVerbindung.class, Bausteinschnittstelle.class, nur max
-            new ErrorSolution(Datensatztyp.class, DbsDatVerbindung.class, NConnectionPanel.class, "Datenbanksystem"),
-            new ErrorSolution(Dokumententyp.class, DoksDokVerbindung.class, NConnectionPanel.class, "Dokumentensammlung"),
-            new ErrorSolution(Dokumententyp.class, DoksDokVerbindung.class, NConnectionPanel.class, "Dokumentensammlung"),
-            new ErrorSolution(EtntEtdtKombination.class, EtntKommstVerbindung.class, NConnectionPanel.class, "Kommunikationsstandard"),
-            new ErrorSolution(AufOrgKombination.class, OrgAufOrgVerbindung.class, NConnectionPanel.class, "Organisationseinheit"),
             new ErrorSolution(DBKonfiguration.class, PdvbPdvbkVerbindung.class, new MetaPath(DBKonfiguration.class, Anwendungsbaustein.class, dbKonfigToAnwendungsbausteinEdgeClasses), PathConnectionPanel.class, "PhysischerDVBaustein_p"),
     };
 
