815efe3ab1349ebe564758a0c9eb66e115abf852
Alexander Strübing <astruebi@regentropfen.org>
2017-06-26 22:37:25 +0200

    EasyComponents: createButton(resKey) versucht zuerst den String als ResourceKey zu laden und wenn das schief geht, dann ein Icon mit dem Namen, das dann für den neuen Button gesetzt wird

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/tools/EasyComponents.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/tools/EasyComponents.java
@@ -2,6 +2,7 @@ package de.imise.tool3lgm.graphtools.dialog.tools;
 
 import java.awt.event.ActionListener;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
 
 import de.imise.tool3lgm.Tool3lgmConstants;
@@ -18,11 +19,13 @@ public class EasyComponents {
      */
     public static JButton createButton(final ActionListener actionListener, final String resKey) {
         String buttonText = resKey;
+        Icon icon = null;
         try {
             buttonText = Tool3lgmConstants.getResString(resKey);
         } catch (Exception e) {
+            icon = Tool3lgmConstants.getIcon(resKey);
         }
-        JButton button = new JButton(buttonText);
+        JButton button = icon == null || icon.getIconWidth() < 0 ? new JButton(buttonText) : new JButton(icon);
         button.setActionCommand(resKey);
         if (actionListener != null) {
             button.addActionListener(actionListener);
