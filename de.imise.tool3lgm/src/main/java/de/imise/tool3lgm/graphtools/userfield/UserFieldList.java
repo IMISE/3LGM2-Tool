d71241beddcdb343ec594e5239291f76201294de
Alexander Strübing <astruebi@regentropfen.org>
2017-07-10 14:39:09 +0200

    UserFieldList: verallgemeinert zur UserFieldTargetSpecificList

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/userfield/UserFieldList.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/userfield/UserFieldList.java
@@ -1,143 +1,20 @@
 package de.imise.tool3lgm.graphtools.userfield;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import com.google.common.collect.ImmutableList;
-
-import de.imise.tool3lgm.Tool3lgmConstants;
-import de.imise.tool3lgm.log.Log;
-
 /**
  * @author Thomas Rudert
  */
-public class UserFieldList implements Cloneable, Iterable<UserField> {
-
-    /**
-     * COMMENTME
-     */
-    private ArrayList<UserField> userFields = new ArrayList<UserField>();
-
-    /**
-     * COMMENTME
-     */
-    private Class<? extends UserFieldTarget> targetClass;
+public class UserFieldList extends UserFieldTargetSpecificList<UserField> {
 
     /**
      * @param targetClass
      */
     public UserFieldList(final Class<? extends UserFieldTarget> targetClass) {
-        this.targetClass = targetClass;
-    }
-
-    /**
-     * @param userField
-     * @param index
-     */
-    public void insert(final UserField userField, final int index) {
-        userFields.remove(userField);
-        userFields.add(index, userField);
-    }
-
-    /**
-     * @param userField
-     */
-    public void add(final UserField userField) {
-        if (userField.hasStyle(UserField.Style.FORMAT)) {
-            insert(userField, 0);
-        }
-        if (!userFields.contains(userField)) {
-            userFields.add(userField);
-        } else {
-            // Das hier ermöglich das Importieren von userFields. 
-            // Es werden somit schon bestehende userField und deren Eiegenschaften überschrieben
-            userFields.remove(userField);
-            userFields.add(userField);
-        }
-    }
-
-    /**
-     * @param userField
-     */
-    public void remove(final UserField userField) {
-        userFields.remove(userField);
-    }
-
-    /**
-     * @return
-     */
-    public int getUserFieldsCount() {
-        return userFields.size();
-    }
-
-    /**
-     * @param i
-     * @return
-     */
-    public UserField get(final int i) {
-        if (i < 0 || i >= userFields.size()) {
-            return null;
-        }
-        return userFields.get(i);
-    }
-
-    /**
-     * @return
-     */
-    public Class<? extends UserFieldTarget> getTargetClass() {
-        return targetClass;
+        super(targetClass);
     }
 
     @Override
-    public Object clone() {
-        UserFieldList collection = null;
-        try {
-            collection = (UserFieldList) super.clone();
-        } catch (Exception e) {
-            Log.show(Log.ERROR, Tool3lgmConstants.getErrString("FehlerAllgemein"), e);
-            return null;
-        }
-        collection.targetClass = targetClass;
-        collection.userFields = new ArrayList<UserField>(userFields);
-        return collection;
+    protected int getInsertIndex(final UserField userField) {
+        return userField.hasStyle(UserField.Style.FORMAT) ? 0 : super.getInsertIndex(userField);
     }
 
-    /**
-     * @return
-     */
-    public String toXMLString() {
-        String retVal = new String();
-        for (UserField uf : userFields) {
-            retVal = retVal.concat(uf.toXMLString());
-        }
-        return retVal;
-    }
-
-    /**
-     * @param hashString
-     * @return
-     */
-    public UserField get(final Object hashString) {
-        for (UserField uf : userFields) {
-            if (uf.getHashCode().equals(hashString)) {
-                return uf;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public Iterator<UserField> iterator() {
-        return userFields.iterator();
-    }
-
-    /**
-     * Kopie
-     * 
-     * @return
-     */
-    public List<UserField> getData() {
-        return ImmutableList.copyOf(userFields);
-    }
 }
---
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/userfield/UserFieldTargetSpecificList.java
@@ -0,0 +1,151 @@
+package de.imise.tool3lgm.graphtools.userfield;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import de.imise.tool3lgm.Tool3lgmConstants;
+import de.imise.tool3lgm.log.Log;
+import de.imise.tool3lgm.xml.IXMLSource;
+
+/**
+ * @author Thomas Rudert
+ */
+public class UserFieldTargetSpecificList<T extends IHashSource & IXMLSource> implements Cloneable, Iterable<T>, IXMLSource {
+
+    private Class<? extends UserFieldTarget> targetClass;
+
+    private ArrayList<T> list = Lists.newArrayList();
+
+    /**
+     * @param targetClass
+     */
+    public UserFieldTargetSpecificList(final Class<? extends UserFieldTarget> targetClass) {
+        this.targetClass = targetClass;
+    }
+
+    /**
+     * @param element
+     * @param index
+     */
+    public final void insert(final T element, final int index) {
+        list.remove(element);
+        list.add(index, element);
+    }
+
+    protected int getInsertIndex(final T element) {
+        return -1;
+    }
+
+    /**
+     * @param element
+     */
+    public final void add(final T element) {
+        int insertIndex = getInsertIndex(element);
+        if (insertIndex >= 0) {
+            insert(element, insertIndex);
+        }
+        if (!list.contains(element)) {
+            list.add(element);
+        } else {
+            // Das hier ermöglich das Importieren von userFields.
+            // Es werden somit schon bestehende userField und deren Eiegenschaften überschrieben
+            list.remove(element);
+            list.add(element);
+        }
+    }
+
+    /**
+     * @param element
+     */
+    public final void remove(final T element) {
+        list.remove(element);
+    }
+
+    /**
+     * @return
+     */
+    public final int getSize() {
+        return list.size();
+    }
+
+    /**
+     * @param i
+     * @return
+     */
+    public final T get(final int i) {
+        return isValidIndex(i) ? list.get(i) : null;
+    }
+
+    /**
+     * @return
+     */
+    public final Class<? extends UserFieldTarget> getTargetClass() {
+        return targetClass;
+    }
+
+    @Override
+    public final Object clone() {
+        UserFieldTargetSpecificList<T> collection = null;
+        try {
+            collection = (UserFieldTargetSpecificList<T>) super.clone();
+        } catch (Exception e) {
+            Log.show(Log.ERROR, Tool3lgmConstants.getErrString("FehlerAllgemein"), e);
+            return null;
+        }
+        collection.targetClass = targetClass;
+        collection.list = Lists.newArrayList(list);
+        return collection;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public final String toXMLString() {
+        String retVal = new String();
+        for (T element : list) {
+            retVal = retVal.concat(element.toXMLString());
+        }
+        return retVal;
+    }
+
+    /**
+     * @param hashString
+     * @return
+     */
+    public final T get(final Object hashString) {
+        for (T element : list) {
+            if (element.getHashCode().equals(hashString)) {
+                return element;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public final Iterator<T> iterator() {
+        return list.iterator();
+    }
+
+    /**
+     * Kopie
+     *
+     * @return
+     */
+    public final List<T> getData() {
+        return ImmutableList.copyOf(list);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isValidIndex(final int index) {
+        return index >= 0 && index < list.size();
+    }
+
+}
