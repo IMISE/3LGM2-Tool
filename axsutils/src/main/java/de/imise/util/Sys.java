d94f96464ed6786294ea1ef820d802fe3ec8a3af
astruebi <a.leipzig@gmx.de>
2017-07-22 00:27:28 +0200

    Sys: komplett umgestellt. Alle Ausgabefunktionen funtionieren jetzt gleich

--- axsutils/src/main/java/de/imise/util/Sys.java
+++ axsutils/src/main/java/de/imise/util/Sys.java
@@ -3,49 +3,59 @@ package de.imise.util;
 public class Sys {
 
     /** Anzahl der StackTrace-Zeilen, die uasgegeben werden sollen */
-    public static int traceSteps = 10;
+    public static int maxTraceSteps = 100;
 
     public static boolean insertBlankLineAfterOutput;
 
-    public static void out(final String message) {
-        System.out.println(message);
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        for (int i = 2; i < 10; i++) {
-            System.out.println(stackTrace[i]);
-        }
-        if (insertBlankLineAfterOutput) {
-            System.out.println();
-        }
+    public static void out1(final Object message) {
+        outInternal(message, 1);
     }
 
-    public static void err(final String message) {
-        System.err.println(message);
+    public static void err1(final Object message) {
+        errInternal(message, 1);
+    }
+
+    public static void out(final Object message) {
+        outInternal(message, maxTraceSteps);
+    }
+
+    public static void err(final Object message) {
+        errInternal(message, maxTraceSteps);
+    }
+
+    public static void out(final Object message, final int maxTraceSteps) {
+        outInternal(message, maxTraceSteps);
+    }
+
+    public static void err(final Object message, final int maxTraceSteps) {
+        errInternal(message, maxTraceSteps);
+    }
+
+    private static void outInternal(final Object message, final int maxTraceSteps) {
+        print(message, maxTraceSteps, false, 4);
+    }
+
+    private static void errInternal(final Object message, final int maxTraceSteps) {
+        print(message, maxTraceSteps, true, 4);
+    }
+
+    private static void print(final Object message, final int maxTraceSteps, final boolean err, final int hideTraceSteps) {
+        println(message, err, false);
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        for (int i = 2; i < 10; i++) {
-            System.err.println(stackTrace[i]);
+        for (int i = hideTraceSteps; i < maxTraceSteps + hideTraceSteps && i < stackTrace.length; i++) {
+            println(stackTrace[i], err, true);
         }
         if (insertBlankLineAfterOutput) {
             System.err.println();
         }
     }
 
-    public static void out1(final String message) {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(message);
-        System.out.print("      ");
-        System.out.println(stackTrace[2]);
-        if (insertBlankLineAfterOutput) {
-            System.out.println();
-        }
-    }
-
-    public static void err1(final String message) {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.err.println(message);
-        System.err.print("      ");
-        System.err.println(stackTrace[2]);
-        if (insertBlankLineAfterOutput) {
-            System.err.println();
+    private static final void println(final Object o, final boolean err, final boolean indent) {
+        String s = indent ? "      " + o.toString() : o.toString();
+        if (err) {
+            System.err.println(s);
+        } else {
+            System.out.println(s);
         }
     }
 
