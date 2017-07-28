e352f2bbcda47456a23d1ee3d11452b36dd6db71
Alexander Strübing <astruebi@regentropfen.org>
2017-07-28 10:25:18 +0200

    Sys: so angepasst, dass print() auch leere Messages, null-Messages und mehrere gleichzeitig ausgeben kann

--- axsutils/src/main/java/de/imise/util/Sys.java
+++ axsutils/src/main/java/de/imise/util/Sys.java
@@ -7,40 +7,50 @@ public class Sys {
 
     public static boolean insertBlankLineAfterOutput;
 
-    public static void out1(final Object message) {
-        outInternal(message, 1);
+    public static void out1(final Object... message) {
+        outInternal(1, message);
     }
 
-    public static void err1(final Object message) {
-        errInternal(message, 1);
+    public static void err1(final Object... message) {
+        errInternal(1, message);
     }
 
-    public static void out(final Object message) {
-        outInternal(message, maxTraceSteps);
+    public static void out(final Object... message) {
+        outInternal(maxTraceSteps, message);
     }
 
-    public static void err(final Object message) {
-        errInternal(message, maxTraceSteps);
+    public static void err(final Object... message) {
+        errInternal(maxTraceSteps, message);
     }
 
-    public static void out(final Object message, final int maxTraceSteps) {
-        outInternal(message, maxTraceSteps);
+    public static void out(final int maxTraceSteps, final Object... message) {
+        outInternal(maxTraceSteps, message);
     }
 
-    public static void err(final Object message, final int maxTraceSteps) {
-        errInternal(message, maxTraceSteps);
+    public static void err(final int maxTraceSteps, final Object... message) {
+        errInternal(maxTraceSteps, message);
     }
 
-    private static void outInternal(final Object message, final int maxTraceSteps) {
-        print(message, maxTraceSteps, false, 4);
+    private static void outInternal(final int maxTraceSteps, final Object... message) {
+        print(maxTraceSteps, false, 4, message);
     }
 
-    private static void errInternal(final Object message, final int maxTraceSteps) {
-        print(message, maxTraceSteps, true, 4);
+    private static void errInternal(final int maxTraceSteps, final Object... message) {
+        print(maxTraceSteps, true, 4, message);
     }
 
-    private static void print(final Object message, final int maxTraceSteps, final boolean err, final int hideTraceSteps) {
-        println(message, err, false);
+    private static void print(final int maxTraceSteps, final boolean err, final int hideTraceSteps, final Object... message) {
+        if (message == null) {
+            println(null, err, false);
+        } else if (message.length == 0) {
+            println("", err, false);
+        } else if (message.length == 1) { //ich denke das ist der häufigste Fall und der sollte ohne init einer for-Schleife gehen -> daher extra
+            println(message[0], err, false);
+        } else {
+            for (int i = 0; i < message.length; i++) {
+                println(message[i], err, false);
+            }
+        }
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
         for (int i = hideTraceSteps; i < maxTraceSteps + hideTraceSteps && i < stackTrace.length; i++) {
             println(stackTrace[i], err, true);
@@ -51,7 +61,7 @@ public class Sys {
     }
 
     private static final void println(final Object o, final boolean err, final boolean indent) {
-        String s = indent ? "      " + o.toString() : o.toString();
+        String s = indent ? "      " + o : String.valueOf(o); // nicht über toString() gehen, weil es null sein kann
         if (err) {
             System.err.println(s);
         } else {
