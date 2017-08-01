1b8481b4947afd1db1d4f50e8c82175ce4ae2489
Alexander Strübing <astruebi@regentropfen.org>
2017-08-01 10:54:01 +0200

    UnfloatableToolBar: aus dem Tool hierher verschoben

---
+++ axsutils/src/main/java/de/imise/util/swing/component/UnfloatableToolBar.java
@@ -0,0 +1,14 @@
+package de.imise.util.swing.component;
+
+import javax.swing.JToolBar;
+
+/**
+ * @author Thomas Rudert
+ *         Abstrakte Klasse für alle Werkzeugleisten, die nicht floatable sein sollen.
+ */
+public abstract class UnfloatableToolBar extends JToolBar {
+
+    public UnfloatableToolBar() {
+        setFloatable(false);
+    }
+}
