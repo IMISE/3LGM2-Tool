84c2412fa48d8b833a5f33d429036d4a74dec6a1
Alexander Strübing <astruebi@regentropfen.org>
2017-09-25 20:35:47 +0200

    Tool3lgmMain: den UI-Manager unter MacOs und Linux (bzw. allem, was nicht Windows ist) auf den systemübergreifenden und nicht mehr den systemspezifischen gestelllt. Das behebt das Problem mit dem langsamen Tool unter MacOS. Grund schreint ein Problem (bzw. Bug) zu sein, der bei InternalFrames auftritt. Hat man viele davon (und das haben wir ganz schnell mal) dann wird das Tool grottenlahm, weil die CPU durch irgendwelche Zeichenroutinen des Systems ausgelastet wird

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgmMain.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgmMain.java
@@ -55,7 +55,11 @@ public class Tool3lgmMain {
             if (System.getProperty("os.name").startsWith("Windows")) {
                 javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
             } else {
-                javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+                //auf dem Mac gibt es mit dem Apple-Glas-Look-And-Feel totale Probleme, wenn man viele InternalFrames
+                //verwendet, was unser Tool tut. Viele beginnt hier bereits bei ca. 5. Da beginnt es schlimm zu werden
+                //und ab 10 friert das Tool immer mal für ne Minute ein.
+                // javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+                javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getCrossPlatformLookAndFeelClassName());
             }
         } catch (Exception ex) {
             Log.show(Log.FATAL, getResString("LookAndFeelLadenException"), ex);
