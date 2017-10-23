1069867b82f6581cb1c0bcb08191333ddb20027d
Alexander Strübing <astruebi@regentropfen.org>
2017-10-23 12:24:00 +0200

    PropertyChangeHandler: firePropertyChange() kann für Enum<?>-Werte aufgerufen werden, deren name()-Funktion aufgerufen wird, um den Property-name zu erzeugen

--- axsutils/src/main/java/de/imise/util/event/PropertyChangeHandler.java
+++ axsutils/src/main/java/de/imise/util/event/PropertyChangeHandler.java
@@ -5,59 +5,73 @@ import java.beans.PropertyChangeSupport;
 
 public abstract class PropertyChangeHandler {
 
-	/** 
-	 * Stellt Property-Change-Funktionalität zur Verfügung. <br>
-	 * Zu der Klasse <code>PropertyChangeSupport</code> werden alle Property-Change-Listener
-	 * hinzugefügt und in <code>firePorpertyChange()</code> werden alle Listener benachrichtigt. 
-	 */
-	protected PropertyChangeSupport changeSupport = null;
+    /**
+     * Stellt Property-Change-Funktionalität zur Verfügung. <br>
+     * Zu der Klasse <code>PropertyChangeSupport</code> werden alle Property-Change-Listener
+     * hinzugefügt und in <code>firePorpertyChange()</code> werden alle Listener benachrichtigt.
+     */
+    protected PropertyChangeSupport changeSupport = null;
 
-	public PropertyChangeHandler() {
-		super();
-	}
+    public PropertyChangeHandler() {
+        super();
+    }
 
-	public PropertyChangeHandler(Object sourceBean) {
-		super();
-		changeSupport = new PropertyChangeSupport(sourceBean);
-	}
+    public PropertyChangeHandler(final Object sourceBean) {
+        super();
+        changeSupport = new PropertyChangeSupport(sourceBean);
+    }
 
-	//Listener hinzufügen/entfernen/benachrichtigen
-	
-	/**
-	 * Fügt einen <code>PropertyChangeListener</code> hinzu
-	 * @param listener
-	 */
-	public void addPropertyChangeListener(PropertyChangeListener listener) {
-		if (changeSupport == null) {
-			changeSupport = new PropertyChangeSupport(this);
-		}
-		changeSupport.addPropertyChangeListener(listener);
-	}
+    //Listener hinzufügen/entfernen/benachrichtigen
 
-	/**
-	 * Entfernt einen <code>PropertyChangeListener</code>
-	 * @param listener
-	 */
-	public void removePropertyChangeListener(PropertyChangeListener listener) {
-		if (changeSupport != null) {
-			changeSupport.removePropertyChangeListener(listener);
-		}
-	}
+    /**
+     * Fügt einen <code>PropertyChangeListener</code> hinzu
+     *
+     * @param listener
+     */
+    public void addPropertyChangeListener(final PropertyChangeListener listener) {
+        if (changeSupport == null) {
+            changeSupport = new PropertyChangeSupport(this);
+        }
+        changeSupport.addPropertyChangeListener(listener);
+    }
 
-	/**
-	 * Sendet an alle <code>PropertyChangeListener</code> ein <code>PropertyChangeEvent</code>
-	 * mit dem Namen <code>propertyName</code>. 
-	 * @param propertyName
-	 * @param oldValue
-	 * @param newValue
-	 */
-	protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
-		if (changeSupport != null)
-			changeSupport.firePropertyChange(propertyName, oldValue, newValue);
-	}
+    /**
+     * Entfernt einen <code>PropertyChangeListener</code>
+     *
+     * @param listener
+     */
+    public void removePropertyChangeListener(final PropertyChangeListener listener) {
+        if (changeSupport != null) {
+            changeSupport.removePropertyChangeListener(listener);
+        }
+    }
 
-	//Ende Listener hinzufügen/entfernen/benachrichtugen
+    /**
+     * Sendet an alle <code>PropertyChangeListener</code> ein <code>PropertyChangeEvent</code>
+     * mit dem Namen <code>propertyName</code>.
+     *
+     * @param propertyName
+     * @param oldValue
+     * @param newValue
+     */
+    protected void firePropertyChange(final String propertyName, final Object oldValue, final Object newValue) {
+        if (changeSupport != null) {
+            changeSupport.firePropertyChange(propertyName, oldValue, newValue);
+        }
+    }
 
+    /**
+     * Sendet an alle <code>PropertyChangeListener</code> ein <code>PropertyChangeEvent</code>
+     * mit dem Namen <code>propertyName</code>.
+     *
+     * @param propertyName
+     * @param oldValue
+     * @param newValue
+     */
+    protected void firePropertyChange(final Enum<?> propertyName, final Object oldValue, final Object newValue) {
+        firePropertyChange(propertyName.name(), oldValue, newValue);
+    }
+
+    //Ende Listener hinzufügen/entfernen/benachrichtugen
 
 }
-
