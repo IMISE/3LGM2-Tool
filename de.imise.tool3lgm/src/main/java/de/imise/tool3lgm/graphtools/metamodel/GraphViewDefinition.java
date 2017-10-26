9d65d8db302141c58dc3a4520b59fcf20669894b
Alexander Strübing <astruebi@regentropfen.org>
2017-10-26 15:57:40 +0200

    GraphViewDefinition: Fehler in der Funktion isPaintable() behoben. Jetzt: eine Kante ist nur dann paintable == true, wenn beide Knoten, die die Kante verbinden kann auch paintable ==  true sind (nicht mehr oder, was einfach falsch war!)

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/GraphViewDefinition.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/GraphViewDefinition.java
@@ -45,7 +45,7 @@ public abstract class GraphViewDefinition {
     public final boolean isPaintable(final Class<? extends ModelElement> elementClass) {
         if (Edge.class.isAssignableFrom(elementClass)) {
             Class<? extends Edge> edgeClass = elementClass.asSubclass(Edge.class);
-            return isPaintable(Edge.getStartClass(edgeClass)) || isPaintable(Edge.getEndClass(edgeClass));
+            return isPaintable(Edge.getStartClass(edgeClass)) && isPaintable(Edge.getEndClass(edgeClass));
         }
         return allPaintableNodes.contains(elementClass);
     }
