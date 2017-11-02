6b6a09626c0fff70bcc0f8310f5393219896720d
Alexander Strübing <astruebi@regentropfen.org>
2017-11-02 10:08:19 +0100

    GrapViewOptionsDialog: nutzt jetzt die GraphViewOptionSliders und hat keine Code-Fehler mehr

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/GraphViewOptionsDialog.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/GraphViewOptionsDialog.java
@@ -5,24 +5,17 @@ import static de.imise.tool3lgm.Tool3lgmConstants.getResString;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.JTextField;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 import de.imise.tool3lgm.Static;
 import de.imise.tool3lgm.graphtools.model.Szenario;
-import de.imise.tool3lgm.graphtools.undoredo.TransactionManager;
-import de.imise.tool3lgm.graphtools.view.graph.InputGraphArea;
-import de.imise.tool3lgm.gui.GraphAreaToolBar;
-import de.imise.tool3lgm.gui.GraphAreaToolbarManager;
+import de.imise.tool3lgm.gui.GraphAreaOptionSliders;
+import de.imise.tool3lgm.gui.GraphAreaOptionSliders.SliderWithTextField;
 import de.imise.tool3lgm.gui.InternalGraphFrame;
 
 /**
@@ -30,37 +23,17 @@ import de.imise.tool3lgm.gui.InternalGraphFrame;
  *
  * @author N.N.
  */
-public class GraphViewOptionsDialog extends JDialog implements ChangeListener, ActionListener {
+public class GraphViewOptionsDialog extends JDialog {
 
-    /**
-     * COMMENTME
-     */
-    private JTextField textzoom, textwinkel, textabstand;
+    private GraphAreaOptionSliders sliders;
 
-    /**
-     * COMMENTME
-     */
-    private JTextField textPageSizeFactor;
+    double pageSizeFactorBackup;
 
-    /**
-     * COMMENTME
-     */
-    private JSlider pageSizeFactorSlider;
+    private final JPanel labelsPanel = createPanel();
 
-    /**
-     * COMMENTME
-     */
-    private double pageSizeFactorBackup = 1d;
+    private final JPanel slidersPanel = createPanel();
 
-    /**
-     * COMMENTME
-     */
-    private InternalGraphFrame frame;
-
-    /**
-     * COMMENTME
-     */
-    private JSlider zoom, winkel, abstand;
+    private final JPanel textFieldsPanel = createPanel();
 
     /**
      *
@@ -69,16 +42,24 @@ public class GraphViewOptionsDialog extends JDialog implements ChangeListener, A
         super(Static.getTool());
     }
 
+    private static final JPanel createPanel() {
+        return new JPanel(new GridLayout(4, 1));
+    }
+
     /**
-     * @param f
+     * @param frame
      */
-    public void showDialog(final InternalGraphFrame f) {
+    public void showDialog(final InternalGraphFrame frame) {
         // Dialog nur für Teilmodelle anzeigen, da nur Teilmodelle einen Grafischen View besitzen
-        if (!(f.getGraphDocument() instanceof Szenario)) {
+        if (!(frame.getGraphDocument() instanceof Szenario)) {
             return;
         }
+        if (sliders == null) {
+            sliders = new GraphAreaOptionSliders(frame);
+        } else {
+            sliders.setFrame(frame);
+        }
 
-        frame = f;
         setTitle(getResString("OPTIONS_GRAPH_VIEW_DIALOG_TITLE"));
         setSize(340, 180);
 
@@ -90,71 +71,26 @@ public class GraphViewOptionsDialog extends JDialog implements ChangeListener, A
         panel.setLayout(bl);
         panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
-        JPanel westPanel = new JPanel();
-        westPanel.setLayout(new GridLayout(4, 1));
-        JPanel centerPanel = new JPanel();
-        centerPanel.setLayout(new GridLayout(4, 1));
-        JPanel eastPanel = new JPanel();
-        eastPanel.setLayout(new GridLayout(4, 1));
-        GraphAreaToolbarManager graphAreaToolBarManager = Static.getTool().getGraphAreaToolBarManager();
-        //graphAreaToolBarManager
-        GraphAreaToolBar leiste = (GraphAreaToolBar) frame.getToolBar();
+        add(sliders.getSliderZoom());
+        add(sliders.getSliderPageSizeFactor());
 
-        JLabel text = new JLabel(getResString("zoom"));
-        zoom = new JSlider(leiste.sliderZoom.getMinimum(), leiste.sliderZoom.getMaximum());
-        zoom.setValue(leiste.sliderZoom.getValue());
-        zoom.addChangeListener(this);
-        textzoom = new JTextField(4);
-        textzoom.setText(new Integer(zoom.getValue()).toString());
-        textzoom.setEditable(false);
-        westPanel.add(text);
-        centerPanel.add(zoom);
-        eastPanel.add(textzoom);
-
-        JLabel textz = new JLabel(getResString("page_zoom"));
-        pageSizeFactorBackup = frame.getGraphDocument().getPageSizeFactor();
-        pageSizeFactorSlider = new JSlider(100, 1000);
-        pageSizeFactorSlider.setValue((int) (pageSizeFactorBackup * 100));
-        pageSizeFactorSlider.addChangeListener(this);
-        textPageSizeFactor = new JTextField(4);
-        textPageSizeFactor.setText(new Double(pageSizeFactorSlider.getValue() / 100d).toString());
-        textPageSizeFactor.setEditable(false);
-        westPanel.add(textz);
-        centerPanel.add(pageSizeFactorSlider);
-        eastPanel.add(textPageSizeFactor);
-
-        JLabel textw = new JLabel(getResString("winkel"));
-        winkel = new JSlider(leiste.sliderDegree.getMinimum(), leiste.sliderDegree.getMaximum());
-        winkel.setValue(leiste.sliderDegree.getValue());
-        winkel.addChangeListener(this);
-        textwinkel = new JTextField(4);
-        JLabel texta = new JLabel(getResString("abstand"));
-        textwinkel.setText(new Integer(winkel.getValue()).toString());
-        textwinkel.setEditable(false);
-
-        abstand = new JSlider(leiste.sliderGap.getMinimum(), leiste.sliderGap.getMaximum());
-        abstand.setValue(leiste.sliderGap.getValue());
-        abstand.addChangeListener(this);
-        textabstand = new JTextField(4);
-        textabstand.setText(new Integer(abstand.getValue()).toString());
-        textabstand.setEditable(false);
         if (frame.getInputGraphArea().isMultiView()) {
-            westPanel.add(textw);
-            centerPanel.add(winkel);
-            eastPanel.add(textwinkel);
-            westPanel.add(texta);
-            centerPanel.add(abstand);
-            eastPanel.add(textabstand);
+            add(sliders.getSliderDegree());
+            add(sliders.getSliderGap());
         }
 
-        panel.add(westPanel, BorderLayout.WEST);
-        panel.add(centerPanel, BorderLayout.CENTER);
-        panel.add(eastPanel, BorderLayout.EAST);
+        panel.add(labelsPanel, BorderLayout.WEST);
+        panel.add(slidersPanel, BorderLayout.CENTER);
+        panel.add(textFieldsPanel, BorderLayout.EAST);
 
         JPanel buttonpanel = new JPanel();
-        JButton b = new JButton(getResString("close"));
-        b.addActionListener(this);
-        buttonpanel.add(b);
+        JButton closeButton = new JButton(new AbstractAction(getResString("close")) {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                dispose();
+            }
+        });
+        buttonpanel.add(closeButton);
 
         getContentPane().add(panel, BorderLayout.CENTER);
         getContentPane().add(buttonpanel, BorderLayout.SOUTH);
@@ -162,42 +98,10 @@ public class GraphViewOptionsDialog extends JDialog implements ChangeListener, A
         setVisible(true);
     }
 
-    @Override
-    public void stateChanged(final ChangeEvent e) {
-        textzoom.setText(new Integer(zoom.getValue()).toString());
-        textwinkel.setText(new Integer(winkel.getValue()).toString());
-        textabstand.setText(new Integer(abstand.getValue()).toString());
-        frame.getGraphDocument().setPageSizeFactor(pageSizeFactorSlider.getValue() / 100d);
-
-        InputGraphArea area = frame.getInputGraphArea();
-        area.setZoom((double) Integer.parseInt(textzoom.getText()) / 100);
-        zoom.setValue(Integer.parseInt(textzoom.getText()));
-        GraphAreaToolBar leiste = (GraphAreaToolBar) frame.getToolBar();
-        leiste.sliderZoom.setValue(Integer.parseInt(textzoom.getText()));
-        if (frame.getInputGraphArea().isMultiView()) {
-            area.setLayerAngle(Integer.parseInt(textwinkel.getText()));
-            area.setLayerGap(Integer.parseInt(textabstand.getText()));
-            winkel.setValue(Integer.parseInt(textwinkel.getText()));
-            abstand.setValue(Integer.parseInt(textabstand.getText()));
-            leiste.sliderDegree.setValue(Integer.parseInt(textwinkel.getText()));
-            leiste.sliderGap.setValue(Integer.parseInt(textabstand.getText()));
-        }
-        textPageSizeFactor.setText(new Double(frame.getGraphDocument().getPageSizeFactor()).toString());
-        leiste.sliderGap.setMaximum(new Double(800 * frame.getGraphDocument().getPageSizeFactor()).intValue());
-        abstand.setMaximum(leiste.sliderGap.getMaximum());
-        // in der Einzelansicht den Viewpoint korrekt aktualisieren
-        if (!area.isMultiView()) {
-            area.setLayerGap(0);
-        }
-        frame.layoutChanged(frame.getGraphDocument());
+    private void add(final SliderWithTextField slider) {
+        labelsPanel.add(slider.getLabel());
+        slidersPanel.add(slider);
+        textFieldsPanel.add(slider.getTextField());
     }
 
-    @Override
-    public void actionPerformed(final ActionEvent e) {
-        String str = e.getActionCommand();
-        if (str.equals(getResString("close"))) {
-            frame.getGraphDocument().setPageSizeFactor(pageSizeFactorBackup, pageSizeFactorSlider.getValue() / 100d, true, TransactionManager.STANDARD_PID);
-            dispose();
-        }
-    }
 }
