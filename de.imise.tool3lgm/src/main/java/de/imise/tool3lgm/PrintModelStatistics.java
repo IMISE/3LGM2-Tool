8ddb11ff7c01bb97bff7a59ccbd5826ae259df6b
Alexander Strübing <astruebi@regentropfen.org>
2017-11-02 12:48:17 +0100

    Tool3LGM: Funktion zur Ausgabe einer Modellstatistik aus Tool3lgm in die neue Klasse PrintModelStatistics verschoben

---
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/PrintModelStatistics.java
@@ -0,0 +1,131 @@
+package de.imise.tool3lgm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.imise.tool3lgm.graphtools.metamodel.Edge;
+import de.imise.tool3lgm.graphtools.metamodel.Knickpunkt;
+import de.imise.tool3lgm.graphtools.metamodel.ModelElement;
+import de.imise.tool3lgm.graphtools.metamodel.Node;
+import de.imise.tool3lgm.graphtools.model.GDCollection;
+import de.imise.tool3lgm.graphtools.model.GraphDocument;
+import de.imise.tool3lgm.graphtools.model.Szenario;
+import de.imise.tool3lgm.graphtools.view.container.ElementContainer;
+import de.imise.tool3lgm.graphtools.view.container.LayerContainer;
+import de.imise.util.Alphabetical;
+
+public class PrintModelStatistics {
+
+    /**
+     * @param gdc
+     *            {@link GDCollection} to print or <code>null</code> if all {@link GDCollection}s should be printed
+     */
+    public static final void printStatistic(final List<GDCollection> collections, final boolean useElements, final boolean alphabetic) {
+        for (GDCollection gdcoll : collections) {
+            GraphDocument mainDoc = gdcoll.getMainGraphDocument();
+
+            List<ElementContainer> allContainer = useElements ? null : mainDoc.getElementContainer(ModelElement.class, true, alphabetic);
+            List<ModelElement> allElements = useElements ? mainDoc.getModelItems(ModelElement.class, true, alphabetic) : null;
+
+            Map<Class<? extends ModelElement>, Integer> class2ElementCount = new HashMap<>();
+            Map<Class<? extends ModelElement>, Integer> class2ContainerCountFromGraphDocuments = new HashMap<>();
+            Map<Class<? extends ModelElement>, Integer> class2ContainerCountFromModelElements = new HashMap<>();
+            //für alle ElementContainer im MainDoc = alle, die es gibt!
+            for (Object ecOrMe : useElements ? allElements : allContainer) {
+                //Anzahl der Modellelemente im Gesamtmodell hochzählen
+                ModelElement me = useElements ? (ModelElement) ecOrMe : ((ElementContainer) ecOrMe).getElement();
+                Class<? extends ModelElement> meClass = me.getClass();
+
+                Integer count = class2ElementCount.get(meClass);
+                count = count == null ? new Integer(1) : new Integer(count.intValue() + 1);
+                class2ElementCount.put(meClass, count);
+
+                List<GraphDocument> docs = new ArrayList<>(gdcoll.getSzenarioCount() + 1);
+                for (Szenario szen : gdcoll.getSzenarios()) {
+                    docs.add(szen);
+                }
+                docs.add(mainDoc);
+
+                //Anzahl der ElementContainer der Modellelemente im Gesamtmodell hochzählen
+                for (GraphDocument doc : docs) {
+                    for (LayerContainer lcc : doc.getLayers()) {
+                        for (ElementContainer layerEc : lcc.getKnickpunkte()) {
+                            if (layerEc.getElement() == me) {
+                                count = class2ContainerCountFromGraphDocuments.get(meClass);
+                                count = count == null ? new Integer(1) : new Integer(count.intValue() + 1);
+                                class2ContainerCountFromGraphDocuments.put(meClass, count);
+                            }
+                        }
+                        for (ElementContainer layerEc : lcc.getKnoten()) {
+                            if (layerEc.getElement() == me) {
+                                count = class2ContainerCountFromGraphDocuments.get(meClass);
+                                count = count == null ? new Integer(1) : new Integer(count.intValue() + 1);
+                                class2ContainerCountFromGraphDocuments.put(meClass, count);
+                            }
+                        }
+                        for (ElementContainer layerEc : lcc.getKanten()) {
+                            if (layerEc.getElement() == me) {
+                                count = class2ContainerCountFromGraphDocuments.get(meClass);
+                                count = count == null ? new Integer(1) : new Integer(count.intValue() + 1);
+                                class2ContainerCountFromGraphDocuments.put(meClass, count);
+                            }
+                        }
+                    }
+                }
+                count = class2ContainerCountFromModelElements.get(meClass);
+                count = count == null ? new Integer(me.getContainerCount()) : new Integer(count.intValue() + me.getContainerCount());
+                class2ContainerCountFromModelElements.put(meClass, count);
+            }
+
+            int nodeCount = 0, nodeContFromDoc = 0, nodeContFromMe = 0;
+            int edgeCount = 0, edgeContFromDoc = 0, edgeContFromMe = 0;
+            int bendCount = 0, bendContFromDoc = 0, bendContFromMe = 0;
+
+            System.err.println("Modellstatistik: " + gdcoll.getName());
+            System.err.println("-------------------------------------");
+            List<Class<? extends ModelElement>> classList = new ArrayList<>(class2ElementCount.keySet());
+            Alphabetical.sort(classList);
+            //für jede Elementklasse
+            for (Class<? extends ModelElement> elementClass : classList) {
+                //              Integer integer = class2ElementCount.get(elementClass);
+                //              int count = integer == null ? 0 : integer.intValue();
+                //              integer = class2ContainerCountFromGraphDocuments.get(elementClass);
+                //              int contFromDoc = integer == null ? 0 : integer.intValue();
+                //              integer = class2ContainerCountFromModelElements.get(elementClass);
+                //              int contFromMe = integer == null ? 0 : integer.intValue();
+
+                int count = class2ElementCount.get(elementClass).intValue();
+                int contFromDoc = class2ContainerCountFromGraphDocuments.get(elementClass).intValue();
+                int contFromMe = class2ContainerCountFromModelElements.get(elementClass).intValue();
+
+                System.err.print(elementClass.getName() + ": " + count + " " + contFromDoc + " " + contFromMe);
+                if (contFromMe != contFromDoc) {
+                    System.err.println(" <----------");
+                } else {
+                    System.err.println();
+                }
+                if (Knickpunkt.class.isAssignableFrom(elementClass)) {
+                    bendCount += count;
+                    bendContFromDoc += contFromDoc;
+                    bendContFromMe += contFromMe;
+                } else if (Node.class.isAssignableFrom(elementClass)) {
+                    nodeCount += count;
+                    nodeContFromDoc += contFromDoc;
+                    nodeContFromMe += contFromMe;
+                } else if (Edge.class.isAssignableFrom(elementClass)) {
+                    edgeCount += count;
+                    edgeContFromDoc += contFromDoc;
+                    edgeContFromMe += contFromMe;
+                }
+            }
+            System.err.println("Node      " + nodeCount + " " + nodeContFromDoc + " " + nodeContFromMe);
+            System.err.println("Kanten      " + edgeCount + " " + edgeContFromDoc + " " + edgeContFromMe);
+            System.err.println("Knickpunkte " + bendCount + " " + bendContFromDoc + " " + bendContFromMe);
+
+            System.err.println();
+        }
+    }
+
+}
--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgm.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgm.java
@@ -21,9 +21,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.help.CSH;
 import javax.swing.JDesktopPane;
@@ -46,8 +44,6 @@ import de.imise.tool3lgm.graphtools.consistency.ModelCleaner;
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
 import de.imise.tool3lgm.graphtools.dialog.RMIErrorPanel;
 import de.imise.tool3lgm.graphtools.matrixview.MatrixViewInternalFrame;
-import de.imise.tool3lgm.graphtools.metamodel.Edge;
-import de.imise.tool3lgm.graphtools.metamodel.Knickpunkt;
 import de.imise.tool3lgm.graphtools.metamodel.ModelConstants;
 import de.imise.tool3lgm.graphtools.metamodel.ModelElement;
 import de.imise.tool3lgm.graphtools.metamodel.Node;
@@ -61,7 +57,6 @@ import de.imise.tool3lgm.graphtools.undoredo.TransactionManager;
 import de.imise.tool3lgm.graphtools.userfield.UserfieldResourceHandler;
 import de.imise.tool3lgm.graphtools.view.browser.ModelBrowserPanel;
 import de.imise.tool3lgm.graphtools.view.container.ElementContainer;
-import de.imise.tool3lgm.graphtools.view.container.LayerContainer;
 import de.imise.tool3lgm.graphtools.view.graph.InputGraphArea;
 import de.imise.tool3lgm.graphtools.view.graph.ViewParameter;
 import de.imise.tool3lgm.gui.AbstractInternalFrame;
@@ -77,7 +72,6 @@ import de.imise.tool3lgm.rmi.Tool3lgmServer;
 import de.imise.tool3lgm.rmi.Tool3lgmServerImpl;
 import de.imise.tool3lgm.tools.BrowseUtils;
 import de.imise.tool3lgm.userproperties.UserProperties;
-import de.imise.util.Alphabetical;
 import de.imise.util.swing.dialog.ExtendedFileChooser;
 
 /** Hauptklasse der Anwendung 3lgm */
@@ -530,121 +524,6 @@ public class Tool3lgm extends JFrame implements WindowListener, InternalFrameLis
         //		System.err.println("###########################################################################");
 
         return true;
-    }
-
-    /**
-     * @param gdc
-     *            {@link GDCollection} to print or <code>null</code> if all {@link GDCollection}s should be printed
-     */
-    public final void printStatistic(final GDCollection gdc, final boolean useElements, final boolean alphabetic) {
-        List<GDCollection> collections = this.collections;
-        if (gdc != null) {
-            collections = new ArrayList<>();
-            collections.add(gdc);
-        }
-        for (GDCollection gdcoll : collections) {
-            GraphDocument mainDoc = gdcoll.getMainGraphDocument();
-
-            List<ElementContainer> allContainer = useElements ? null : mainDoc.getElementContainer(ModelElement.class, true, alphabetic);
-            List<ModelElement> allElements = useElements ? mainDoc.getModelItems(ModelElement.class, true, alphabetic) : null;
-
-            Map<Class<? extends ModelElement>, Integer> class2ElementCount = new HashMap<>();
-            Map<Class<? extends ModelElement>, Integer> class2ContainerCountFromGraphDocuments = new HashMap<>();
-            Map<Class<? extends ModelElement>, Integer> class2ContainerCountFromModelElements = new HashMap<>();
-            //für alle ElementContainer im MainDoc = alle, die es gibt!
-            for (Object ecOrMe : useElements ? allElements : allContainer) {
-                //Anzahl der Modellelemente im Gesamtmodell hochzählen
-                ModelElement me = useElements ? (ModelElement) ecOrMe : ((ElementContainer) ecOrMe).getElement();
-                Class<? extends ModelElement> meClass = me.getClass();
-
-                Integer count = class2ElementCount.get(meClass);
-                count = count == null ? new Integer(1) : new Integer(count.intValue() + 1);
-                class2ElementCount.put(meClass, count);
-
-                List<GraphDocument> docs = new ArrayList<>(gdcoll.getSzenarioCount() + 1);
-                for (Szenario szen : gdcoll.getSzenarios()) {
-                    docs.add(szen);
-                }
-                docs.add(mainDoc);
-
-                //Anzahl der ElementContainer der Modellelemente im Gesamtmodell hochzählen
-                for (GraphDocument doc : docs) {
-                    for (LayerContainer lcc : doc.getLayers()) {
-                        for (ElementContainer layerEc : lcc.getKnickpunkte()) {
-                            if (layerEc.getElement() == me) {
-                                count = class2ContainerCountFromGraphDocuments.get(meClass);
-                                count = count == null ? new Integer(1) : new Integer(count.intValue() + 1);
-                                class2ContainerCountFromGraphDocuments.put(meClass, count);
-                            }
-                        }
-                        for (ElementContainer layerEc : lcc.getKnoten()) {
-                            if (layerEc.getElement() == me) {
-                                count = class2ContainerCountFromGraphDocuments.get(meClass);
-                                count = count == null ? new Integer(1) : new Integer(count.intValue() + 1);
-                                class2ContainerCountFromGraphDocuments.put(meClass, count);
-                            }
-                        }
-                        for (ElementContainer layerEc : lcc.getKanten()) {
-                            if (layerEc.getElement() == me) {
-                                count = class2ContainerCountFromGraphDocuments.get(meClass);
-                                count = count == null ? new Integer(1) : new Integer(count.intValue() + 1);
-                                class2ContainerCountFromGraphDocuments.put(meClass, count);
-                            }
-                        }
-                    }
-                }
-                count = class2ContainerCountFromModelElements.get(meClass);
-                count = count == null ? new Integer(me.getContainerCount()) : new Integer(count.intValue() + me.getContainerCount());
-                class2ContainerCountFromModelElements.put(meClass, count);
-            }
-
-            int nodeCount = 0, nodeContFromDoc = 0, nodeContFromMe = 0;
-            int edgeCount = 0, edgeContFromDoc = 0, edgeContFromMe = 0;
-            int bendCount = 0, bendContFromDoc = 0, bendContFromMe = 0;
-
-            System.err.println("Modellstatistik: " + gdcoll.getName());
-            System.err.println("-------------------------------------");
-            List<Class<? extends ModelElement>> classList = new ArrayList<>(class2ElementCount.keySet());
-            Alphabetical.sort(classList);
-            //für jede Elementklasse
-            for (Class<? extends ModelElement> elementClass : classList) {
-                //				Integer integer = class2ElementCount.get(elementClass);
-                //				int count = integer == null ? 0 : integer.intValue();
-                //				integer = class2ContainerCountFromGraphDocuments.get(elementClass);
-                //				int contFromDoc = integer == null ? 0 : integer.intValue();
-                //				integer = class2ContainerCountFromModelElements.get(elementClass);
-                //				int contFromMe = integer == null ? 0 : integer.intValue();
-
-                int count = class2ElementCount.get(elementClass).intValue();
-                int contFromDoc = class2ContainerCountFromGraphDocuments.get(elementClass).intValue();
-                int contFromMe = class2ContainerCountFromModelElements.get(elementClass).intValue();
-
-                System.err.print(elementClass.getName() + ": " + count + " " + contFromDoc + " " + contFromMe);
-                if (contFromMe != contFromDoc) {
-                    System.err.println(" <----------");
-                } else {
-                    System.err.println();
-                }
-                if (Knickpunkt.class.isAssignableFrom(elementClass)) {
-                    bendCount += count;
-                    bendContFromDoc += contFromDoc;
-                    bendContFromMe += contFromMe;
-                } else if (Node.class.isAssignableFrom(elementClass)) {
-                    nodeCount += count;
-                    nodeContFromDoc += contFromDoc;
-                    nodeContFromMe += contFromMe;
-                } else if (Edge.class.isAssignableFrom(elementClass)) {
-                    edgeCount += count;
-                    edgeContFromDoc += contFromDoc;
-                    edgeContFromMe += contFromMe;
-                }
-            }
-            System.err.println("Node      " + nodeCount + " " + nodeContFromDoc + " " + nodeContFromMe);
-            System.err.println("Kanten      " + edgeCount + " " + edgeContFromDoc + " " + edgeContFromMe);
-            System.err.println("Knickpunkte " + bendCount + " " + bendContFromDoc + " " + bendContFromMe);
-
-            System.err.println();
-        }
     }
 
     /**
