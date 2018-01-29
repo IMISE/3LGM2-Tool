38c64b981d99934b9be0696a586693f3a983db70
Alexander Strübing <astruebi@regentropfen.org>
2018-01-29 19:36:15 +0100

    ApplicationComponent_PhysicalDataProcessingComponent_Edge: neue abstracte Oberklasse für die beiden Kanten zwischen ApplicationSystem und PhysicalDataProcessingComponent. Diese Kante wird genraucht, um in der GraphViewDefinition den Pfad für die zu zeichnenden Konfigurationen anzugeben

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/edge/ApplicationComponent_PhysicalDataProcessingComponent_RequiresForStorage_Edge.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/edge/ApplicationComponent_PhysicalDataProcessingComponent_RequiresForStorage_Edge.java
@@ -1,24 +1,8 @@
 package de.imise.tool3lgm.metamodel.tlgm_service.edge;
 
-import static de.imise.tool3lgm.graphtools.metamodel.EdgeCardinality.ZERO_UNIMITED;
-
-import de.imise.tool3lgm.graphtools.metamodel.EdgeCardinality;
-import de.imise.tool3lgm.graphtools.metamodel.elements.Edge;
-import de.imise.tool3lgm.graphtools.metamodel.elements.ModelElement;
-import de.imise.tool3lgm.metamodel.tlgm_service.node.ApplicationComponent;
-import de.imise.tool3lgm.metamodel.tlgm_service.node.PhysicalDataProcessingComponent;
-
 /**
  * @author AXS (12.01.2018)
  */
-public final class ApplicationComponent_PhysicalDataProcessingComponent_RequiresForStorage_Edge extends Edge {
-
-    public static final Class<? extends ModelElement> stcl = ApplicationComponent.class;
-
-    public static final EdgeCardinality scard = ZERO_UNIMITED;
-
-    public static final EdgeCardinality ecard = ZERO_UNIMITED;
-
-    public static final Class<? extends ModelElement> etcl = PhysicalDataProcessingComponent.class;
+public final class ApplicationComponent_PhysicalDataProcessingComponent_RequiresForStorage_Edge extends ApplicationComponent_PhysicalDataProcessingComponent_Edge {
 
 }
