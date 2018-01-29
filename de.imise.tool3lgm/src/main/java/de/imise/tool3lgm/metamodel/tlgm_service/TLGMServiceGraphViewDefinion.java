6419dfc654c5dcbf9033fe78aca995fa3a56d484
Alexander Strübing <astruebi@regentropfen.org>
2018-01-29 22:25:56 +0100

    TLGMServiceMetaModell + TLGMServiceGraphViewDefinition + deutsche Metamodell-Resourcen hinzugefügt

---
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/TLGMServiceGraphViewDefinion.java
@@ -0,0 +1,74 @@
+package de.imise.tool3lgm.metamodel.tlgm_service;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import de.imise.tool3lgm.graphtools.metamodel.GraphViewDefinition;
+import de.imise.tool3lgm.graphtools.metamodel.elements.Edge;
+import de.imise.tool3lgm.graphtools.metamodel.elements.ModelElement;
+import de.imise.tool3lgm.graphtools.path.MetaPath;
+import de.imise.tool3lgm.graphtools.view.graph.GraphElementLayout;
+import de.imise.tool3lgm.graphtools.view.graph.GraphElementLayout.SHAPE;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationComponent_PhysicalDataProcessingComponent_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.CommunicationLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Function_Use_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.SupportLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.ApplicationComponent;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.ApplicationSystem;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.CommunicationInterface;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.Function;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.InvokingInterface;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.ObjectType;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.OrganisationSystem;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.PhysicalDataProcessingComponent;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.ProvidingInterface;
+import de.imise.util.Pair;
+
+public class TLGMServiceGraphViewDefinion extends GraphViewDefinition {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected final Class[] getPaintableNodes() {
+        //diese Funtkion wird nur ein einziges Mal aufgerufen, daher ist es ok,
+        //dass das Array hier in der Funktion immer wieder neu angelegt wird
+        Class[] graphViewVisibleNodes = {
+                Function.class,
+                ObjectType.class,
+                ApplicationSystem.class,
+                OrganisationSystem.class,
+                InvokingInterface.class,
+                ProvidingInterface.class,
+                PhysicalDataProcessingComponent.class,
+        };
+        return graphViewVisibleNodes;
+    }
+
+    @Override
+    protected final MetaPath[] getConfigurationPaths() {
+        MetaPath[] configurationPaths = {
+                //Testpfad über alle Ebenen hinweg
+                //new MetaPath(Aufgabe.class, PhysischerDVBaustein.class, AufAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class, PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class),
+                new MetaPath(Function.class, ApplicationComponent.class, Function_Use_Edge.class, SupportLink_Edge.class),
+                new MetaPath(ApplicationComponent.class, PhysicalDataProcessingComponent.class, ApplicationComponent_PhysicalDataProcessingComponent_Edge.class),
+        };
+        return configurationPaths;
+    }
+
+    @Override
+    public List<Pair<Class<? extends ModelElement>, Class<? extends Edge>>> getHidableIfNotConnected() {
+        return ImmutableList.of(new Pair<>(CommunicationInterface.class, CommunicationLink_Edge.class));
+    }
+
+    @Override
+    protected void initDefaultElementLayout() {
+        setDefaultLayout(Function.class, SHAPE.rechteck, GraphElementLayout.COLORS[GraphElementLayout.RED]);
+        setDefaultLayout(ObjectType.class, GraphElementLayout.SHAPE.oval, GraphElementLayout.COLORS[GraphElementLayout.BLUE]);
+        setDefaultLayout(ApplicationSystem.class, GraphElementLayout.SHAPE.rundeck, GraphElementLayout.COLORS[GraphElementLayout.LIGHTRED]);
+        setDefaultLayout(OrganisationSystem.class, GraphElementLayout.SHAPE.rundeck, GraphElementLayout.COLORS[GraphElementLayout.BLUE]);
+        setDefaultLayout(InvokingInterface.class, GraphElementLayout.SHAPE.dreieck, GraphElementLayout.COLORS[GraphElementLayout.LIGHTGREEN], 15, 15);
+        setDefaultLayout(ProvidingInterface.class, GraphElementLayout.SHAPE.dreieck, GraphElementLayout.COLORS[GraphElementLayout.ORANGE], 15, 15);
+        setDefaultLayout(PhysicalDataProcessingComponent.class, GraphElementLayout.SHAPE.rechteck, GraphElementLayout.COLORS[GraphElementLayout.ORANGE]);
+    }
+
+}
---
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/TLGMServiceMetaModel.java
@@ -0,0 +1,373 @@
+package de.imise.tool3lgm.metamodel.tlgm_service;
+
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import de.imise.tool3lgm.graphtools.metamodel.AnalysisDefinition;
+import de.imise.tool3lgm.graphtools.metamodel.CopyDependencies;
+import de.imise.tool3lgm.graphtools.metamodel.ExtrasActionsDefinition;
+import de.imise.tool3lgm.graphtools.metamodel.GraphViewDefinition;
+import de.imise.tool3lgm.graphtools.metamodel.MetaModel;
+import de.imise.tool3lgm.graphtools.metamodel.PathsDefinition;
+import de.imise.tool3lgm.graphtools.metamodel.elements.Edge;
+import de.imise.tool3lgm.graphtools.metamodel.elements.ModelElement;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationComponent_CommunicationInterface_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationComponent_CommunicationLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationComponent_IsPartOfEdge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationComponent_PhysicalDataProcessingComponent_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationComponent_PhysicalDataProcessingComponent_RequiresForFunctionality_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationComponent_PhysicalDataProcessingComponent_RequiresForStorage_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationComponent_SupportLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationSystem_SoftwareProduct_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.CommunicationLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.DataTransmissionLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.DeviceClass_IsPartOfEdge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Function_IsPartOfEdge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Function_ObjectType_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Function_SoftwareProduct_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Function_Use_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Location_IsPartOfEdge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ObjectType_IsPartOfEdge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.OrganisationalUnit_IsPartOfEdge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.OrganisationalUnit_SupportLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.OrganisationalUnit_Use_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.PhysicalDataProcessingComponentVirtualises_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.PhysicalDataProcessingComponent_DeviceClass_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.PhysicalDataProcessingComponent_IsPartOfEdge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.PhysicalDataProcessingComponent_Location_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Process_Function_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ServiceUses_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Service_CommunicationLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Service_Function_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Service_InvokingInterface_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Service_ObjectType_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Service_ProvidingInterface_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.Service_ServiceClass_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.StorageLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.SupportLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.TransmissionMedium_DataTransmissionLink_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.ApplicationComponent;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.ApplicationSystem;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.CommunicationInterface;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.DeviceClass;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.Function;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.InvokingInterface;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.Location;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.ObjectType;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.OrganisationSystem;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.OrganisationalUnit;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.PhysicalDataProcessingComponent;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.ProvidingInterface;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.Service;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.ServiceClass;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.SoftwareProduct;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.SystemOfConcepts;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.TransmissionMedium;
+import de.imise.tool3lgm.metamodel.tlgm_service.node.Use;
+import de.imise.tool3lgm.metamodel.tlgm_v3_0.edge.PdvbVirtualPdvbVerbindung;
+
+@SuppressWarnings({
+        "unchecked",
+        "rawtypes"
+})
+public class TLGMServiceMetaModel extends MetaModel {
+
+    private final ImmutableMap<String, String> oldToNewName = ImmutableMap.of();
+
+    @Override
+    protected final Map<String, String> getOldToNewClassName() {
+        return oldToNewName;
+    }
+
+    /////////////////////
+    // PathsDefinition //
+    /////////////////////
+
+    @Override
+    public PathsDefinition createPathsDefinition() {
+        //im Moment hat dieses Metamodell keine eigene Pfaddefinition. Man könnte diese Funktion auch weglassen.
+        return super.createPathsDefinition();
+    }
+
+    /////////////////////////
+    // GraphViewDefinition //
+    /////////////////////////
+
+    @Override
+    public final GraphViewDefinition createGraphViewDefinition() {
+        return new TLGMServiceGraphViewDefinion();
+    }
+
+    //////////////////////
+    // CopyDependencies //
+    //////////////////////
+
+    @Override
+    public final CopyDependencies createCopyDependencies() {
+        return new CopyDependencies();
+    }
+
+    ////////////////////////
+    // AnalysisDefinition //
+    ////////////////////////
+
+    @Override
+    protected Class<? extends AnalysisDefinition> getAnalysisDefinitionClass() {
+        return null;
+    }
+
+    /////////////////////////////
+    // ExtrasActionsDefinition //
+    /////////////////////////////
+
+    @Override
+    protected Class<? extends ExtrasActionsDefinition> getExtrasActionsDefinitionClass() {
+        return null;
+    }
+
+    ////////////
+    // Node //
+    ////////////
+
+    /** Alle Node der FE als Array */
+    private static final Class[] ALL_DOMAIN_LAYER_NODES = {
+            Function.class,
+            ObjectType.class,
+            OrganisationalUnit.class,
+            Use.class,
+            SystemOfConcepts.class,
+            de.imise.tool3lgm.metamodel.tlgm_service.node.Process.class, // es gibt im lang-package ebenfalls eine Klasse Process
+    };
+
+    /** Alle Node zw. FE und LWE als Array */
+    private final Class[] ALL_INTER_DOMAIN_LOGICAL_LAYER_NODES = {
+            //auch die Assoziationsklasse hier eintagen
+            SupportLink_Edge.class, StorageLink_Edge.class,
+    };
+
+    /** Alle Node der LWE als Array */
+    private final Class[] ALL_LOGICAL_LAYER_NODES = {
+            ApplicationSystem.class,
+            OrganisationSystem.class,
+            SoftwareProduct.class,
+            InvokingInterface.class,
+            ProvidingInterface.class,
+            Service.class,
+            ServiceClass.class,
+
+            //auch die Assoziationsklasse hier eintagen
+            CommunicationLink_Edge.class,
+
+            //abstracte Node müssen hier auch eingetragen werden
+            ApplicationComponent.class,
+            CommunicationInterface.class,
+    };
+
+    /** Alle Node zw. LWE und PWE als Array */
+    private final Class[] ALL_INTER_LOGICAL_PHYSICAL_LAYER_NODES = {};
+
+    /** Alle Node der PWE als Array */
+    private final Class[] ALL_PHYSICAL_LAYER_NODES = {
+            PhysicalDataProcessingComponent.class,
+            DeviceClass.class,
+            Location.class,
+            DataTransmissionLink_Edge.class,
+            TransmissionMedium.class,
+    };
+
+    @Override
+    public final Class[] getAllDomainLayerNodes() {
+        return ALL_DOMAIN_LAYER_NODES;
+    }
+
+    @Override
+    public final Class[] getAllInterDomainLogicalLayerNodes() {
+        return ALL_INTER_DOMAIN_LOGICAL_LAYER_NODES;
+    }
+
+    @Override
+    public final Class[] getAllLogicalLayerNodes() {
+        return ALL_LOGICAL_LAYER_NODES;
+    }
+
+    @Override
+    public final Class[] getAllInterLogicalPhysicalLayerNodes() {
+        return ALL_INTER_LOGICAL_PHYSICAL_LAYER_NODES;
+    }
+
+    @Override
+    public final Class[] getAllPhysicalLayerNodes() {
+        return ALL_PHYSICAL_LAYER_NODES;
+    }
+
+    @Override
+    public Class[] getTreeLogicalLayerVisibleAbstractNodes() {
+        return new Class[] {
+                //nur bei Anwendungsbausteinen soll die abstrakte Oberklasse im Baum angezeigt werden
+                ApplicationComponent.class
+        };
+    }
+
+    ////////////
+    // Kanten //
+    ////////////
+
+    /** Kanten FE */
+    private final Class[] ALL_DOMAIN_LAYER_EDGES = {
+            Function_IsPartOfEdge.class,
+            Function_ObjectType_Edge.class,
+            Function_Use_Edge.class,
+            ObjectType_IsPartOfEdge.class,
+            OrganisationalUnit_IsPartOfEdge.class,
+            OrganisationalUnit_Use_Edge.class,
+            Process_Function_Edge.class,
+
+    };
+
+    /** Kanten Inter FE -LWE */
+    private final Class[] ALL_INTER_DOMAIN_LOGICAL_LAYER_EDGES = {
+            ApplicationComponent_SupportLink_Edge.class,
+            Function_SoftwareProduct_Edge.class,
+            OrganisationalUnit_SupportLink_Edge.class,
+            Service_Function_Edge.class,
+            Service_ObjectType_Edge.class,
+            StorageLink_Edge.class,
+            SupportLink_Edge.class
+    };
+
+    /** Kanten LWE */
+    private final Class[] ALL_LOGICAL_LAYER_EDGES = {
+            ApplicationComponent_CommunicationInterface_Edge.class,
+            ApplicationComponent_CommunicationLink_Edge.class,
+            ApplicationComponent_IsPartOfEdge.class,
+            ApplicationSystem_SoftwareProduct_Edge.class,
+            CommunicationLink_Edge.class,
+            Service_CommunicationLink_Edge.class,
+            Service_InvokingInterface_Edge.class,
+            Service_ProvidingInterface_Edge.class,
+            Service_ServiceClass_Edge.class,
+            ServiceUses_Edge.class,
+    };
+
+    /** Kanten Inter LWE - PWE */
+    private final Class[] ALL_INTER_LOGICAL_PHYSICAL_LAYER_EDGES = {
+            ApplicationComponent_PhysicalDataProcessingComponent_Edge.class,
+            ApplicationComponent_PhysicalDataProcessingComponent_RequiresForFunctionality_Edge.class,
+            ApplicationComponent_PhysicalDataProcessingComponent_RequiresForStorage_Edge.class,
+    };
+
+    /** Kanten PWE */
+    private final Class[] ALL_PHYSICAL_LAYER_EDGES = {
+            DataTransmissionLink_Edge.class,
+            DeviceClass_IsPartOfEdge.class,
+            Location_IsPartOfEdge.class,
+            PhysicalDataProcessingComponent_DeviceClass_Edge.class,
+            PhysicalDataProcessingComponent_IsPartOfEdge.class,
+            PdvbVirtualPdvbVerbindung.class,
+            PhysicalDataProcessingComponent_Location_Edge.class,
+            PhysicalDataProcessingComponentVirtualises_Edge.class,
+            TransmissionMedium_DataTransmissionLink_Edge.class,
+    };
+
+    @Override
+    public Class[] getAllDomainLayerEdges() {
+        return ALL_DOMAIN_LAYER_EDGES;
+    }
+
+    @Override
+    public Class[] getAllInterDomainLogicalLayerEdges() {
+        return ALL_INTER_DOMAIN_LOGICAL_LAYER_EDGES;
+    }
+
+    @Override
+    public Class[] getAllLogicalLayerEdges() {
+        return ALL_LOGICAL_LAYER_EDGES;
+    }
+
+    @Override
+    public Class[] getAllInterLogicalPhysicalLayerEdges() {
+        return ALL_INTER_LOGICAL_PHYSICAL_LAYER_EDGES;
+    }
+
+    @Override
+    public Class[] getAllPhysicalLayerEdges() {
+        return ALL_PHYSICAL_LAYER_EDGES;
+    }
+
+    ///////////////////////////////////
+    // spezielle Knoteneigenschaften //
+    ///////////////////////////////////
+
+    private final Class[] IMPORTABLE_NODES = {
+            //            Aufgabe.class,
+            //            Bausteintyp.class,
+            //            DBVerwaltungssystem.class,
+            //            Dokumententyp.class,
+            //            Ereignistyp.class,
+            //            KommBeziehung.class,
+            //            Kommunikationsstandard.class,
+            //            Nachrichtentyp.class,
+            //            Netzprotokoll.class,
+            //            Netztyp.class,
+            //            Objekttyp.class,
+            //            Organisationseinheit.class,
+            //            Organisationsplan.class,
+            //            KonAnwendungsbaustein.class,
+            //            PhysischerDVBaustein.class,
+            //            RechAnwendungsbaustein.class,
+            //            Softwareprodukt.class,
+            //            Standort.class,
+            //            Subnetz.class,
+    };
+
+    /** Alle Klassen, die man über den Datenimport einlesen kann */
+    @Override
+    public final Class<? extends ModelElement>[] getImportableNodes() {
+        return IMPORTABLE_NODES;
+    }
+
+    ///////////////////////////////////
+    // spezielle Kanteneigenschaften //
+    ///////////////////////////////////
+
+    /**
+     * Liste aller Kantenklassen, die auch 2 gerichtete Assoziationen im Metamodell sein könnten, aber in eine Assoziation verpackt wurden,
+     * bei denen die Richtung der Edge (Doppelkante.FORWARD, Doppelkante.BACKWARD, Doppelkante.DOUBLE) die Bedeutung angibt. Wegen dieser
+     * Kanten braucht man den ganzen Kanten-Richtungsmechanismus. Wenn sie grafisch dargestellt werden, dann werden sie als eine Edge dargestellt,die
+     * je nach Bedeutung eine der Richtungen oder beide als Pfeile darstellt.
+     */
+    @Override
+    public final Set<Class<? extends Edge>> getDoubleMeaningEdgeClasses() {
+        return ImmutableSet.<Class<? extends Edge>> of(
+                //vorwärts1: bearbeitet; rückwärts1: wird bearbeitet von
+                //vorwärts2: interpretiert; rückwärts2: wird interpretiert von
+                Function_ObjectType_Edge.class,
+                //vorwärts1: hat als Ergebnis; rückwärts1: ist Ergebnis von
+                //vorwärts2: hat als Parameter; rückwärts2: ist Parameter von
+                Service_ObjectType_Edge.class);
+    }
+
+    /**
+     * Menge aller Kantenklassen, die nur in Vorwärtsrichtung verbunden werden und somit immer nur in dieser Richtung in
+     * der Grafik dargestelt werden.
+     */
+    @Override
+    public final Set<Class<? extends Edge>> getForwardConnectedEdgeClasses() {
+        return ImmutableSet.of(); //im Moment keine eingetragen
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // Maps von Elementklassen auf Sets von Elementklassen (und mehr)//
+    ///////////////////////////////////////////////////////////////////
+
+    private final Set<Class<? extends ModelElement>> GENERATE_NAME_CLASSES = ImmutableSet.<Class<? extends ModelElement>> of();
+
+    @Override
+    public Set<Class<? extends ModelElement>> getGenerateNameClasses() {
+        return GENERATE_NAME_CLASSES;
+    }
+
+}
