cd2119ee655a1ab63436d44c9dc28d420a6d53d3
Alexander Strübing <astruebi@regentropfen.org>
2018-01-31 22:04:25 +0100

    Panel für die Tarnsaktionsbeziehungen zwischen IheActors hinzugefüht

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheActorOfIntegrationProfile.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheActorOfIntegrationProfile.java
@@ -3,6 +3,7 @@ package de.imise.tool3lgm.metamodel.tlgm_service.node;
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheActorOfIntegrationProfile_ApplicationSystem_Edge;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheIntegrationProfile_IheActorOfIntegrationProfile_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheTransactionLink_Edge;
 
 /**
  * @author AXS (31.01.2018)
@@ -14,6 +15,7 @@ public class IheActorOfIntegrationProfile extends IheConcept {
         ElementPropertyDialog dialog = super.getPropertyDialog();
         dialog.addDescripSingleConnectionPanel(IheIntegrationProfile_IheActorOfIntegrationProfile_Edge.class);
         dialog.addEdgePanel(IheActorOfIntegrationProfile_ApplicationSystem_Edge.class);
+        dialog.addEdgePanel(IheTransactionLink_Edge.class);
         return dialog;
     }
 
