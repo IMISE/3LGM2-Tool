752f01ab69f3783942cd8d58667938984d1eb226
Alexander Strübing <astruebi@regentropfen.org>
2018-02-15 12:02:25 +0100

    Textfeld: die Kante zur zuordnung, zu welchem Element dieses Textfeld gehört in den PropertyDialog des Textfeldes eingetragen

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/Textfeld.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/Textfeld.java
@@ -1,5 +1,14 @@
 package de.imise.tool3lgm.graphtools.metamodel.elements;
 
+import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
+
 public class Textfeld extends Node {
+
+    @Override
+    public ElementPropertyDialog createPropertyDialog() {
+        ElementPropertyDialog dialog = super.createPropertyDialog();
+        dialog.addDescripSingleConnectionPanel(true, Textfield_ModelElement_Edge.class);
+        return dialog;
+    }
 
 }
