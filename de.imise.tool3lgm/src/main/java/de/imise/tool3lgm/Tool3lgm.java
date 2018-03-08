5b81570c309304beb4e428580650e28b0933a717
Alexander Strübing <astruebi@regentropfen.org>
2018-03-08 15:05:10 +0100

    Tool3lgm und Tool3lgmMain: RMI-Startroutine aus Tool3lgm in Tool3lgmMain verlegt -> übersichtlicher

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgm.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgm.java
@@ -1,6 +1,5 @@
 package de.imise.tool3lgm;
 
-import static de.imise.tool3lgm.Static.tool;
 import static de.imise.tool3lgm.Tool3lgmConstants.getResString;
 
 import java.awt.BorderLayout;
@@ -16,11 +15,6 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyVetoException;
 import java.io.File;
-import java.rmi.Naming;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,7 +37,6 @@ import de.imise.tool3lgm.Tool3lgmConstants.FileFilterType;
 import de.imise.tool3lgm.graphtools.consistency.ConsistencyChecker;
 import de.imise.tool3lgm.graphtools.consistency.ModelCleaner;
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
-import de.imise.tool3lgm.graphtools.dialog.RMIErrorPanel;
 import de.imise.tool3lgm.graphtools.matrixview.MatrixViewInternalFrame;
 import de.imise.tool3lgm.graphtools.metamodel.ModelConstants;
 import de.imise.tool3lgm.graphtools.metamodel.elements.ModelElement;
@@ -70,8 +63,6 @@ import de.imise.tool3lgm.gui.menu.ContextGenerator;
 import de.imise.tool3lgm.gui.menu.MenuBar;
 import de.imise.tool3lgm.help.Help;
 import de.imise.tool3lgm.log.Log;
-import de.imise.tool3lgm.rmi.Tool3lgmServer;
-import de.imise.tool3lgm.rmi.Tool3lgmServerImpl;
 import de.imise.tool3lgm.tools.BrowseUtils;
 import de.imise.tool3lgm.userproperties.UserProperties;
 import de.imise.tool3lgm.userproperties.UserProperties.BooleanProperty;
@@ -158,163 +149,14 @@ public class Tool3lgm extends JFrame implements WindowListener, InternalFrameLis
     }
 
     /**
-     * Wenn der Baukasten mit RMI gestartet werden soll, wird <code>activateRMI</code> ausgeführt.
-     *
-     * @param args
-     * @return true, wenn der Baukasten erfolgreich den RMI starten konnte. Wenn Fehler aufgetreten sind, false.
-     */
-    static boolean activateRMI(final String args[], final boolean visible) {
-
-        // Der port, auf dem die RMI-Registry lauschen soll
-        int regPort = Registry.REGISTRY_PORT;
-
-        // Versuchen auf dem voreingestellten Port die Registry zu starten
-        try {
-            Registry registry = LocateRegistry.getRegistry("127.0.0.1");
-            // hole den vom User eingestellen RegistryPort
-            String regValue = String.valueOf(UserProperties.getRMIRegistryPort());
-
-            // hier wird geprüft, ob der Wert ungleich "" ist und mittels regulären Ausdruck, ob nur Ziffern enthalten sind.
-            if (!regValue.equals("") && regValue.matches("\\d*")) {
-                regPort = Integer.parseInt(regValue);
-            }
-
-            // Falls ein Fehler mit dem RegistryPort auftritt, wird hier der alte Portwert gesichert.
-            int oldRegPort = regPort;
-
-            // Wenn der RMI-Service erfolgreich gestartet werden konnte, wird <code>bound</code> true
-            boolean bound = false;
-
-            Remote remote = null;
-
-            // Im Fehlerfall soll der Dialog angezeigt werden, den den user entscheiden lässt, wie das weitere Vorgehen sein soll.
-            boolean showErrorDialog = true;
-
-            // Es wird solange versucht den RMI-Service zu starten, bis ein freier Port gefunden wurde oder der User einen freien eingegeben hat.
-            while (!bound) {
-
-                try {
-                    registry.list();
-                } catch (Exception ex) {
-                    try {
-                        if (Registry.REGISTRY_PORT != regPort) {
-                            registry = LocateRegistry.createRegistry(regPort);
-                        } else {
-                            registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-                        }
-                    } catch (Exception e) {
-                    }
-                }
-                try {
-                    //TODO:############# auf jeden Fall wieder reinnehmen!!!
-                    remote = Naming.lookup("//127.0.0.1:" + regPort + "/Tool3lgmServer");
-                } catch (Exception innerEx) {
-                }
-                // Wenn der RMI-Service noch nicht läuft, wird hier weiter gemacht.
-                if (remote == null || !(remote instanceof Tool3lgmServer)) {
-
-                    // Wenn der Baukasten schon läuft, wird kein neuer instanziiert, sonst schon.
-                    if (tool == null) {
-                        tool = new Tool3lgm();
-                        menuBar = new MenuBar();
-                        CSH.setHelpIDString(menuBar, "uebersicht_menueleiste");
-                        KeyStrokes.registerPublicKeyStrokes(tool.getRootPane());
-                        tool.setJMenuBar(menuBar);
-                        tool.setVisible(visible);
-                        tool.toolbar.selectedDocChanged();
-                    }
-
-                    // Hier ist die kritische Stelle. Das Rebind schlägt fehl, wenn ein fremder Service den Port belegt, auf dem der Baukasten lauschen soll.
-                    try {
-                        // System.err.println("try port: "+regPort);
-                        Naming.rebind("//127.0.0.1:" + regPort + "/Tool3lgmServer", new Tool3lgmServerImpl(Static.tool));
-
-                        // Wenn der RMI-Server erfolgreich gestartet werden konnte, wird bound true.
-                        // Wenn nicht, ist eine Exception geflogen und ist in die catch () gesprungen. bound wurde nicht true.
-                        bound = true;
-
-                        // Wenn der alte regPort ungleich dem neuen ist, wird der neue gespeichert und beim nächsten Programmstart als Standard-Port angewandt.
-                        if (regPort != oldRegPort) {
-                            UserProperties.setRMIRegistryPort(regPort);
-                            JOptionPane.showMessageDialog(Static.tool, getResString("rmiNewRegPortIs") + " " + regPort);
-                        }
-
-                    } catch (RemoteException e) {
-                        // e.printStackTrace();
-                        // Sollte der RMI-Server nicht auf dem aktuellen eingestellten regPort lauschen können:
-                        // Wenn ein Fehler aufgetreten ist und nicht schon nach einem neuen Port gesucht wird, wird der RMI-FehlerDialog angezeigt.
-                        RMIErrorPanel rmip = new RMIErrorPanel();
-
-                        if (showErrorDialog) {
-                            if (JOptionPane.showOptionDialog(Static.tool, rmip, getResString("rmiError"), JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null) == JOptionPane.YES_OPTION) {
-                                if (rmip.isRmiAutoNextFreePortCheckBox()) {
-                                    // Es wird ein neue Port bis 65500 gesucht, wenn bis dahin keiner frei ist, wird wieder beim standardPort begonnen
-                                    if (regPort < 65500) {
-                                        regPort++;
-                                    } else {
-                                        regPort = 1099;
-                                    }
-                                    showErrorDialog = false;
-                                }
-                                // Wenn der Benutzer einen Port eingegeben hat, wird er gesichtert und versucht darauf zu verbinden.
-                                // Sollte en fehler auftreten, wir dder Errordialog wieder angezeigt.
-                                else {
-                                    regPort = Integer.parseInt(rmip.getRmiRegistryPortTextFieldValue());
-                                    UserProperties.setRMIRegistryPort(regPort);
-                                }
-                            }
-                        }
-                        // Wenn schon nach einem neuen Port gescuht wird, wird der ErrorDialog nicht nochmal angezeigt sondern gleich hier weiter gemacht:
-                        else {
-                            // Es wird ein neuer Port bis 65500 gesucht, wenn bis dahin keiner frei ist, wird wieder beim standardPort begonnen
-                            if (regPort < 65500) {
-                                regPort++;
-                            } else {
-                                regPort = 1099;
-                            }
-
-                        }
-                    }
-                    // e.printStackTrace();
-
-                } else {
-                    // Wenn schon eine Instanz des Tools läuft, wird hier hergesprungen.
-                    // <code>bound</code> muss auf true gesetzt werden, dmit die Schleife beendet werden kann.
-                    bound = true;
-                }
-
-            }
-
-            remote = Naming.lookup("//127.0.0.1:" + regPort + "/Tool3lgmServer");
-            if (remote == null) {
-                Log.show(Log.FATAL, "RMI registration failed", new Exception("RMI registration failed"));
-                return false;
-            }
-            // Wenn der RMI-Service erfolgreich auf dem regPort lauscht, wird hier weiter gemacht.
-            // Der RMI-Server steht für RMI-Aufrufe bereit.
-
-            Tool3lgmServer tool3lgmServer = (Tool3lgmServer) remote;
-            if (args.length != 0) {
-                String[] params = new String[args.length - 1];
-                for (int i = 0; i < params.length; i++) {
-                    params[i] = args[i + 1];
-                }
-                tool3lgmServer.processCommand(args[0], params);
-            }
-        } catch (Exception ex) {
-            System.err.println(ex);
-            Log.show(Log.FATAL, "RMI registration failed", ex);
-            return false;
-        }
-        return true;
-    }
-
-    /**
      * constructor
      *
      * @see java.lang.Object#Object()
      */
-    private Tool3lgm() {
+    Tool3lgm(final boolean visible) {
+
+        Static.tool = this;
+
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -393,6 +235,14 @@ public class Tool3lgm extends JFrame implements WindowListener, InternalFrameLis
         Help.getHelp().enableHelpKey(rootPane, "willkommen");
         // Direkthilfe für die einzelnen Baukastenteile
         CSH.setHelpIDString(modelBrowserPanel, "uebersicht_modellbrowser");
+
+        menuBar = new MenuBar();
+        CSH.setHelpIDString(menuBar, "uebersicht_menueleiste");
+        KeyStrokes.registerPublicKeyStrokes(getRootPane());
+        setJMenuBar(menuBar);
+        setVisible(visible);
+        toolbar.selectedDocChanged();
+
     }
 
     /**
