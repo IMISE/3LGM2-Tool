e2589416b3084b39dad77ab4d3ff2d96d4044dcb
Alexander Strübing <astruebi@regentropfen.org>
2018-03-28 22:27:03 +0200

    ActionSource: so umgeschrieben, dass sie nicht mehr allg. Actions sonder ExtendedActions und deren eventuell vorhandene Selection korrekt initialisiert

--- axsutils/src/main/java/de/imise/util/swing/event/ActionSource.java
+++ axsutils/src/main/java/de/imise/util/swing/event/ActionSource.java
@@ -1,5 +1,6 @@
 package de.imise.util.swing.event;
 
+import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -8,19 +9,35 @@ import java.util.Set;
 
 import javax.swing.Action;
 
+/**
+ * Interface das vorzugsweise an alle Enum-Klassen gehängt werden kann, die irgendwelche ActionIdentifiert
+ * definieren. Damit können gleich die Actions definiert werden, wenn diese zur Initialisierung nur das
+ * ActionSource-Object brauchen.
+ *
+ * @author AXS (10.03.2018)
+ */
 public interface ActionSource {
 
     public static final String PPP = "...";
 
-    public static final Map<ActionSource, Class<? extends Action>> ACTION_CLASS = new HashMap<>();
+    /**
+     * Mappt von einer ActionSource auf die Action-Klasse, die für diese ActionSource initialisiert werden soll.
+     */
+    public static final Map<ActionSource, Class<? extends ExtendedAction>> ACTION_CLASS = new HashMap<>();
 
-    public default Action getAction() {
-        Class<? extends Action> actionClass = getActionClass();
+    /**
+     * Liefert die Action zu dieser ActionSource
+     *
+     * @return
+     */
+    public default ExtendedAction getAction() {
+        Class<? extends ExtendedAction> actionClass = getActionClass();
         Class<?> constructorParameterClass = getClass();
-        Action action = null;
+        ExtendedAction action = null;
         while (constructorParameterClass != null) {
             try {
-                action = actionClass.getConstructor(constructorParameterClass).newInstance(this);
+                Constructor<? extends ExtendedAction> actionConstructor = actionClass.getConstructor(constructorParameterClass);
+                action = actionConstructor.newInstance(this);
                 break;
             } catch (Exception e) {
                 constructorParameterClass = constructorParameterClass.getSuperclass();
@@ -29,10 +46,20 @@ public interface ActionSource {
         if (isInteractiveAction()) {
             action.putValue(Action.NAME, action.getValue(Action.NAME).toString() + PPP);
         }
+        if (isSelected()) {
+            action.setSelected(true);
+        }
         return action;
     }
 
-    public static void put(final Class<? extends Action> actionClass, final ActionSource... actionSources) {
+    /**
+     * Speichert für die übergebenen ActionSources, welche ActionClass über die Methode {@link #getActionClass()} zurück
+     * geliefert werden soll (wenn die default implementierung bestehen bleibt).
+     *
+     * @param actionClass
+     * @param actionSources
+     */
+    public static void put(final Class<? extends ExtendedAction> actionClass, final ActionSource... actionSources) {
         for (ActionSource actionSource : actionSources) {
             ACTION_CLASS.put(actionSource, actionClass);
         }
@@ -41,15 +68,19 @@ public interface ActionSource {
     /**
      * Actions bei denen irgendwie ein Dialog nach dem Aufruf angezeigt wird, sollten über diese put-Funktion geadded werden.
      * Einzige Auswirkung ist, dass sie im Menü 3 Punkte hinter ihren Namen bekommen.
-     * 
+     *
      * @param actionClass
      * @param actionSources
      */
-    public static void putInteractive(final Class<? extends Action> actionClass, final ActionSource... actionSources) {
+    public static void putInteractive(final Class<? extends ExtendedAction> actionClass, final ActionSource... actionSources) {
         put(actionClass, actionSources);
         setInteractive(actionSources);
     }
-    public default Class<? extends Action> getActionClass() {
+
+    /**
+     * Liefert die ActionKlasse, die für diese ActionSource initialisiert wird.
+     */
+    public default Class<? extends ExtendedAction> getActionClass() {
         return ACTION_CLASS.get(this);
     }
 
@@ -67,6 +98,10 @@ public interface ActionSource {
      */
     public default boolean isInteractiveAction() {
         return INTERACTIVE_ACTIONS.contains(this);
+    }
+
+    public default boolean isSelected() {
+        return false;
     }
 
 }
