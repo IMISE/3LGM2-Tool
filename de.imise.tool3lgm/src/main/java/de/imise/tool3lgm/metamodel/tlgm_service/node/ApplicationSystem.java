63f88091e9b857aab01842921ae543b8175808e2
Alexander Strübing <astruebi@regentropfen.org>
2018-04-10 15:24:52 +0200

    ApplicationSystem und IheActorApplicationSystem: Eigenschaftsdialoge berichtigt

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/ApplicationSystem.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/ApplicationSystem.java
@@ -2,8 +2,8 @@ package de.imise.tool3lgm.metamodel.tlgm_service.node;
 
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
 import de.imise.tool3lgm.graphtools.path.MetaPath;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationSystem_IheActorApplicationSystem_Edge;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.ApplicationSystem_SoftwareProduct_Edge;
-import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheActorOfIntegrationProfile_IheActorApplicationSystem_Edge;
 
 /**
  * @author AXS (26.12.2017)
@@ -14,7 +14,7 @@ public final class ApplicationSystem extends ApplicationComponent {
     public ElementPropertyDialog createPropertyDialog() {
         ElementPropertyDialog dialog = super.createPropertyDialog();
         dialog.addEdgePanel(ApplicationSystem_SoftwareProduct_Edge.class);
-        dialog.addEdgePanel(IheActorOfIntegrationProfile_IheActorApplicationSystem_Edge.class);
+        dialog.addEdgePanel(ApplicationSystem_IheActorApplicationSystem_Edge.class);
         return dialog;
     }
 
--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheActorApplicationSystem.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheActorApplicationSystem.java
@@ -1,8 +1,14 @@
 package de.imise.tool3lgm.metamodel.tlgm_service.node;
 
+import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheActorOfIntegrationProfile_IheActorApplicationSystem_Edge;
+
 public class IheActorApplicationSystem extends ApplicationComponent {
 
-    public IheActorApplicationSystem() {
+    @Override
+    public ElementPropertyDialog createPropertyDialog() {
+        ElementPropertyDialog dialog = super.createPropertyDialog();
+        dialog.addEdgePanel(IheActorOfIntegrationProfile_IheActorApplicationSystem_Edge.class);
+        return dialog;
     }
-
 }
