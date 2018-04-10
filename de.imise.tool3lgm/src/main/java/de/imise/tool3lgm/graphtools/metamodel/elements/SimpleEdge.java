79636cfc978d99d535679685cd87c3dca7fcfde6
Alexander Strübing <astruebi@regentropfen.org>
2018-04-10 17:51:00 +0200

    SubordinationEdge: Interface eingeführt

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/SimpleEdge.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/SimpleEdge.java
@@ -3,7 +3,6 @@ package de.imise.tool3lgm.graphtools.metamodel.elements;
 public abstract class SimpleEdge extends Edge {
 
     public SimpleEdge() {
-        // TODO Auto-generated constructor stub
     }
 
 }
---
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/SubordinationEdge.java
@@ -0,0 +1,10 @@
+package de.imise.tool3lgm.graphtools.metamodel.elements;
+
+/**
+ * Interface, um Kanten zu markern, die ein Element einem anderen unterorden
+ *
+ * @author AXS (10.04.2018)
+ */
+public interface SubordinationEdge {
+
+}
