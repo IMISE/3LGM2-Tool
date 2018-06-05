37187890e3228c910474e5fbc872fad44a86235f
Alexander Strübing <astruebi@regentropfen.org>
2018-06-05 14:36:39 +0200

    GDCollectionPrinter: Ausgabe der Collection umfangeicher gemacht

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/model/GDCollectionPrinter.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/model/GDCollectionPrinter.java
@@ -1,11 +1,13 @@
 package de.imise.tool3lgm.graphtools.model;
 
 import de.imise.tool3lgm.graphtools.metamodel.ModelConstants;
+import de.imise.tool3lgm.graphtools.metamodel.elements.Edge;
 import de.imise.tool3lgm.graphtools.metamodel.elements.ModelElement;
 import de.imise.tool3lgm.graphtools.view.container.EdgeContainer;
 import de.imise.tool3lgm.graphtools.view.container.ElementContainer;
 import de.imise.tool3lgm.graphtools.view.container.LayerContainer;
 import de.imise.tool3lgm.graphtools.view.container.NodeContainer;
+import de.imise.util.Sys;
 
 public class GDCollectionPrinter {
 
@@ -23,9 +25,9 @@ public class GDCollectionPrinter {
     private StringBuilder appendMainModelInfromation() {
         appendln("Model name=", gdcoll.getName());
         increaseIndent();
-        appendln(gdcoll.getMainGraphDocument());
+        appendGraphDocument(gdcoll.getMainGraphDocument());
         for (Szenario szen : gdcoll.getSzenarios()) {
-            appendln(szen);
+            appendGraphDocument(szen);
         }
         resetIndent();
         appendln();
@@ -33,10 +35,12 @@ public class GDCollectionPrinter {
     }
 
     private StringBuilder appendGraphDocument(final GraphDocument doc) {
-        appendln(doc.getClass().getSimpleName()).append(": ").append(doc);
+        append(doc.getClass().getSimpleName()).append(": ").append(doc);
+        appendln();
         for (int layerIndex : ModelConstants.LAYERS) {
             LayerContainer lc = doc.getLayer(layerIndex);
             appendln("Layer ", layerIndex, " Knoten: ", lc.getNodeContainerCount(), " Kanten: ", lc.getEdgeContainerCount(), " Knickpunkte: ", lc.getBendpointContainerCount());
+            increaseIndent();
             appendln("Knoten");
             increaseIndent();
             for (NodeContainer nc : lc.getNodeContainersAlphabetical()) {
@@ -49,21 +53,44 @@ public class GDCollectionPrinter {
                 appendElementContainer(ec);
             }
             decreaseIndent();
-
+            decreaseIndent();
         }
         resetIndent();
-        return sb;
-    }
-
-    private StringBuilder appendNodeContainer(final NodeContainer nc) {
-        ModelElement me = nc.getElement();
-        appendln(me.getClass().getSimpleName(), ": ", me, " hash=", me.getHashString());
         return sb;
     }
 
     private StringBuilder appendElementContainer(final ElementContainer ec) {
         ModelElement me = ec.getElement();
         appendln(me.getClass().getSimpleName(), ": ", me);
+        increaseIndent();
+        appendIndent();
+        append("hash=", me.getHashString(), "    idMe=", System.identityHashCode(me), "\tidCont=", System.identityHashCode(ec));
+        if (me instanceof Edge) {
+            Edge edge = (Edge) me;
+            ModelElement start = edge.getStart();
+            String startHash = start == null ? "null" : start.getHashString();
+            ModelElement end = edge.getEnd();
+            String endHash = end == null ? "null" : end.getHashString();
+            append("\tstartHash=", startHash, "\tendHash=", endHash);
+        }
+        appendln();
+        appendEdgesOfElement(me);
+        decreaseIndent();
+        return sb;
+    }
+
+    private StringBuilder appendEdgesOfElement(final ModelElement me) {
+        for (Edge edge : me.getEdges()) {
+            appendln("--> ", edge.getClass().getSimpleName(), ": ", edge);
+            increaseIndent();
+            //appendIndent();
+            ModelElement start = edge.getStart();
+            String startHash = start == null ? "null" : start.getHashString();
+            ModelElement end = edge.getEnd();
+            String endHash = end == null ? "null" : end.getHashString();
+            appendln("hash=", edge.getHashString(), "    idEdge=", System.identityHashCode(edge), "\tstartHash=", startHash, "\tendHash=", endHash);
+            decreaseIndent();
+        }
         return sb;
     }
 
@@ -80,6 +107,7 @@ public class GDCollectionPrinter {
     }
 
     private StringBuilder appendln(final Object... objects) {
+        appendIndent();
         append(objects);
         appendln();
         return sb;
@@ -87,7 +115,7 @@ public class GDCollectionPrinter {
 
     private StringBuilder appendIndent() {
         for (int i = 0; i < indent; i++) {
-            append("\t");
+            sb.append("\t");
         }
         return sb;
     }
@@ -105,6 +133,15 @@ public class GDCollectionPrinter {
     private int resetIndent() {
         indent = 0;
         return indent;
+    }
+
+    @Override
+    public String toString() {
+        return sb.toString();
+    }
+
+    public static final void print(final GDCollection gdcoll) {
+        Sys.errn(2, new GDCollectionPrinter(gdcoll));
     }
 
 }
