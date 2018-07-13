e84263bf9e5ab48477e33b6082b76d18812e0e74
Alexander Strübing <astruebi@regentropfen.org>
2018-07-13 10:28:55 +0200

    IheActorOfIntegrationProfile: Eigenschaftsdialog um die IHE Bereitstellende und IHE Aufrufende Schnittstelle ergänzt

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheActorOfIntegrationProfile.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheActorOfIntegrationProfile.java
@@ -1,7 +1,9 @@
 package de.imise.tool3lgm.metamodel.tlgm_service.node;
 
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
+import de.imise.tool3lgm.graphtools.metamodel.ModelConstants;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheActorApplicationSystem_IheActorOfIntegrationProfile_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheActorOfIntegrationProfile_IheInterface_Edge;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheIntegrationProfile_IheActorOfIntegrationProfile_Edge;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheTransactionLink_Edge;
 
@@ -16,6 +18,9 @@ public class IheActorOfIntegrationProfile extends IheConcept {
         dialog.addDescripSingleConnectionPanel(IheIntegrationProfile_IheActorOfIntegrationProfile_Edge.class);
         dialog.addEdgePanel(IheTransactionLink_Edge.class);
         dialog.addEdgePanel(IheActorApplicationSystem_IheActorOfIntegrationProfile_Edge.class);
+        dialog.addTabbedPanel(ModelConstants.getDisplayablePluralName(IheInterface.class));
+        dialog.addTabbedPanelPathConnectionPanel(IheInvokingInterface.class, IheActorOfIntegrationProfile_IheInterface_Edge.class);
+        dialog.addTabbedPanelPathConnectionPanel(IheProvidingInterface.class, IheActorOfIntegrationProfile_IheInterface_Edge.class);
         return dialog;
     }
 
