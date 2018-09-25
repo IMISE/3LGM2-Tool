e7e7459f158eafb5dbbe99a1d20462b3be355702
Alexander Strübing <astruebi@regentropfen.org>
2018-09-25 14:24:41 +0200

    DoubleMeaningEdge: kommentiert und leeren Konstruktor entfernt

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/DoubleMeaningEdge.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/DoubleMeaningEdge.java
@@ -1,9 +1,10 @@
 package de.imise.tool3lgm.graphtools.metamodel.elements;
 
+/**
+ * Oberklasse für alle Kantenklassen mit doppelter Bedeutung. Also Kanten, die 2 Assoziationen in einer zwischen 2 Klassen sind.
+ *
+ * @author AXS (25.09.2017)
+ */
 public abstract class DoubleMeaningEdge extends Edge {
-
-    public DoubleMeaningEdge() {
-        // TODO Auto-generated constructor stub
-    }
 
 }
