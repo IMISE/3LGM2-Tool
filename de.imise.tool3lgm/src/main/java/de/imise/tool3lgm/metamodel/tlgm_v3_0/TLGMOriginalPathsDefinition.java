114ff71e8c8794f99f44f7c12989872259328076
Alexander Strübing <astruebi@regentropfen.org>
2018-11-28 14:55:14 +0100

    TLGMOriginalPathsDefinition: aufgeräumt, so dass keine Pfade mehr doppelt vorkommen, nur weil sie nochmal für Unterklassen der eigentlichen Start- oder Endklasse definiert wurden und Resorucen für jeweils beiden Pfadrichungen hinzugefügt

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_v3_0/TLGMOriginalPathsDefinition.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_v3_0/TLGMOriginalPathsDefinition.java
@@ -20,7 +20,6 @@ import de.imise.tool3lgm.metamodel.tlgm_v3_0.node.Anwendungsbaustein;
 import de.imise.tool3lgm.metamodel.tlgm_v3_0.node.Aufgabe;
 import de.imise.tool3lgm.metamodel.tlgm_v3_0.node.Datenbanksystem;
 import de.imise.tool3lgm.metamodel.tlgm_v3_0.node.Kommunikationsstandard;
-import de.imise.tool3lgm.metamodel.tlgm_v3_0.node.KonAnwendungsbaustein;
 import de.imise.tool3lgm.metamodel.tlgm_v3_0.node.Objekttyp;
 import de.imise.tool3lgm.metamodel.tlgm_v3_0.node.Organisationseinheit;
 import de.imise.tool3lgm.metamodel.tlgm_v3_0.node.PhysischerDVBaustein;
@@ -35,66 +34,42 @@ public class TLGMOriginalPathsDefinition extends MetaPathDefinition {
     protected final void init() {
 
         /* Aufgabe - Organisationseinheit */
-        put(Aufgabe.class, Organisationseinheit.class, "text_wird_erledigt_in", AufAufOrgVerbindung.class, OrgAufOrgVerbindung.class);
+        put(Aufgabe.class, Organisationseinheit.class, "PATH_is_executed_by", AufAufOrgVerbindung.class, OrgAufOrgVerbindung.class);
 
         /* Aufgabe - Anwendungsbaustein */
-        put(Aufgabe.class, Anwendungsbaustein.class, "text_unterstuetzt", AufAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class);
-
-        /* Aufgabe - RechAnwendungsbaustein */
-        put(Aufgabe.class, RechAnwendungsbaustein.class, "text_unterstuetzt", AufAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class);
-
-        /* Aufgabe - KonAnwendungsbaustein */
-        put(Aufgabe.class, KonAnwendungsbaustein.class, "text_unterstuetzt", AufAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class);
+        put(Aufgabe.class, Anwendungsbaustein.class, "PATH_is_supported_by", AufAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class);
 
         /* Aufgabe - PhyDVBaustein */
-        put(Aufgabe.class, PhysischerDVBaustein.class, "text_unterstuetzt", AufAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class, PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class);
+        put(Aufgabe.class, PhysischerDVBaustein.class, "PATH_is_supported_by", AufAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class, PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class);
 
         /* Aufgabe - Standort */
-        put(Aufgabe.class, Standort.class, "text_erledigt", AufAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class, PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class, PdvbStoVerbindung.class);
+        put(Aufgabe.class, Standort.class, "PATH_is_executed_at", AufAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class, PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class, PdvbStoVerbindung.class);
 
         /* Objekttyp - Datenbanksystem */
-        put(Objekttyp.class, Datenbanksystem.class, "text_speichert", ObjReprVerbindung.class, DbsDatVerbindung.class);
+        put(Objekttyp.class, Datenbanksystem.class, "PATH_is_stored_in", ObjReprVerbindung.class, DbsDatVerbindung.class);
 
         /* Anwendungsbaustein - PhyDVBaustein */
-        put(Anwendungsbaustein.class, PhysischerDVBaustein.class, "text_installiert", PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class);
-
-        /* RechAnwendungsbaustein - PhyDVBaustein */
-        put(RechAnwendungsbaustein.class, PhysischerDVBaustein.class, "text_installiert", PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class);
-
-        /* KonAnwendungsbaustein - PhyDVBaustein */
-        put(KonAnwendungsbaustein.class, PhysischerDVBaustein.class, "text_installiert", PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class);
+        put(Anwendungsbaustein.class, PhysischerDVBaustein.class, "PATH_is_installed_on", PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class);
 
         /* Organisationseinheit - PhyDVBaustein */
-        put(Organisationseinheit.class, PhysischerDVBaustein.class, "text_nutzt", OrgAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class, PdvbkAwbVerbindung.class, PdvbPdvbkVerbindung.class);
+        put(PhysischerDVBaustein.class, Organisationseinheit.class, "PATH_is_used_by", PdvbPdvbkVerbindung.class, PdvbkAwbVerbindung.class, AwbAwbkVerbindung.class, AwbkAufOrgVerbindung.class, OrgAufOrgVerbindung.class);
 
         /* Organisationseinheit - Anwendungsbaustein */
-        put(Organisationseinheit.class, Anwendungsbaustein.class, "text_nutzt", OrgAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class);
-
-        /* Organisationseinheit - RechAnwendungsbaustein */
-        put(Organisationseinheit.class, RechAnwendungsbaustein.class, "text_nutzt", OrgAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class);
-
-        /* Organisationseinheit - KonAnwendungsbaustein */
-        put(Organisationseinheit.class, KonAnwendungsbaustein.class, "text_nutzt", OrgAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class);
+        put(Anwendungsbaustein.class, Organisationseinheit.class, "PATH_is_used_by", AwbAwbkVerbindung.class, AwbkAufOrgVerbindung.class, OrgAufOrgVerbindung.class);
 
         /* Organisationseinheit - Softwareprodukt */
-        put(Organisationseinheit.class, Softwareprodukt.class, "text_nutzt", OrgAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class, RawbAwpVerbindung.class, AwpSwpVerbindung.class);
+        put(Softwareprodukt.class, Organisationseinheit.class, "PATH_is_used_by", AwpSwpVerbindung.class, RawbAwpVerbindung.class, AwbAwbkVerbindung.class, AwbkAufOrgVerbindung.class, OrgAufOrgVerbindung.class);
 
         /* Anwendungsbaustein - Kommunikationsstandard */
-        put(Anwendungsbaustein.class, Kommunikationsstandard.class, "text_nutzt", AwbKommssVerbindung.class, BssKommstVerbindung.class);
-
-        /* RechAnwendungsbaustein - Kommunikationsstandard */
-        put(RechAnwendungsbaustein.class, Kommunikationsstandard.class, "text_nutzt", AwbKommssVerbindung.class, BssKommstVerbindung.class);
-
-        /* KonAnwendungsbaustein - Kommunikationsstandard */
-        put(KonAnwendungsbaustein.class, Kommunikationsstandard.class, "text_nutzt", AwbKommssVerbindung.class, BssKommstVerbindung.class);
+        put(Kommunikationsstandard.class, Anwendungsbaustein.class, "PATH_is_used_by", BssKommstVerbindung.class, AwbKommssVerbindung.class);
 
         /* RechAnwendungsbaustein - Softwareprodukt */
-        put(RechAnwendungsbaustein.class, Softwareprodukt.class, "text_gesteuert", RawbAwpVerbindung.class, AwpSwpVerbindung.class);
+        put(RechAnwendungsbaustein.class, Softwareprodukt.class, "PATH_is_controlled_by", RawbAwpVerbindung.class, AwpSwpVerbindung.class);
 
         /* Objekttyp - RechAnwendungsbaustein */
-        put(Objekttyp.class, RechAnwendungsbaustein.class, "text_fuehrenden", ObjLogspVerbindung.class, RawbDbsVerbindung.class);
+        put(RechAnwendungsbaustein.class, Objekttyp.class, "PATH_is_master_of", RawbDbsVerbindung.class, ObjLogspVerbindung.class);
 
-        put(Objekttyp.class, RechAnwendungsbaustein.class, "text_speichert", ObjReprVerbindung.class, DbsDatVerbindung.class, RawbDbsVerbindung.class);
+        put(Objekttyp.class, RechAnwendungsbaustein.class, "PATH_is_stored_in", ObjReprVerbindung.class, DbsDatVerbindung.class, RawbDbsVerbindung.class);
 
         //        put(new MetaPath(Objekttyp.class, Anwendungsbaustein.class, new Class[][] {
         //                {
