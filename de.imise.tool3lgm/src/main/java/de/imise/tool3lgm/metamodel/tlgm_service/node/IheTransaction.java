9ebebd66942e8fcda0f680913227b60690285ae9
Alexander Strübing <alexander.struebing@gmail.com>
2019-03-18 15:02:30 +0100

    IheTransaction: Dialog berichtigt: Schnittstellen werden nicht mehr im SingleConnectionPanel sondern in PathConnectionPanels angezeigt und dieselbe Tabelle wie beim Actor hin zu den Transaktionen zeigt die Verbindungen hin zu den Actors an

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheTransaction.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheTransaction.java
@@ -1,6 +1,12 @@
 package de.imise.tool3lgm.metamodel.tlgm_service.node;
 
+import static de.imise.tool3lgm.graphtools.path.meta.SimpleMetaPathCreator.createSimpleMetaPath;
+
+import de.imise.tool3lgm.graphtools.ElementsNameBuilder;
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
+import de.imise.tool3lgm.graphtools.dialog.panel.ConnectedElementsTableColumnsDefinition;
+import de.imise.tool3lgm.graphtools.path.meta.SimpleMetaPath;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheActor_IheInterface_Edge;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheIntegrationProfile_IheTransaction_Edge;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheInvokingInterface_IheTransaction_Edge;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheProvidingInterface_IheTransaction_Edge;
@@ -13,8 +19,18 @@ public class IheTransaction extends IheConcept {
     @Override
     public ElementPropertyDialog createPropertyDialog() {
         ElementPropertyDialog dialog = super.createPropertyDialog();
-        dialog.addDescripSingleConnectionPanel(true, IheInvokingInterface_IheTransaction_Edge.class);
-        dialog.addDescripSingleConnectionPanel(true, IheProvidingInterface_IheTransaction_Edge.class);
+        dialog.addTabbedPanel(ElementsNameBuilder.getDisplayablePluralName(IheInterface.class));
+        dialog.addTabbedPanelPathConnectionPanel(IheInvokingInterface.class, IheInvokingInterface_IheTransaction_Edge.class);
+        dialog.addTabbedPanelPathConnectionPanel(IheProvidingInterface.class, IheProvidingInterface_IheTransaction_Edge.class);
+
+        ConnectedElementsTableColumnsDefinition columnsDefinition = new ConnectedElementsTableColumnsDefinition();
+        columnsDefinition.addColumnEndElement(300);
+        columnsDefinition.addColumnOptional(1, 50);
+        columnsDefinition.addColumnPathStepName(0, "HEADER_ACTOR_TRANSACTION_CONNECTION_NAME", 150);
+        SimpleMetaPath path1 = createSimpleMetaPath(IheActor.class, IheTransaction.class, "PATH_IHE_ACTOR_IHE_INVOKING_INTERFACE_IHE_TRANSACTION", IheActor_IheInterface_Edge.class, IheInvokingInterface_IheTransaction_Edge.class).getOtherDirection();
+        SimpleMetaPath path2 = createSimpleMetaPath(IheActor.class, IheTransaction.class, "PATH_IHE_ACTOR_IHE_PROVIDING_INTERFACE_IHE_TRANSACTION", IheActor_IheInterface_Edge.class, IheProvidingInterface_IheTransaction_Edge.class).getOtherDirection();
+        dialog.addTablePanel(true, columnsDefinition, path1, path2);
+
         dialog.addEdgePanel(IheIntegrationProfile_IheTransaction_Edge.class);
         return dialog;
     }
