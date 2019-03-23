ac818c2e0f0e2111f15fe64cd6b7f001106a993d
Alexander Strübing <alexander.struebing@gmail.com>
2019-03-23 15:12:21 +0100

    IheIntegrationProfile: Eigenschaftsdialog hat Tabelle wie in den Technical Documents (Actors, Transaction, Optional)

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheIntegrationProfile.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/metamodel/tlgm_service/node/IheIntegrationProfile.java
@@ -1,8 +1,13 @@
 package de.imise.tool3lgm.metamodel.tlgm_service.node;
 
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
+import de.imise.tool3lgm.graphtools.dialog.panel.ConnectedElementsTableColumnsDefinition;
+import de.imise.tool3lgm.graphtools.path.meta.SimpleMetaPath;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheActor_IheInterface_Edge;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheIntegrationProfile_IheActor_Edge;
 import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheIntegrationProfile_IheTransaction_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheInvokingInterface_IheTransaction_Edge;
+import de.imise.tool3lgm.metamodel.tlgm_service.edge.IheProvidingInterface_IheTransaction_Edge;
 
 /**
  * @author AXS (31.01.2018)
@@ -14,6 +19,16 @@ public class IheIntegrationProfile extends IheConcept {
         ElementPropertyDialog dialog = super.createPropertyDialog();
         dialog.addEdgePanel(IheIntegrationProfile_IheTransaction_Edge.class);
         dialog.addEdgePanel(IheIntegrationProfile_IheActor_Edge.class);
+
+        ConnectedElementsTableColumnsDefinition columnsDefinition = new ConnectedElementsTableColumnsDefinition();
+        columnsDefinition.addColumnPathStepEnd(0, 300);
+        columnsDefinition.addColumnEndElement(300);
+        columnsDefinition.addColumnOptional(1, 50);
+        columnsDefinition.addColumnPathStepName(2, "HEADER_IHE_ACTOR_IHE_TRANSACTION_CONNECTION_NAME", 150);
+        SimpleMetaPath path1 = createSimpleMetaPath(2, IheIntegrationProfile_IheActor_Edge.class, IheActor_IheInterface_Edge.class, IheInvokingInterface_IheTransaction_Edge.class);
+        SimpleMetaPath path2 = createSimpleMetaPath(2, IheIntegrationProfile_IheActor_Edge.class, IheActor_IheInterface_Edge.class, IheProvidingInterface_IheTransaction_Edge.class);
+        dialog.addTablePanel(true, columnsDefinition, path1, path2);
+
         return dialog;
     }
 
