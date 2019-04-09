dc49840dd3c1133a7b30db50c7f90bf9040eb16b
Alexander Strübing <alexander.struebing@gmail.com>
2019-04-09 12:03:33 +0200

    ApllicationManager: statische Funktion zum Ermitteln des Anwendungsverzeichnisses (kommt aus Tool3lgmConstants)

---
+++ axsutils/src/main/java/de/imise/util/ApplicationManager.java
@@ -0,0 +1,27 @@
+package de.imise.util;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author AXS (9 Apr 2019)
+ */
+public final class ApplicationManager {
+
+    /**
+     * Gibt das Oberste Verzeichnis zurück, in dem sich Anwendungsdaten befinden, also das Installationsverzeichnis.<br>
+     *
+     * @return Pfad zur Anwendung
+     */
+    public static File getApplicationDir() {
+        File f = null;
+        try {
+            f = new File(".").getCanonicalFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.err.println(f);
+        return f;
+    }
+
+}
