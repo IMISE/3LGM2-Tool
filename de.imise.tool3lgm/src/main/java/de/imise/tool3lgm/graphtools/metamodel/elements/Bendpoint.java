14458451b97b3ee4b15163d24554004e84f46d5c
Alexander Strübing <alexander.struebing@gmail.com>
2019-04-19 00:25:48 +0200

    Bendpoint: komplett final gemacht

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/Bendpoint.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/Bendpoint.java
@@ -8,7 +8,7 @@ import de.imise.tool3lgm.graphtools.view.container.ElementContainer;
 /**
  * @author imi0wendt (01.11.2004)
  */
-public class Bendpoint extends Node {
+public final class Bendpoint extends Node {
 
     /**
      * ID der Kante, zu der der Knickpunkt gehört
@@ -24,13 +24,6 @@ public class Bendpoint extends Node {
      * Index des Knickpunktes auf dem {@link EdgeContainer}
      */
     private int index = 0;
-
-    /**
-     *
-     */
-    public Bendpoint() {
-        super();
-    }
 
     @Override
     public ElementContainer createContainer(final GraphDocument doc) {
