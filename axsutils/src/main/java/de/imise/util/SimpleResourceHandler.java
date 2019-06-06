c2b3abc510fb8dc796ccfef41c9191101d70f71a
Alexander Strübing <alexander.struebing@gmail.com>
2019-06-06 21:52:57 +0200

    SimpleResourceHandler: lädt die Resourcen mit demselben ClassLoader, mit dem die Klasse, zu der die Resourcen gehören auch geladen wurde. Das hat bisher immer nur hingehauen, weil alles mit demselben ClassLoader geladen wurde. Aber nun sind einige Projektteile in eigenen jars und da haut das so nicht mehr hin

--- axsutils/src/main/java/de/imise/util/SimpleResourceHandler.java
+++ axsutils/src/main/java/de/imise/util/SimpleResourceHandler.java
@@ -1,41 +1,43 @@
 package de.imise.util;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
  * Erzeugt einen neuen Handler, der die Resourcen für eine übergebene Klasse zurückgeben kann.
- * 
+ *
  * @author AXS created on 21.08.2007
  */
 public class SimpleResourceHandler {
 
-	/**
-	 * ResourceBundles mit den speziellen Ressourcen für eine bestimmte Klasse
-	 */
-	protected ResourceBundle resourceBundle;
+    /**
+     * ResourceBundles mit den speziellen Ressourcen für eine bestimmte Klasse
+     */
+    protected ResourceBundle resourceBundle;
 
-	/**
-	 * Erzeugt einen neuen Handler, der die Resourcen für die übergebene Klasse zurückgeben kann.
-	 * 
-	 * @param ressourceNameClassSource
-	 * 		Klassenname, der den Namen der zu ladenden Ressorcendatei vorgibt.
-	 */
-	public SimpleResourceHandler(Class<?> ressourceNameClassSource) {
-		super();
-		if (ressourceNameClassSource == null)
-			return;
-		String resourceFileName = ressourceNameClassSource.getName().replace('.', '/');
-		resourceBundle = ResourceBundle.getBundle(resourceFileName);
-	}
+    /**
+     * Erzeugt einen neuen Handler, der die Resourcen für die übergebene Klasse zurückgeben kann.
+     *
+     * @param ressourceNameClassSource
+     *            Klassenname, der den Namen der zu ladenden Ressorcendatei vorgibt.
+     */
+    public SimpleResourceHandler(final Class<?> ressourceNameClassSource) {
+        super();
+        if (ressourceNameClassSource == null) {
+            return;
+        }
+        String resourceFileName = ressourceNameClassSource.getName().replace('.', '/');
+        resourceBundle = ResourceBundle.getBundle(resourceFileName, Locale.getDefault(), ressourceNameClassSource.getClassLoader());
+    }
 
-	/**
-	 * Liefert den Resourcen-String zum übergebenen Schlüssel
-	 * 
-	 * @param key
-	 * @return
-	 */
-	public String getString(String key) {
-		return resourceBundle.getString(key);
-	}
+    /**
+     * Liefert den Resourcen-String zum übergebenen Schlüssel
+     *
+     * @param key
+     * @return
+     */
+    public String getString(final String key) {
+        return resourceBundle.getString(key);
+    }
 
 }
