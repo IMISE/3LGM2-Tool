41509b9903bf28c6dfbd8d743bb7b4526901c430
Alexander Strübing <alexander.struebing@gmail.com>
2019-06-14 09:19:10 +0200

    DataImporter + UrlSourceDataImporter: abstracte Oberklassen für Importer hinzugefügt. Nach diesen Klassen wird zukünftig im Pluginverzeichnis gesucht (das ist noch nocht umgesetzt)

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/imexport/UrlSourceDataImporter.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/imexport/UrlSourceDataImporter.java
@@ -1,23 +1,32 @@
 package de.imise.tool3lgm.imexport;
 
-import de.imise.tool3lgm.graphtools.model.GDCollection;
-
 /**
  * Ein {@link DataImporter}, der eine Url als Quelle übergeben bekommt.
  *
  * @author AXS (10 Jun 2019)
  */
-public abstract class UrlSourceDataImporter extends DataImporter {
+public abstract class UrlSourceDataImporter<T> extends DataImporter<T> {
+
+    /** Url zur Datenquelle als String */
+    protected String urlString;
+
+    /**
+     * @param urlString Url zur Datenquelle als String
+     */
+    public UrlSourceDataImporter(final String urlString) {
+        //TODO: wenn hier null oder ein leerer String übergeben wird, könnte man festlegen, dass die Datenquell-URL aus einer Property-Datei mit demselben Namen wie die Importer-Klasse (this.getCLass().getsimpleName()) und im selben Verzeichnis wie das Jar-File des Importers liegen muss. Dann könnte man das sehr einfach von außen steuern.
+        this.urlString = urlString;
+    }
 
     @Override
-    protected final void importData(final GDCollection gdcoll) {
-        importData(null, gdcoll);
+    protected boolean importData() {
+        return importData(urlString);
     }
 
     /**
-     * @param url URL zu den Daten. Kann <code>null</code> sein und muss dann durch einen validen Wert ersetzt werden .
-     * @param gdcoll Zielmodell des Imports
+     * @param urlString Url zur Datenquelle als String. Kann <code>null</code> sein und muss dann durch einen validen Wert ersetzt werden .
+     * @return <code>true</code>, wenn der Import erfolgreich war
      */
-    protected abstract void importData(final String url, final GDCollection gdcoll);
+    protected abstract boolean importData(final String urlString);
 
 }
