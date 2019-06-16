5af0dfc2607006364ae1576eeb7f53cd8fbdb055
Alexander Strübing <alexander.struebing@gmail.com>
2019-06-16 22:47:02 +0200

    MultipleOptionPane: NullPointer verhindern

--- axsutils/src/main/java/de/imise/util/swing/dialog/MultipleOptionPane.java
+++ axsutils/src/main/java/de/imise/util/swing/dialog/MultipleOptionPane.java
@@ -250,9 +250,7 @@ public class MultipleOptionPane extends JOptionPane {
             if (buttonGroup != null) {
                 buttonGroup.add(checkBox);
             }
-            if (selected != null) {
-                checkBox.setSelected(selected.get(i));
-            }
+            checkBox.setSelected(Boolean.TRUE.equals(selected.get(i)));
             checkBox.setActionCommand(new Integer(i).toString());
             checkBoxPanel.add(checkBox);
         }
