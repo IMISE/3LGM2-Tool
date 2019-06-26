4d05e1c60e9c8e30dbf607c488ca54ba1a09a704
Alexander Strübing <alexander.struebing@gmail.com>
2019-06-26 21:58:49 +0200

    UrlSourceDataImporter: importData() von protected -> public

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/imexport/UrlSourceDataImporter.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/imexport/UrlSourceDataImporter.java
@@ -16,10 +16,11 @@ public abstract class UrlSourceDataImporter<T> extends DataImporter<T> {
     public UrlSourceDataImporter(final String urlString) {
         //TODO: wenn hier null oder ein leerer String übergeben wird, könnte man festlegen, dass die Datenquell-URL aus einer Property-Datei mit demselben Namen wie die Importer-Klasse (this.getCLass().getsimpleName()) und im selben Verzeichnis wie das Jar-File des Importers liegen muss. Dann könnte man das sehr einfach von außen steuern.
         this.urlString = urlString;
+        gdcoll.setName(urlString);
     }
 
     @Override
-    protected boolean importData() {
+    public boolean importData() {
         return importData(urlString);
     }
 
