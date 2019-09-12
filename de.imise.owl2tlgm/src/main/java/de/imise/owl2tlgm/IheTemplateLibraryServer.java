1e3447197a4007e59587991e3b9947c3026dac11
Alexander Strübing <alexander.struebing@gmail.com>
2019-09-13 00:10:36 +0200

    IheTemplateLibraryServer: Plugin-Umsetzung zum laden der IHE RDF-Datei als Template

---
+++ de.imise.owl2tlgm/src/main/java/de/imise/owl2tlgm/IheTemplateLibraryServer.java
@@ -0,0 +1,78 @@
+package de.imise.owl2tlgm;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import de.imise.tool3lgm.MetaModelContext;
+import de.imise.tool3lgm.Static;
+import de.imise.tool3lgm.Tool3lgmMetaModelContext;
+import de.imise.tool3lgm.Tool3lgmModelType.ModelCategory;
+import de.imise.tool3lgm.graphtools.metamodel.MetaModel;
+import de.imise.tool3lgm.graphtools.metamodel.MetaModelDefinition;
+import de.imise.tool3lgm.graphtools.metamodel.ModelConverter;
+import de.imise.tool3lgm.graphtools.model.GDCollection;
+import de.imise.tool3lgm.graphtools.model.template.TemplateLibraryServer;
+import de.imise.tool3lgm.graphtools.model.template.TemplateViewDefinition;
+import de.imise.tool3lgm.graphtools.path.meta.AbstractMetaPath;
+import de.imise.tool3lgm.graphtools.path.meta.SimpleMetaPath;
+import de.imise.tool3lgm.graphtools.path.meta.SimpleMetaPathCreator;
+import de.imise.tool3lgm.metamodel.service.TLGMServiceMetaModel;
+import de.imise.tool3lgm.metamodel.service.edge.IheIntegrationProfile_IheActor_Edge;
+import de.imise.tool3lgm.metamodel.service.edge.IheIntegrationProfile_IheDomain_Edge;
+import de.imise.tool3lgm.metamodel.service.node.IheActor;
+import de.imise.tool3lgm.metamodel.service.node.IheDomain;
+
+/**
+ * Function to load a {@link TLGMServiceMetaModel} template and the definition of the corresponding view of the template browser.
+ *
+ * @author AXS (30.08.2019)
+ */
+public class IheTemplateLibraryServer extends TemplateLibraryServer {
+
+    @Override
+    public String getDisplayName() {
+        return super.getDisplayName();
+    }
+
+    @Override
+    public TemplateViewDefinition getViewDefinition() {
+        return new TemplateViewDefinition() {
+
+            @Override
+            public String getMainCategoryName() {
+                return super.getMainCategoryName();
+            }
+
+            @Override
+            public List<AbstractMetaPath> getViewMetaPaths() {
+                MetaModelContext metaModelContext = Tool3lgmMetaModelContext.getMetaModelContextForDefinitionClass(getMetaModelDefinitionClass());
+                MetaModel metaModel = metaModelContext.getMetaModel();
+                SimpleMetaPath submodelMetaPath = SimpleMetaPathCreator.createSimpleMetaPath(metaModel, IheDomain.class, IheActor.class, IheIntegrationProfile_IheDomain_Edge.class, IheIntegrationProfile_IheActor_Edge.class);
+                return ImmutableList.of(submodelMetaPath);
+            }
+
+        };
+    }
+
+    @Override
+    public Class<? extends MetaModelDefinition> getMetaModelDefinitionClass() {
+        return TLGMServiceMetaModel.class;
+    }
+
+    @Override
+    public GDCollection getTemplateLibrary() {
+        IheRDFDataImporter dataImporter = Static.loadPlugin(IheRDFDataImporter.class);
+        IheModelConverterDefinition modelConverterDefinition = Static.loadPlugin(IheModelConverterDefinition.class);
+        if (dataImporter.startImport(ModelCategory.TEMPLATE)) {
+            GDCollection gdcoll = dataImporter.getCollection();
+            if (modelConverterDefinition != null) {
+                gdcoll = ModelConverter.convert(modelConverterDefinition, gdcoll);
+            }
+            gdcoll.setModelCategory(ModelCategory.TEMPLATE);
+            return gdcoll;
+        }
+        return null;
+    }
+
+}
