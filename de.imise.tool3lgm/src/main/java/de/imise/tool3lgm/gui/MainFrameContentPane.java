ceee3a572f9c2528633cca4dbf9bdd87e69eb301
Alexander Strübing <alexander.struebing@gmail.com>
2019-09-22 00:37:35 +0200

    MainFrameContenPane: Löschen der ViewParameter nach dem Init wieder einfernt, weil damit das Öffnen von mehr als einem Modell gleichzeitig nicht mehr ging

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/gui/MainFrameContentPane.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/gui/MainFrameContentPane.java
@@ -349,7 +349,6 @@ public class MainFrameContentPane extends JPanel implements PropertyChangeListen
         bgp.setMultiViewLayerGap(view.layerGap);
         bgp.setZoom(view.zoom);
         frame.getScrollPane().getViewport().setViewPosition(new Point(view.viewPositionX, view.viewPositionY));
-        szenario.deleteViewParameter(); // die ViewParameter werden nur für diesen Init gebraucht -> danach können sie gelöscht werden
     }
 
     /**
