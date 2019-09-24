27cc3a9a8a14ee80c86b3eec822e728ae8384c33
Alexander Strübing <alexander.struebing@gmail.com>
2019-09-24 09:35:38 +0200

    InputGraphArea: Änderung rückgängig

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/view/graph/InputGraphArea.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/view/graph/InputGraphArea.java
@@ -372,9 +372,7 @@ public class InputGraphArea extends BasicGraphArea implements MouseListener, Mou
                     grabbedElementsRealRect = getIncludingRectangle(grabbedElementsRealRect, kc);
                 }
             }
-            if (grabbedElementsRasteredRect == null) {
-                grabbedElementsRasteredRect = new Rectangle(grabbedElementsRealRect);
-            }
+            grabbedElementsRasteredRect = new Rectangle(grabbedElementsRealRect);
             if (grabbedElementsFullRect == null) {
                 grabbedElementsFullRect = new Rectangle(grabbedElementsRealRect);
             } else {
