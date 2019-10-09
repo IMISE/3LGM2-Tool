abbf405315172b64e9017342f269214056c41fe8
Alexander Strübing <alexander.struebing@gmail.com>
2019-10-09 11:09:44 +0200

    ApplicationManager: getApplicationDir(String ignoreSubDir) so angelegt, dass ein eventuell zu ignorierendes Unterverzeichnis übergeben werden kann. Dies braucht man, wenn der Start der Apllication nicht aus dem Hauptverzeichnis, sondern eventuell aus einem Unterverzeichnis heraus erfolgt ist (z.B. "lib", wenn man die tool3lgm.jar direkt über einen Doppelklick startet)

--- axsutils/src/main/java/de/imise/util/ApplicationManager.java
+++ axsutils/src/main/java/de/imise/util/ApplicationManager.java
@@ -3,6 +3,8 @@ package de.imise.util;
 import java.io.File;
 import java.io.IOException;
 
+import com.google.common.base.Strings;
+
 /**
  * @author AXS (9 Apr 2019)
  */
@@ -11,16 +13,35 @@ public final class ApplicationManager {
     /**
      * Gibt das Oberste Verzeichnis zurück, in dem sich Anwendungsdaten befinden, also das Installationsverzeichnis.<br>
      *
+     * @param ignoreSubDir falls die Anwendung aus einem Unterverzeichnis heraus gestartet wird, dann wird dieses Unterverzeichnis hier abgeschnitten
+     *            und nur das File auf das Oberverzeichnis zurück gegeben. Das passiert, wenn man die Applikation direkt über die jar-Datei z.B. aus
+     *            dem lib-Verzeichnis startet. Dann muss man eins hoch.
      * @return Pfad zur Anwendung
      */
-    public static File getApplicationDir() {
+    public static File getApplicationDir(final String ignoreSubDir) {
         File f = null;
         try {
             f = new File(".").getCanonicalFile();
+            if (!Strings.isNullOrEmpty(ignoreSubDir)) {
+                String path = f.getAbsolutePath();
+                if (path.endsWith(ignoreSubDir)) {
+                    path = path.substring(0, path.length() - ignoreSubDir.length());
+                    f = new File(path);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return f;
+    }
+
+    /**
+     * Gibt das Oberste Verzeichnis zurück, in dem sich Anwendungsdaten befinden, also das Installationsverzeichnis.<br>
+     *
+     * @return Pfad zur Anwendung
+     */
+    public static File getApplicationDir() {
+        return getApplicationDir("");
     }
 
 }
