5fa1493a330826cfffdd75956002a8da29d8466a
Alexander Strübing <alexander.struebing@gmail.com>
2019-12-16 12:33:52 +0100

    original/Organisationseinheit: hat jetzt auch das Unterpanel analog zur Aufgabe, um über Konfigurationen Anwendungsbausteine zuzuordnen

--- de.imise.metamodel.original/src/main/java/de/imise/tool3lgm/metamodel/original/node/Organisationseinheit.java
+++ de.imise.metamodel.original/src/main/java/de/imise/tool3lgm/metamodel/original/node/Organisationseinheit.java
@@ -3,6 +3,8 @@ package de.imise.tool3lgm.metamodel.original.node;
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
 import de.imise.tool3lgm.graphtools.metamodel.elements.Node;
 import de.imise.tool3lgm.metamodel.original.edge.AufAufOrgVerbindung;
+import de.imise.tool3lgm.metamodel.original.edge.AwbAwbkVerbindung;
+import de.imise.tool3lgm.metamodel.original.edge.AwbkAufOrgVerbindung;
 import de.imise.tool3lgm.metamodel.original.edge.OrgAufOrgVerbindung;
 
 public final class Organisationseinheit extends Node {
@@ -11,6 +13,7 @@ public final class Organisationseinheit extends Node {
     public ElementPropertyDialog createPropertyDialog() {
         ElementPropertyDialog dialog = super.createPropertyDialog();
         dialog.addPathConnectionLeafPanel(OrgAufOrgVerbindung.class, AufAufOrgVerbindung.class);
+        dialog.addPathConnectionPanel(OrgAufOrgVerbindung.class, AwbkAufOrgVerbindung.class, AwbAwbkVerbindung.class);
         return dialog;
     }
 
