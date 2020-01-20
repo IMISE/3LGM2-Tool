a205c2373547b94b722402ad3feb7809439008b2
Alexander Strübing <alexander.struebing@gmail.com>
2020-01-20 11:37:35 +0100

    CommunicationLink_Edge: Zeigt im Eigenschaftsdialog den Tab "Transaktionen" nur an, wenn diese Kommunikationsbeziehung Schnittstellen zwischen 2 ActorInstances verbindet und nicht, wenn sie normale Anwendungssystemschnittstellen verbindet, die gar nicht mit Transaktionen verknüpft sein können

--- de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/edge/CommunicationLink_Edge.java
+++ de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/edge/CommunicationLink_Edge.java
@@ -2,10 +2,20 @@ package de.imise.tool3lgm.metamodel.service.edge;
 
 import static de.imise.tool3lgm.graphtools.metamodel.EdgeCardinality.ZERO_UNLIMITED;
 
+import java.util.Collection;
+
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
 import de.imise.tool3lgm.graphtools.metamodel.EdgeCardinality;
+import de.imise.tool3lgm.graphtools.metamodel.MetaModel;
 import de.imise.tool3lgm.graphtools.metamodel.elements.BooleanAttributeEdge;
 import de.imise.tool3lgm.graphtools.metamodel.elements.ModelElement;
+import de.imise.tool3lgm.graphtools.path.MetaPathFunctions;
+import de.imise.tool3lgm.graphtools.path.meta.ElementaryMetaPath;
+import de.imise.tool3lgm.graphtools.path.meta.ElementaryMetaPathHandler;
+import de.imise.tool3lgm.graphtools.path.meta.SimpleMetaPath;
+import de.imise.tool3lgm.graphtools.path.meta.UnionMetaPath;
+import de.imise.tool3lgm.metamodel.service.node.CommunicationInterface;
+import de.imise.tool3lgm.metamodel.service.node.IheActorInstance;
 import de.imise.tool3lgm.metamodel.service.node.InvokingInterface;
 import de.imise.tool3lgm.metamodel.service.node.ProvidingInterface;
 
@@ -29,9 +39,38 @@ public final class CommunicationLink_Edge extends BooleanAttributeEdge {
     @Override
     public ElementPropertyDialog createPropertyDialog() {
         ElementPropertyDialog dialog = super.createPropertyDialog();
+        if (showTransactionTab()) {
+            dialog.addPathConnectionLeafPanel(IheCommunicationLink_CommunicationLink_Edge.class, IheTransaction_IheCommunicationLink_Edge.class);
+        }
+
         dialog.addEdgePanel(Service_CommunicationLink_Edge.class);
-        dialog.addPathConnectionLeafPanel(IheCommunicationLink_CommunicationLink_Edge.class, IheTransaction_IheCommunicationLink_Edge.class);
         return dialog;
+    }
+
+    private boolean showTransactionTab() {
+        //das Transaktions-Panel nur anzeigen, wenn diese Kommunikationsbeziehung Schnittstellen verbindet,
+        //die zu IheActorInstances gehören und nicht nur zu normalen Anwendungssystemen, denn nur bei
+        //IheActorInsances können überhaupt Transaktionen verknüpft sein
+
+        MetaModel metaModel = getMetaModel();
+        ElementaryMetaPathHandler elementaryMetaPathHandler = metaModel.getElementaryMetaPathHandler();
+        //this = CommunicationLink_Edge -> StartElement = InvokingInterface
+        ElementaryMetaPath communicationLink_StartElement_InvokingInterface = ElementaryMetaPath.createEdgeToStartElementMetaPath(metaModel, CommunicationLink_Edge.class);
+        //this = CommunicationLink_Edge -> EndElement = ProvidingInterface
+        ElementaryMetaPath communicationLink_EndElement_ProvidingInterface = ElementaryMetaPath.createEdgeToEndElementMetaPath(metaModel, CommunicationLink_Edge.class);
+        //CommunicationInterface -> IheActorInstance
+        ElementaryMetaPath communicationInterface_IheActorInstance = elementaryMetaPathHandler.getMetaPath(CommunicationInterface.class, ApplicationComponent_CommunicationInterface_Edge.class, Direction.BACKWARD, IheActorInstance.class);
+        //Gesamtpfad 1: this = CommunicationLink_Edge -> StartElement = InvokingInterface -> IheActorInstance
+        SimpleMetaPath communicationLink_InvokingInterface_IheActorInstance = new SimpleMetaPath(communicationLink_StartElement_InvokingInterface, communicationInterface_IheActorInstance);
+        //Gesamtpfad 2: this = CommunicationLink_Edge -> EndElement = ProvidingInterface -> IheActorInstance
+        SimpleMetaPath communicationLink_ProvidingInterface_IheActorInstance = new SimpleMetaPath(communicationLink_EndElement_ProvidingInterface, communicationInterface_IheActorInstance);
+        //Vereinigungspfad aus Gesamtpfad 1 und Gesamtpfad 2 = Ergebnisselemente sind über beide Einzelpfade verknüpfte IheActorInstances
+        UnionMetaPath communicationLink_Interfaces_IheActorInstances = new UnionMetaPath(communicationLink_InvokingInterface_IheActorInstance, communicationLink_ProvidingInterface_IheActorInstance);
+        //Verknüpfte Elemente holen (multiple ist true, falls beide Schnittstellen zu derselben IheActorInstance gehören
+        Collection<ModelElement> actorInstancesOfCommunicationLink = MetaPathFunctions.getConnectedElements(this, communicationLink_Interfaces_IheActorInstances, true);
+        //über das InvokingInterface muss die verbundene IheActorInstance gefunden worden sein und genauso über das ProvidingInterface
+        // => es müssen 2 IheActorInstances gefunden worden sein, damit der TransactionTab angezeigt wird
+        return actorInstancesOfCommunicationLink.size() == 2;
     }
 
 }
