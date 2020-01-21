24e53ebf34f45e05fefecfd780483dabd9bc7d19
Alexander Strübing <alexander.struebing@gmail.com>
2020-01-21 17:19:59 +0100

    Service/PhysicalDataProcessingComponent: Darstellung der Anwendungsbausteine auf ein Multipanel gelegt und nicht mehr auf 2 getrennte Tabs

--- de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/node/PhysicalDataProcessingComponent.java
+++ de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/node/PhysicalDataProcessingComponent.java
@@ -1,5 +1,6 @@
 package de.imise.tool3lgm.metamodel.service.node;
 
+import static de.imise.tool3lgm.graphtools.dialog.panel.AbstractPathConnectionPanel.PanelLabelOption.LABEL_LAST_EDGE_CONNECTION_NAME;
 import static de.imise.tool3lgm.graphtools.dialog.panel.AbstractPathConnectionPanel.PanelLabelOption.LABEL_LAST_EDGE_ELEMENT_NAME_SINGULAR;
 
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
@@ -23,8 +24,13 @@ public class PhysicalDataProcessingComponent extends Node {
         dialog.addDescripPanel(PhysicalDataProcessingComponent_DeviceClass_Edge.class);
         dialog.addEdgePanel(LABEL_LAST_EDGE_ELEMENT_NAME_SINGULAR, DataTransmissionLink_Edge.class);
         dialog.addEdgePanel(LABEL_LAST_EDGE_ELEMENT_NAME_SINGULAR, PhysicalDataProcessingComponentVirtualises_Edge.class);
-        dialog.addEdgePanel(LABEL_LAST_EDGE_ELEMENT_NAME_SINGULAR, ApplicationComponent_PhysicalDataProcessingComponent_RequiresForFunctionality_Edge.class);
-        dialog.addEdgePanel(LABEL_LAST_EDGE_ELEMENT_NAME_SINGULAR, ApplicationComponent_PhysicalDataProcessingComponent_RequiresForStorage_Edge.class);
+        dialog.addMultiPanel(ApplicationComponent.class);
+        dialog.addMultiPanelEdgePanel(LABEL_LAST_EDGE_CONNECTION_NAME, ApplicationComponent_PhysicalDataProcessingComponent_RequiresForFunctionality_Edge.class);
+        dialog.addMultiPanelEdgePanel(LABEL_LAST_EDGE_CONNECTION_NAME, ApplicationComponent_PhysicalDataProcessingComponent_RequiresForStorage_Edge.class);
+        //Das auskommentierte ist dasselbe wie das MultiPanel, nur in 2 Einzelpanels. Das MultiPanel ist wahrscheinlich besser (analog zur
+        //Gegenrichtung - also dem Dialog der Anwendungssysteme)
+        //        dialog.addEdgePanel(LABEL_LAST_EDGE_ELEMENT_NAME_SINGULAR, ApplicationComponent_PhysicalDataProcessingComponent_RequiresForFunctionality_Edge.class);
+        //        dialog.addEdgePanel(LABEL_LAST_EDGE_ELEMENT_NAME_SINGULAR, ApplicationComponent_PhysicalDataProcessingComponent_RequiresForStorage_Edge.class);
         return dialog;
     }
 
