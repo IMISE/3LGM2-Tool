511a2691892b7d423ea4f5aaabc96ccc6c5a56b4
Alexander Strübing <alexander.struebing@gmail.com>
2020-02-05 06:34:05 +0100

    InternalGraphFrame: equals() und hashCode() -> 2 InternalGraphFrames sind gleich, wenn die dasselbe GraphDocument darstellen

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/gui/InternalGraphFrame.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/gui/InternalGraphFrame.java
@@ -185,4 +185,34 @@ public class InternalGraphFrame extends AbstractInternalFrame implements ActionL
         repaint();
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (doc == null ? 0 : doc.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        InternalGraphFrame other = (InternalGraphFrame) obj;
+        if (doc == null) {
+            if (other.doc != null) {
+                return false;
+            }
+        } else if (!doc.equals(other.doc)) {
+            return false;
+        }
+        return true;
+    }
+
 }
