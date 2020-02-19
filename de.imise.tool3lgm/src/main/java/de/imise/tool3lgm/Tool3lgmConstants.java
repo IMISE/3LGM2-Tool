2b2130f3227e4ecc4a9ea41d5c88cba8834c4e63
Alexander Strübing <alexander.struebing@gmail.com>
2020-02-19 09:34:34 +0100

    Tool3lgmConstants: Version auf 4.2.0 (dev) geändert

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgmConstants.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgmConstants.java
@@ -48,7 +48,7 @@ public abstract class Tool3lgmConstants {
 
     /** String with the version-identifier for Tool3lgm */
     // TODO _____###### ständig aktualisieren!
-    public static final String TOOL_VERSION = "4.1.0 (dev)";
+    public static final String TOOL_VERSION = "4.2.0 (dev)";
     public static final String Branch = ""; //"origin/SplitMetaModelAndTemplates";
 
     /**
