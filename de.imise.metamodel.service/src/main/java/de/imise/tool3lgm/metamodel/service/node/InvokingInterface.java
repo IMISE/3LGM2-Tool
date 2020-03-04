385d90ead9c612bf77d8085ee89253a7186ba405
Alexander Strübing <alexander.struebing@gmail.com>
2020-03-04 21:05:50 +0100

    InvokingIntercae + ProvidingInterface: die nun überflüssige Ausnahmebehandlung, über die Transaktionen der Schnittstellen nur angezeigt wurden, wenn sie zu IheActorInstances gehören wieder entfernt (also Ticket 201 rückgängig gemacht)

--- de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/node/InvokingInterface.java
+++ de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/node/InvokingInterface.java
@@ -1,10 +1,6 @@
 package de.imise.tool3lgm.metamodel.service.node;
 
-import static de.imise.tool3lgm.graphtools.dialog.panel.AbstractPathConnectionPanel.PanelLabelOption.LABEL_LAST_EDGE_CONNECTION_NAME;
-
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
-import de.imise.tool3lgm.metamodel.service.edge.IheInvokingInterface_IheTransaction_Edge;
-import de.imise.tool3lgm.metamodel.service.edge.IheInvokingInterface_IheActorInstanceInvokingInterface_Edge;
 import de.imise.tool3lgm.metamodel.service.edge.Service_InvokingInterface_Edge;
 
 /**
@@ -15,10 +11,6 @@ public final class InvokingInterface extends CommunicationInterface {
     @Override
     public ElementPropertyDialog createPropertyDialog() {
         ElementPropertyDialog dialog = super.createPropertyDialog();
-        if (!getConnectedElements(IheActorInstance.class).isEmpty()) {
-            dialog.addDescripPanel(LABEL_LAST_EDGE_CONNECTION_NAME, IheInvokingInterface_IheActorInstanceInvokingInterface_Edge.class);
-            dialog.addDescripPanel(IheInvokingInterface_IheActorInstanceInvokingInterface_Edge.class, IheInvokingInterface_IheTransaction_Edge.class);
-        }
         dialog.addEdgePanel(Service_InvokingInterface_Edge.class);
         return dialog;
     }
