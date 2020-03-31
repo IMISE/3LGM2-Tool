a42369216e8a7c398d0cba8c865019ed5f22a8ea
Alexander Strübing <alexander.struebing@gmail.com>
2020-03-31 20:43:05 +0200

    ConsistencyCheckSectionMetaPath: prüft im Konstruktor, ob der 2. MetaPfad anlegbar ist, was er sein muss, damit dieser MetaPfad valide ist

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/path/meta/ConsistencyCheckSectionMetaPath.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/path/meta/ConsistencyCheckSectionMetaPath.java
@@ -1,5 +1,7 @@
 package de.imise.tool3lgm.graphtools.path.meta;
 
+import java.util.List;
+
 /**
  * Special {@link SectionMetaPath} for missing path consistency checks. The path always consists
  * of only two sub MetaPaths. The first is an {@link AbstractMetaPath} which leads to all elements
@@ -20,6 +22,9 @@ public class ConsistencyCheckSectionMetaPath extends SectionMetaPath {
      */
     public ConsistencyCheckSectionMetaPath(final String baseResKeyOrName, final AbstractMetaPath firstMetaPathToConnectableElements, final SimpleMetaPath secondSubMetaPathToConnectedElements) {
         super(baseResKeyOrName, firstMetaPathToConnectableElements, secondSubMetaPathToConnectedElements);
+        if (!isCreatable(secondSubMetaPathToConnectedElements)) {
+            throw new IllegalArgumentException(secondSubMetaPathToConnectedElements.getFullName());
+        }
     }
 
     /**
@@ -34,6 +39,32 @@ public class ConsistencyCheckSectionMetaPath extends SectionMetaPath {
      */
     public SimpleMetaPath getSecondSubMetaPathToConnectedElements() {
         return (SimpleMetaPath) subMetaPaths.get(1);
+    }
+
+    /**
+     * Checks if the second metapath is createable. The in the difference to the
+     * {@link SequenceMetaPath#isCreatable(boolean)} this function checks if the
+     * metaPath starts with a combination of two ElemtaryMetaPaths where the second
+     * ElementaryMetaPath is the other direction of the first metapath. If this
+     * condition is fulfilled the metaPaths counts as createable if the sub metapath
+     * from the third ElementaryMetaPath must be createable over
+     * {@link SequenceMetaPath#isCreatable(boolean)}.
+     *
+     * @param secondSubMetaPathToConnectedElements
+     * @return
+     */
+    private boolean isCreatable(final SimpleMetaPath secondSubMetaPathToConnectedElements) {
+        SimpleMetaPath pathToCheckCreatable = secondSubMetaPathToConnectedElements;
+        List<ElementaryMetaPath> elementaryMetaPaths = pathToCheckCreatable.getElementaryMetaPaths();
+        if (elementaryMetaPaths.size() > 2) {
+            ElementaryMetaPath firstElementaryMetaPath = elementaryMetaPaths.get(0);
+            ElementaryMetaPath secondElementaryMetaPath = elementaryMetaPaths.get(1);
+            secondElementaryMetaPath = secondElementaryMetaPath.getOtherDirection();
+            if (firstElementaryMetaPath.equals(secondElementaryMetaPath)) {
+                pathToCheckCreatable = pathToCheckCreatable.getSubPath(1);
+            }
+        }
+        return pathToCheckCreatable.isCreatable(false);
     }
 
 }
