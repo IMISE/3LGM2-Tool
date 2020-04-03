d9c68738c73631209d6ad2672029f80543385ff9
Alexander Strübing <alexander.struebing@gmail.com>
2020-04-03 11:15:08 +0200

    refs #226: ApplicationSystem: das neue InstanciationPathPanel wird von ApplicationSystem benutzt (das hätte schon früher committet werden müssen, um die anderen Commits nachvollziehen zu können)

--- de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/node/ApplicationSystem.java
+++ de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/node/ApplicationSystem.java
@@ -3,6 +3,7 @@ package de.imise.tool3lgm.metamodel.service.node;
 import de.imise.tool3lgm.graphtools.dialog.ElementPropertyDialog;
 import de.imise.tool3lgm.metamodel.service.edge.ApplicationSystem_IheActorInstance_Edge;
 import de.imise.tool3lgm.metamodel.service.edge.ApplicationSystem_SoftwareProduct_Edge;
+import de.imise.tool3lgm.metamodel.service.edge.IheActor_IheActorInstance_Edge;
 
 /**
  * @author AXS (26.12.2017)
@@ -13,9 +14,7 @@ public class ApplicationSystem extends PartableApplicationComponent {
     public ElementPropertyDialog createPropertyDialog() {
         ElementPropertyDialog dialog = super.createPropertyDialog();
         dialog.addEdgePanel(ApplicationSystem_SoftwareProduct_Edge.class);
-        //Den folgenden Pfad unbedingt mit Startelementklasse angeben, damit der Pfad bei der Unterklasse
-        //IheActorInstance nicht angelegt werden kann und das Panel, so wie es richtig ist, nicht geaddet wird.
-        dialog.addEdgePanel(IheActorInstance.class, ApplicationSystem_IheActorInstance_Edge.class);
+        dialog.addPathConnectionPanel(IheActor.class, ApplicationSystem_IheActorInstance_Edge.class, IheActor_IheActorInstance_Edge.class);
         return dialog;
     }
 
