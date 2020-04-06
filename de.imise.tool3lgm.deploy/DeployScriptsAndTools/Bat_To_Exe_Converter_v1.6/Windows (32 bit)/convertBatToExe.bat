16b8c7ca6dc4f8a07b6be3e41679c7dd37a94d60
Alexander Strübing <alexander.struebing@gmail.com>
2020-04-06 12:16:33 +0200

    eben beim Merge aus versehen alle Datein unstaged

--- de.imise.tool3lgm.deploy/DeployScriptsAndTools/Bat_To_Exe_Converter_v1.6/Windows (32 bit)/convertBatToExe.bat
+++ de.imise.tool3lgm.deploy/DeployScriptsAndTools/Bat_To_Exe_Converter_v1.6/Windows (32 bit)/convertBatToExe.bat
@@ -1,2 +1,8 @@
 DEL ..\..\..\..\Tool3lgm\3lgm2tool.exe /s /q
 CALL Bat_To_Exe_Converter.exe -bat ..\..\..\Tool3lgm\3lgm2tool.bat -save ..\..\..\Tool3lgm\3lgm2tool.exe -icon ..\..\Icons\toolIcon_gross.ico -invisible
+
+Zur Info (Unterschiede 1.6 vs. 3.0):
+
+"Bat_To_Exe_Converter_v1.6\Windows (32 bit)"\Bat_To_Exe_Converter.exe -bat ..\Tool3lgm\3lgm2tool.bat -save ..\Tool3lgm\test3.exe -icon Icons\toolIcon_gross.ico -invisble -overide -attributes -display -fileversion "1,0,0,0" -productversion "1,0,0,0" -productname "3lgm2-tool"
+
+Bat_To_Exe_Converter_v3.0.10\Bat_To_Exe_Converter_(x64).exe /bat 3lgm2tool.bat /exe test3.exe /icon ..\DeployScriptsAndTools\Icons\toolIcon_gross.ico /invisble /overide /attributes /display /fileversion "1.0.0.0" /productversion "4.1.1dev" /productname "3lgm2-tool"
--- de.imise.tool3lgm.deploy/Tool3lgm/3lgm2tool.bat
+++ de.imise.tool3lgm.deploy/Tool3lgm/3lgm2tool.bat
@@ -1,19 +1,26 @@
 @echo off
+
 ::change to script directory
 PushD "%~dp0"
+
 ::Max Memory for 32-Bit Systems
 SET MEMORY32=-Xmx512m -Xss20m 
 ::Max Memory for 64-Bit Systems
 SET MEMORY64=-Xmx768m -Xss32m
 ::Set Max Memory to 32-Bit
 SET MEMORY=%MEMORY32%
+
 ::wmic os get osarchitecture -> if return String contains "64" -> Set Max Memory to 64-Bit
 wmic os get osarchitecture | FINDSTR /IL "64" > NUL
 IF %ERRORLEVEL% EQU 0 SET MEMORY=%MEMORY64%
+
 set PATH=%PATH%;.;lib;Plugins
+
 @echo on
 ::java %MEMORY% -jar ./lib/tool3lgm.jar
 java %MEMORY% -classpath ".;./lib/*;./Plugins/*" -splash:splash.gif de.imise.tool3lgm.Tool3lgmMain
+
 pause
+
 ::change to last directory before changing to script directory 
 PopD
