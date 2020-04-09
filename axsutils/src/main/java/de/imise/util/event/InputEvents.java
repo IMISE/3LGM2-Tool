6f831428cb602c2fae5b70c7f13f7c208c9587dc
Alexander Strübing <alexander.struebing@gmail.com>
2020-04-09 10:16:47 +0200

    InputEvents: Funktion boolean isOperatingSystemDependentCTRLorCMDorSHIFTdown() hinzugefügt, die true liefert, wenn CTRL oder CMD (Mac) oder SHIFT gedrückt wird während eines InputEvents

--- axsutils/src/main/java/de/imise/util/event/InputEvents.java
+++ axsutils/src/main/java/de/imise/util/event/InputEvents.java
@@ -27,13 +27,36 @@ public class InputEvents {
      * @return <code>true</code> if the CTRL-Key is pressed on Windows or MacOS or Linux OR the
      *         CMD-Key is pressed on Mac. On MacOS both - CTRL-Key and CMD-Key return <code>true</code>.
      */
-    public static boolean isOperatingSystemDependentCTRLorCMDdown(final InputEvent event) {
+    public static final boolean isOperatingSystemDependentCTRLorCMDdown(final InputEvent event) {
+        return isOperatingSystemDependentCTRLorCMDorSHIFTdown(event, false);
+    }
+
+    /**
+     * @param event
+     * @return <code>true</code> if Shift and/or the CTRL-Key is pressed on Windows or MacOS or Linux OR
+     *         the CMD-Key is pressed on Mac. On MacOS both - CTRL-Key and CMD-Key return <code>true</code>.
+     */
+    public static final boolean isOperatingSystemDependentCTRLorCMDorSHIFTdown(final InputEvent event) {
+        return isOperatingSystemDependentCTRLorCMDorSHIFTdown(event, true);
+    }
+
+    /**
+     * @param event
+     * @param checkShift if <code>true</code> the function also returns <code>true</code> if SHIFT is pressed.
+     *            If <code>false</code> it doesn't matter if SIHFT ist pressed.
+     * @return <code>true</code> if SHIFT and/or the CTRL-Key is pressed on Windows or MacOS or Linux OR
+     *         the CMD-Key is pressed on Mac. On MacOS both - CTRL-Key and CMD-Key return <code>true</code>.
+     */
+    private static final boolean isOperatingSystemDependentCTRLorCMDorSHIFTdown(final InputEvent event, final boolean checkShift) {
         int modifiers = event.getModifiers();
         int ctrlPressed = modifiers & InputEvent.CTRL_MASK;
         if (ctrlPressed == 0) {
             boolean isMacOs = OperatingSystem.isMacOs();
             if (isMacOs) {
                 ctrlPressed = modifiers & InputEvent.META_MASK;
+            }
+            if (ctrlPressed == 0 && checkShift) {
+                ctrlPressed = modifiers & InputEvent.SHIFT_MASK;
             }
         }
         return ctrlPressed != 0;
