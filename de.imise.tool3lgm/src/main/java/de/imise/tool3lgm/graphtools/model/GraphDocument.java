1980dda31a2efbe7059215b338c43b1ebc688586
Alexander Strübing <alexander.struebing@gmail.com>
2020-05-05 13:03:43 +0200

    resolves #113: GraphDocument: Wird ein Pfad zwischen 2 existierenden Elementen angelegt, dann wird die Einfügepostion immer auf die Position des EndElementes gelegt. Da wir im Moment nur einen Pfad zwischen Actor und ApplicationSystem haben, haut das auch immer hin. Bei weiteren Pfaden in Zukunft kann es sein, dass man die Bedingungen erweitern muss (statt EndElement auch das StartElement beachten oder beachten, wo man das Ereignis ausgelöst hat (Grafik oder Baum))

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/model/GraphDocument.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/model/GraphDocument.java
@@ -162,12 +162,12 @@ public abstract class GraphDocument extends ElementSelectionContext {
     public final static int INITIAL_PAGE_WIDTH = 1024;
 
     /**
-     * COMMENTME
+     * X-position of a new created element
      */
     private int next_x_pos = 0;
 
     /**
-     * COMMENTME
+     * Y-position of a new created element
      */
     private int next_y_pos = 0;
 
@@ -1669,6 +1669,23 @@ public abstract class GraphDocument extends ElementSelectionContext {
     public final void setNodeContainerInsertPosition(final int x, final int y) {
         next_x_pos = x;
         next_y_pos = y;
+    }
+
+    /**
+     * Set the insertPosition to the center of the element if it is not null and
+     * has a visible container in this doc.
+     *
+     * @param me
+     */
+    private final void setNodeContainerInsertPosition(final ModelElement me) {
+        if (me != null && me.isPaintable()) {
+            ElementContainer ec = me.getContainer(this);
+            boolean setInserpositionToPathEndElement = ec != null && ec.isVisible();
+            if (setInserpositionToPathEndElement) {
+                GraphElementLayout layout = ec.get3LGMLayout();
+                setNodeContainerInsertPosition(layout.x, layout.y);
+            }
+        }
     }
 
     // --- Layer-Verwaltung --- Ende ---
@@ -3946,6 +3963,13 @@ public abstract class GraphDocument extends ElementSelectionContext {
         //wird das EndElement über diese Kante instanziiert und der Restpfad bis zu dieser Instanz dann wieder über diese Funktion angelegt
         List<ElementaryMetaPath> elementaryMetaPaths = metaPath.getElementaryMetaPaths();
         boolean createSubPath = false;
+
+        //set the insertPosition to the center of the end element if existst and is visible
+        //maybe this is not correct for future paths, but now (05.05.2020) there is only a
+        //path between an actor and an application system and the actor instance should be
+        //created in the center of the application system
+        setNodeContainerInsertPosition(endElement);
+
         if (lastPathStepIndex > 0 && endElement != null) {
             ElementaryMetaPath lastElementaryMetaPath = elementaryMetaPaths.get(lastPathStepIndex);
             if (lastElementaryMetaPath.hasEdgeClass(InstanciationEdge.class)) {
