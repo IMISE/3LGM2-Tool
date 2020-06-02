2a88a5367fc7566537577f7d31859c2a5709f945
Alexander Strübing <alexander.struebing@gmail.com>
2020-06-02 23:27:45 +0200

    ModelBrowser: anscheinenden Java-Bug umgangen, der Auftritt, wenn man die Ansicht des ModelBrowsers mehrfach von "Zeige Teilmodelle jeweils in eigenem Browser" auf "Zeige alle Teilmodelle in einem Browser" umstellt

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/view/browser/ModelBrowser.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/view/browser/ModelBrowser.java
@@ -215,4 +215,19 @@ public final class ModelBrowser extends TabbedPane implements ChangeListener, Fo
     public void focusLost(final FocusEvent e) {
     }
 
+    @Override
+    public void setForegroundAt(final int index, final Color foreground) {
+        //If we change the option showSubmodelsInSeparateBrowsers 3 or 4 times
+        //if there are two models opened and the first ist active then there is
+        //fired a java.lang.ArrayIndexOutOfBoundsException: 1 >= 1 at
+        //java.desktop/javax.swing.plaf.basic.BasicTabbedPaneUI.getTextViewForTab(BasicTabbedPaneUI.java:1959).
+        //Hhis seems to be a java bug.
+        //If we sourround this bug with try-catch everything is fine and it works.
+        try {
+            super.setForegroundAt(index, foreground);
+        } catch (Exception e) {
+            // ignore
+        }
+    }
+
 }
