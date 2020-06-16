b7e2a42e99e0fa1ee10771ab44581c6545b5404f
Alexander Strübing <alexander.struebing@gmail.com>
2020-06-16 14:43:15 +0200

    SimpleResourceSource: Comments + Refactoring rename

--- axsutils/src/main/java/de/imise/util/resource/SimpleResourceSource.java
+++ axsutils/src/main/java/de/imise/util/resource/SimpleResourceSource.java
@@ -3,21 +3,25 @@ package de.imise.util.resource;
 import com.google.common.base.Strings;
 
 /**
+ * Interface that provides a default function which returns a resource file name
+ * for a class package. Optionally this name can be extended by an arbitrary string.
+ *
  * @author AXS (19.05.2020)
  */
 public interface SimpleResourceSource {
 
     /**
-     * @param ressourcePackageNameSource
+     * @param resourcePackageNameSource
      * @param resourceName
-     * @return
+     * @return a resource file name for a class package. Optionally this name can be
+     *         extended by an arbitrary string.
      */
-    public default String getResourceFileName(Class<?> ressourcePackageNameSource, final String resourceName) {
+    public default String getResourceFileName(Class<?> resourcePackageNameSource, final String resourceName) {
         boolean appendSimpleName = !Strings.isNullOrEmpty(resourceName);
-        if (ressourcePackageNameSource == null) {
-            ressourcePackageNameSource = getClass();
+        if (resourcePackageNameSource == null) {
+            resourcePackageNameSource = getClass();
         }
-        String resourceFileName = !appendSimpleName ? ressourcePackageNameSource.getName() : ressourcePackageNameSource.getPackage().getName();
+        String resourceFileName = !appendSimpleName ? resourcePackageNameSource.getName() : resourcePackageNameSource.getPackage().getName();
         if (appendSimpleName) {
             resourceFileName += "." + resourceName;
         }
