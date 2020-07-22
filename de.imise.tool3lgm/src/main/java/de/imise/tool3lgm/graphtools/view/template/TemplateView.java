6718dc8ec012a47346259d012500d26c837bc052
astruebi <alexander.struebing@gmail.com>
2020-07-22 21:03:47 +0200

    resolves #311: MainFrameDesktopPane + TemplateBrowserPanel + TemplateBrowserTree + TemplateView: Ist ein Element aus einem Template element entstanden, dann wird bei Selektion dieses Elementes im Modell jetzt das Ursprungselement im Template selektiert. Das hat 2 Aspekte: Ersten direkt kopierte Elemente werden auch im TemplateBrowser selektiert und bei durch InstanciationEdges entstandenen Elementen (z.B. Ableitung von IheActorInstances aus IheActors) werden  auch die Ursprungselemente (also die IheActors) selektiert.

---
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/view/template/TemplateView.java
@@ -0,0 +1,25 @@
+package de.imise.tool3lgm.graphtools.view.template;
+
+import java.util.Collection;
+
+import de.imise.tool3lgm.graphtools.model.GDCollection;
+
+/**
+ * Interface for all views of templates
+ *
+ * @author Ich (22.07.2020)
+ */
+public interface TemplateView {
+
+    /**
+     * @return all template models which are visible in this template view
+     */
+    public Collection<GDCollection> getDisplayedTemplates();
+
+    /**
+     * Displays the selection from the templates in the view.
+     *
+     * @param selectionSource
+     */
+    public void updateSelection();
+}
