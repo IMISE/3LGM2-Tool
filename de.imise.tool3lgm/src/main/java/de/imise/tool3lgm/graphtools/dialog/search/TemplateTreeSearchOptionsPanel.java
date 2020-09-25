abc2e8d343d672e5f1d956cb54b4ed018a602919
hksacc <hks_email@yahoo.com>
2020-09-25 12:34:35 +0200

    refs #99: auf und zuklappen des panels funktioniert erstmal, jedoch nur rudimentär. Commits wurden gemerged

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/search/TemplateTreeSearchOptionsPanel.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/search/TemplateTreeSearchOptionsPanel.java
@@ -1,12 +1,13 @@
-/**
- *
- */
 package de.imise.tool3lgm.graphtools.dialog.search;
 
 import static de.imise.tool3lgm.Tool3lgmConstants.getResString;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
 
@@ -15,11 +16,17 @@ import de.imise.tool3lgm.graphtools.metamodel.elements.ModelElement;
 import de.imise.tool3lgm.graphtools.view.pathtree.PathTreeDefinition;
 import de.imise.tool3lgm.graphtools.view.pathtree.PathTreeModel;
 import de.imise.tool3lgm.graphtools.view.template.TemplateBrowserTree;
+import de.imise.util.Sys;
 
 /**
  * @author AXS (31.07.2020)
  */
 public class TemplateTreeSearchOptionsPanel extends BasicSearchOptionsPanel {
+
+    private final JPanel filterOptionPanel = new JPanel(new GridLayout(0, 2));
+    private final JButton expandOptions;
+    private static boolean expanded;
+    GridBagConstraints c = new GridBagConstraints();
 
     /**
      * Stores the SearchOptions to restore them if
@@ -32,9 +39,14 @@ public class TemplateTreeSearchOptionsPanel extends BasicSearchOptionsPanel {
      */
     public TemplateTreeSearchOptionsPanel(final TemplateBrowserTree tree) {
         super(tree, new BorderLayout());
+        expanded = false;
+        expandOptions = new JButton(">>");
 
         add(searchButton, BorderLayout.WEST);
         add(elementName, BorderLayout.CENTER);
+        add(expandOptions, BorderLayout.EAST);
+        add(filterOptionPanel, BorderLayout.PAGE_END);
+        expandOptions.addActionListener(arg0 -> showFullPanel(expanded));
 
         addListenerToRestoreSearchOptions();
     }
@@ -65,7 +77,56 @@ public class TemplateTreeSearchOptionsPanel extends BasicSearchOptionsPanel {
             }
 
         });
+    }
 
+    /**
+     *
+     */
+    public void addExpandOptionsButtonListener() {
+
+    }
+
+    /**
+     * @param full
+     */
+    public final void showFullPanel(final boolean full) {
+        if (expanded) {
+            showPartlyPanel();
+
+        } else {
+            showFullPanel();
+        }
+        revalidate();
+        repaint();
+    }
+
+    /**
+     *
+     */
+    protected void showFullPanel() {
+        Sys.out1("pushed");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 2;
+        c.gridy = 0;
+        filterOptionPanel.add(checkNameCaseSensitive);
+        filterOptionPanel.add(elementDescription);
+        filterOptionPanel.add(checkDescriptionCaseSensitive);
+        filterOptionPanel.add(elementClassBox);
+
+        expanded = true;
+    }
+
+    /**
+     *
+     */
+    protected void showPartlyPanel() {
+        filterOptionPanel.remove(checkNameCaseSensitive);
+        filterOptionPanel.remove(elementDescription);
+        filterOptionPanel.remove(checkDescriptionCaseSensitive);
+        filterOptionPanel.remove(elementClassBox);
+
+        expanded = false;
     }
 
     /**
