9128c7e277cf304946b271c817aad0e7cb655c5d
astruebi <alexander.struebing@gmail.com>
2020-10-06 11:12:04 +0200

    Tool3lgmConstants: liefert eine Implementierung des Interfaces SimpleResourceBundleSource, die die statsische Instanz der Tool3lgmConstants selbst kapselt. Diese kann man jetzt den Utils-Klassen übergeben, so dass sie auch (ohne konkret die Constants kennen zu müssen) Resourcenstrings laden können.

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgmConstants.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/Tool3lgmConstants.java
@@ -26,6 +26,7 @@ import de.imise.tool3lgm.userproperties.UserProperties;
 import de.imise.util.ApplicationManager;
 import de.imise.util.StringUtils;
 import de.imise.util.collections.CollectionUtils;
+import de.imise.util.resource.SimpleResourceBundleSource;
 import de.imise.util.resource.SimpleResourceIconSource;
 
 /**
@@ -241,6 +242,18 @@ public abstract class Tool3lgmConstants {
      * für die Sanduhr...
      */
     protected static Cursor normalCursor = new Cursor(Cursor.DEFAULT_CURSOR), waitCursor = new Cursor(Cursor.WAIT_CURSOR), handCursor = new Cursor(Cursor.HAND_CURSOR);
+
+    /**
+     * Delegate object to this which can be used by classes
+     * which load the resource strings via the interface
+     * {@link SimpleResourceBundleSource}
+     */
+    public static final SimpleResourceBundleSource RESOURCE_BUNDLE_SOURCE = new SimpleResourceBundleSource() {
+        @Override
+        public String getResString(final String resKey) {
+            return Tool3lgmConstants.getResString(resKey);
+        }
+    };
 
     /**
      * Liefert <code>true</code>, wenn der übergebene String eine Extension eines {@link FileNameExtensionFilter} in
