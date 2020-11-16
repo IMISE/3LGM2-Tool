3257bd3bfe43ceeb9d02ebe2562f173efd947409
astruebi <alexander.struebing@gmail.com>
2020-11-16 12:55:13 +0100

    #320: BasicSearchOptionPanel: in der Auswahlbocx der Elementarten ummer nur diejenigen Anzeigen, die je nach Option "Expertenmodus" sichbar sein sollen oder nicht.

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/search/BasicSearchOptionsPanel.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/search/BasicSearchOptionsPanel.java
@@ -223,7 +223,6 @@ public abstract class BasicSearchOptionsPanel extends JPanel implements ItemList
      * Befüllt die elementClassBox
      */
     protected void fillElementClassBox() {
-
         elementClassBox.removeAllItems();
         elementClassBox.addObject(ModelElement.class, getResString("SEARCH_DIALOG_all_element_types"));
         elementClassBox.addSeparator(true);
@@ -236,6 +235,9 @@ public abstract class BasicSearchOptionsPanel extends JPanel implements ItemList
             ElementsNameBuilder elementsNameBuilder = gdcoll.getElementsNameBuilder();
             for (Class<? extends ModelElement> elementClass : metaModel.allNodesSet) {
                 if (Modifier.isAbstract(elementClass.getModifiers())) {
+                    continue;
+                }
+                if (!Static.isExpertMode() && metaModel.isOnlyExpertModeVisibleElementClass(elementClass)) {
                     continue;
                 }
                 elementClassBox.addObject(elementClass, elementsNameBuilder.getDisplayableFullName(elementClass));
