7d08e37c96ce2ae9925a048ccfe370f5371f3f80
astruebi <alexander.struebing@gmail.com>
2021-02-03 08:47:08 +0100

    resolves #412: GraphDocument: Ausrichtung der Schrift in der Grafik ist nach dem Einlesen genauso, wie beim Speichern und nicht mehr immer zentriert

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/model/GraphDocument.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/model/GraphDocument.java
@@ -5283,13 +5283,16 @@ public abstract class GraphDocument extends ElementSelectionContext implements G
      */
     public void initNodeContainers() {
         for (int i = 0; i < layer.length; i++) {
-            for (NodeContainer kc : layer[i].getGraphNodeContainers()) {
-                if (kc != null) {
-                    kc.refreshText();
-                    Font f = kc.getFont();
-                    if (!kc.isStandardFont(f)) {
-                        kc.setFont(f);
+            for (NodeContainer nc : layer[i].getGraphNodeContainers()) {
+                if (nc != null) {
+                    ModelElement me = nc.getElement();
+                    me.updateHTMLName(nc);
+                    nc.refreshText();
+                    Font f = nc.getFont();
+                    if (!nc.isStandardFont(f)) {
+                        nc.setFont(f);
                     }
+
                 }
             }
             layer[i].revalidate();
