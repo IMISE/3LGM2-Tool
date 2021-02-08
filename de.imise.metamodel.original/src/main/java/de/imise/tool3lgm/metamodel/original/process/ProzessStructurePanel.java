0572a683413c16646336631fbee613072345de8d
astruebi <alexander.struebing@gmail.com>
2021-02-08 22:00:03 +0100

    refs #435: ProzessStructurePanel: so angepasst, dass es erstmal wieder vernünftig aussieht.

--- de.imise.metamodel.original/src/main/java/de/imise/tool3lgm/metamodel/original/process/ProzessStructurePanel.java
+++ de.imise.metamodel.original/src/main/java/de/imise/tool3lgm/metamodel/original/process/ProzessStructurePanel.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComponent;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTree;
@@ -28,6 +29,7 @@ import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 
+import de.imise.tool3lgm.Static;
 import de.imise.tool3lgm.graphtools.dialog.action.ActionNotDefinedForClassException;
 import de.imise.tool3lgm.graphtools.dialog.action.LGMAction;
 import de.imise.tool3lgm.graphtools.dialog.action.LGMActionLibrary;
@@ -59,6 +61,7 @@ import de.imise.tool3lgm.graphtools.view.tree.node.LGMTreeNode;
 import de.imise.tool3lgm.graphtools.view.tree.node.StringTreeNode;
 import de.imise.tool3lgm.metamodel.original.edge.PrzAufVerbindung;
 import de.imise.tool3lgm.metamodel.original.node.Aufgabe;
+import de.imise.util.swing.dialog.GridBagConstrainsInputPanel;
 
 /**
  * 24.10.2018: Dieses Panel funktioniert im Moment überhaupt nicht mehr richtig.
@@ -171,7 +174,7 @@ public class ProzessStructurePanel extends PathConnectionPanel implements TreeWi
         successorConditionMetaPath = getConditionElementaryMetaPath(doubleMeaningEdgeClass, ConnectionState.BACKWARD);
 
         // Panel für die Buttons zur Aenderung der Aufgabenreihenfolge anlegen
-        JPanel upDownControl = new JPanel(new GridLayout(1, 2));
+        JPanel upDownButtonsPanel = new JPanel(new GridLayout(1, 2));
 
         /*
          * Start: Buttons & Actions erstellen, Actions setzen ...
@@ -188,24 +191,16 @@ public class ProzessStructurePanel extends PathConnectionPanel implements TreeWi
          * ... end: Buttons & Actions erstellen, Actions setzen
          */
 
-        upDownControl.add(upButton);
-        upDownControl.add(downButton);
+        upDownButtonsPanel.add(upButton);
+        upDownButtonsPanel.add(downButton);
 
         // Panel für die Buttons zur Aenderung der Aufgabenreihenfolge
         // hinzufügen
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.CENTER;
-        constraints.fill = GridBagConstraints.NONE;
-        constraints.weightx = 0;
-        constraints.weighty = 0;
-        //Windows-spezifisch
-        //        constraints.ipadx = -60;
-        // constraints.ipady = - 10;
-        addUnderLeftTree(upDownControl, constraints);
+        add(upDownButtonsPanel, constraints, 1, 5, 1, 1, false);
 
-        /*
-         * Start: Aktionen für Verifikation und für FehlerButton setzen ...
-         */
+        // Start: Aktionen für Verifikation und für FehlerButton setzen ...
         try {
             verificationCheck = new JCheckBox();
             verificationCheck.setSelected(verify);
@@ -221,26 +216,43 @@ public class ProzessStructurePanel extends PathConnectionPanel implements TreeWi
         } catch (ActionNotDefinedForClassException andfce) {
             andfce.printStackTrace();
         }
-        /*
-         * End: Aktionen für Verifikation und für FehlerButton setzen
-         */
-
+        // End: Aktionen für Verifikation und für FehlerButton setzen
         constraints.anchor = GridBagConstraints.WEST;
         constraints.fill = GridBagConstraints.NONE;
-        // constraints.weightx = 0;
-        // constraints.weighty = 0;
-        constraints.ipadx = 0;
-        constraints.ipady = 0;
         JPanel tmpPanel = new JPanel();
         tmpPanel.add(verificationCheck);
 
         tmpPanel.add(errorBut);
         errorBut.setVisible(verify);
-
-        addSouth(tmpPanel, constraints, 2);
+        //AXS: 08.02.2021: erstmal das Verifizieren wieder weglassen, weil das sowieso nicht richtig funktioniert -> ProzessTickets -> wieder aktivieren!
+        //add(tmpPanel, constraints, 0, 6, 2, 1, false);
 
         update();
 
+    }
+
+    /**
+     * @param component
+     * @param constraints
+     * @param x
+     * @param y
+     * @param w
+     * @param h
+     * @param debugDialog use only <code>true</code> here while development! If
+     *            <code>true</code> there will be a dialog before the add where
+     *            you can change all values of the current
+     *            {@link GridBagConstraints}
+     */
+    private final void add(final JComponent component, final GridBagConstraints constraints, final int x, final int y, final int w, final int h, final boolean debugDialog) {
+        constraints.gridx = x;
+        constraints.gridy = y;
+        constraints.gridwidth = w;
+        constraints.gridheight = h;
+        if (debugDialog) {
+            GridBagConstrainsInputPanel.setConstraintsWithDialog(Static.getMainFrame(), constraints);
+        }
+        add(component, constraints);
+        setSamePreferredLeftRightSize(component);
     }
 
     /**
