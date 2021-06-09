03712c3426f5284fabdb8390f530de297d5fc78f
astruebi <alexander.struebing@gmail.com>
2021-06-09 07:35:36 +0200

    #387: PreviewElementPropertyDialogCreator: Comments

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/element/PreviewElementPropertyDialogCreator.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/dialog/element/PreviewElementPropertyDialogCreator.java
@@ -41,8 +41,9 @@ import de.imise.tool3lgm.graphtools.view.tree.node.LGMTreeNode;
 public class PreviewElementPropertyDialogCreator {
 
     /**
-    *
-    */
+     * We need the last dividr location of the splitpane to restore it after
+     * changing the content components in the splitpane.
+     */
     private static int lastDividerLocation = -1;
 
     /**
@@ -51,23 +52,24 @@ public class PreviewElementPropertyDialogCreator {
     private final JSplitPane splitPane;
 
     /**
-     *
+     * The modelbrowser tree that dislplays the dummy elements
      */
     private final ModelBrowserTree modelBrowserTree;
 
     /**
-     *
+     * The last selected/displayed modelelement to detect the selection changed
      */
     private ModelElement lastDisplayedDialogElement = null;
 
     /**
-     *
+     * The content component of the element's dialog without sybtypes. Its
+     * dialog is used to display the content components also of the elements
+     * with subtypes. The content components are taken from its dialog and put
+     * back there when another element is to be selected and displayed.
      */
     private JComponent standardElementDialogContent;
 
-    /**
-     *
-     */
+    /** Border title for all dialog content components */
     private final String rightBorderTitle;
 
     /**
@@ -80,16 +82,23 @@ public class PreviewElementPropertyDialogCreator {
         DummyGDCollection dummyGDCollection = new DummyGDCollection(modelType);
         dummyGDCollection.setUserFieldDefinitions(definitions);
         rightBorderTitle = dummyGDCollection.getResString("eigensch_dial");
+
+        //create a dummy element for every subtype
         for (SubType subType : definitions.getSubTypes(elementClass)) {
             dummyGDCollection.createElement(elementClass, subType);
         }
+
+        //create a dummy element without a subtype (this elemnet is alway created)
         ModelElement me = dummyGDCollection.createElement(elementClass, DUMMY_SUBTYPE);
         LGMGraphDocument mainDoc = dummyGDCollection.getMainDoc();
+        //select the element without subtype in the dummy model
         mainDoc.addToSelection(me.getID(), STANDARD_PID);
+        //init model broweser an split pane
         modelBrowserTree = new ModelBrowserTree(mainDoc);
-        modelBrowserTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        //init the dialog with the property dialog of the element without subtype
         ElementPropertyDialog previewDialog = initPreviewDialog(dummyGDCollection);
+        //show the dialog
         previewDialog.showDialog();
         TreePath selectionPath = modelBrowserTree.getSelectionPath();
         modelBrowserTree.scrollPathToVisible(selectionPath);
@@ -133,31 +142,49 @@ public class PreviewElementPropertyDialogCreator {
         ElementContainer ec = mainDoc.getLastSelected();
         lastDisplayedDialogElement = ec.getElement();
 
+        //we use the dialog of the element without subtypes and replace only the
+        //dialog content if a subtyped element is selected
         ElementPropertyDialog originalDialog = lastDisplayedDialogElement.getPropertyDialog();
         originalDialog.setModalityType(ModalityType.APPLICATION_MODAL);
+        //dialogs title -> "Preview"
+        String dialogTitle = dummyGDColl.getResString("previewText");
+        originalDialog.setTitle(dialogTitle);
+
+        //resize the dialog to get space display the modelbrowser
         if (originalDialog.hasDefaultSize()) {
             originalDialog.resize(1.8d, 1.0d);
         }
 
-        String dialogTitle = dummyGDColl.getResString("previewText");
-        originalDialog.setTitle(dialogTitle);
-
+        //init model browser sctollpane) and titled border
         JScrollPane modelBrowserTreeScrollPane = new JScrollPane(modelBrowserTree);
         String modelBrowserPanelTitle = dummyGDColl.getResString("PANEL_LABEL_MODEL_BROWSER_TITLE");
         modelBrowserTreeScrollPane.setBorder(BorderFactory.createTitledBorder(modelBrowserPanelTitle));
 
+        //subtype elements have their dialog an we can put the content
+        //back in the dialog to reuse it if the element is selected again
+        //but we use here the dialog of the element without subtypes so
+        //we must store the dialog content
         standardElementDialogContent = getDialogContent(originalDialog);
 
+        //add the selection listener to the modelbrowser that updates
+        //the dialog content component if the selected element in the
+        //modelbrowser has changed
         SampleModelBrowserSelectionListener.addModelBrowserTreeSelectionListener(this);
         modelBrowserTree.expandAll();
         modelBrowserTree.selectObjects();
 
+        //replace the dialogs content by the splitpane with the modelbrowser
+        //at the left and the original element property dialog components
+        //at the rigtht
         Container contentPane = originalDialog.getContentPane();
         contentPane.removeAll();
 
         splitPane.setLeftComponent(modelBrowserTreeScrollPane);
         splitPane.setRightComponent(standardElementDialogContent);
         contentPane.add(splitPane, BorderLayout.CENTER);
+
+        //first start of the preview = static lastDividerLocation < 0
+        //so give the modelbrowser one third
         if (lastDividerLocation < 0) {
             lastDividerLocation = Double.valueOf(0.3d * originalDialog.getWidth()).intValue();
         }
@@ -192,6 +219,8 @@ public class PreviewElementPropertyDialogCreator {
                     ElementContainerTreeNode elementTreeNode = (ElementContainerTreeNode) node;
                     ModelElement me = elementTreeNode.getModelElement();
                     if (me != lastDisplayedDialogElement) {
+                        //for subtype elements we must replace the dialogs content in the
+                        //dialog to get it back if the same element will be selected again
                         if (lastDisplayedDialogElement.hasSubType()) {
                             Component rightComponent = splitPane.getRightComponent();
                             ElementPropertyDialog propertyDialog = lastDisplayedDialogElement.getPropertyDialog();
@@ -199,10 +228,13 @@ public class PreviewElementPropertyDialogCreator {
                         }
                         lastDisplayedDialogElement = me;
                         JComponent dialogContent;
+                        //from subtype elements we take the dialog content
                         if (me.hasSubType()) {
                             ElementPropertyDialog dialog = me.getPropertyDialog();
                             dialogContent = getDialogContent(dialog);
                         } else {
+                            //the full dialog is the dialog of the element without subtypes -> set
+                            //this special stored dialog content component again as dialogs content
                             dialogContent = standardElementDialogContent;
                         }
                         int dividerLocation = lastDividerLocation;
@@ -231,6 +263,9 @@ public class PreviewElementPropertyDialogCreator {
         private SampleModelBrowserSelectionListener(final PreviewElementPropertyDialogCreator dialogCreator) {
             this.dialogCreator = dialogCreator;
             ModelBrowserTree modelBrowserTree = dialogCreator.modelBrowserTree;
+            //only single selection is allowed
+            modelBrowserTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+            //remove all other selection liseteners which are isntalled by default for the modelbrowser tree
             for (TreeSelectionListener listener : modelBrowserTree.getTreeSelectionListeners()) {
                 modelBrowserTree.removeTreeSelectionListener(listener);
             }
