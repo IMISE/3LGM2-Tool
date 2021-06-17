b1f969c499812b83e014ca9e8bf30b90cfda99c5
astruebi <alexander.struebing@gmail.com>
2021-06-17 21:29:13 +0200

    #477: ToolContentHandlerV3_0: Copy&Paste war nach dem letzte Commit doch nicht nur vereinfacht, sondern nicht mehr genz korrekt. Nun merkt er sich im Copy&Paste-Fall die zu ersetzenden IDs nur noch , wenn sie tatsächlich anders sind. Damit weiß man dann auch, welche Elemente nicht einfach nur eingefügt wurden, sondern echte Kopien mit einer anderen ID sind.

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/xml/ToolContentHandlerV3_0.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/xml/ToolContentHandlerV3_0.java
@@ -213,6 +213,7 @@ public class ToolContentHandlerV3_0 implements ContentHandler {
 
     @Override
     public void startDocument() throws SAXException {
+        oldToNewID = new HashMap<>();
     }
 
     /**
@@ -316,14 +317,13 @@ public class ToolContentHandlerV3_0 implements ContentHandler {
                 }
             } else if (qName.equals("container")) {
                 String id = atts.getValue("hash");
-                if (isCopyAndPaste()) {
-                    String newId = oldToNewID.get(id);
-                    element = mainDoc.findElementCoded(newId);
-                } else {
-                    element = mainDoc.findElementCoded(id);
+                String newID = oldToNewID.get(id);
+                if (newID != null) {
+                    id = newID;
                 }
+                element = mainDoc.findElementCoded(id);
                 if (element == null) {
-                    ElementContainer bendpointContainerOrTexField = idToMainDocContainer.get(isCopyAndPaste() ? oldToNewID.get(id) : id);
+                    ElementContainer bendpointContainerOrTexField = idToMainDocContainer.get(id);
                     if (bendpointContainerOrTexField != null) {
                         element = bendpointContainerOrTexField.getElement();
                     }
@@ -513,7 +513,6 @@ public class ToolContentHandlerV3_0 implements ContentHandler {
                 mainDoc = Static.getSelectedGDCollection().getMainDoc();
                 szenario = gdcoll.getSelectedDoc();
                 szenario.clearSelection();
-                oldToNewID = new HashMap<>();
 
             } else if (qName.equals("objects")) {
                 Static.setProgressDialogStatusLabel("labelReadElements");
@@ -857,8 +856,9 @@ public class ToolContentHandlerV3_0 implements ContentHandler {
                     Bendpoint bendpoint = benpointContainer.getBendpoint();
                     String bendpointEdgeID = bendpoint.getEdgeID();
 
-                    if (isCopyAndPaste()) {
-                        bendpointEdgeID = oldToNewID.get(bendpointEdgeID);
+                    String newBendpointEdgeID = oldToNewID.get(bendpointEdgeID);
+                    if (newBendpointEdgeID != null) {
+                        bendpointEdgeID = newBendpointEdgeID;
                     }
                     EdgeContainer kc = benpointContainer.getGraphDocument().findEdgeContainerCoded(bendpointEdgeID);
                     if (kc != null) {
