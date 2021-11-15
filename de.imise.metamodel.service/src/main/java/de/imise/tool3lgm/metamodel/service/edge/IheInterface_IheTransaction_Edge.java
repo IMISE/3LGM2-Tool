b9391a42ed798e62b414e717eb559e647e0df6f3
Alexander Strübing <alexander.struebing@gmail.com>
2021-11-15 21:07:40 +0100

    Actors aus Templates haben wieder Schnittstellen
    Bug behoben. Das Löschen der Schnittstellen lag ganz einfach an der "falschen" Kardinalität, so dass eine Schnittstelle im Template immer mit einer Transaktion verbunden sein musste, damit sie nicht invalid ist. Nun ist die Kardinalität von 1..1 auf 0..1 abgeschwächt.
    Der 2. Bug bestand darin, dass beim Löschen der InferenceEdges immer auch der Bedingungspfad gelöscht wurde, selbst wenn die InferenceEdges nur temporär entfernt werden sollten, damit sie während eines Kopiervorganes nicht im Modell sind und gleich danach wieder hinzugefügt werden sollten. Nun werden die Bedingungspfade nur gelöscht, wenn der Benutzer eine InferneceEdge explizit löscht.

--- de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/edge/IheInterface_IheTransaction_Edge.java
+++ de.imise.metamodel.service/src/main/java/de/imise/tool3lgm/metamodel/service/edge/IheInterface_IheTransaction_Edge.java
@@ -1,13 +1,13 @@
 package de.imise.tool3lgm.metamodel.service.edge;
 
-import static de.imise.tool3lgm.graphtools.metamodel.EdgeCardinality.ONE_ONE;
+import static de.imise.tool3lgm.graphtools.metamodel.EdgeCardinality.ZERO_ONE;
 import static de.imise.tool3lgm.graphtools.metamodel.EdgeCardinality.ZERO_UNLIMITED;
 
 import de.imise.tool3lgm.graphtools.metamodel.EdgeCardinality;
 import de.imise.tool3lgm.graphtools.metamodel.elements.ModelElement;
 import de.imise.tool3lgm.graphtools.metamodel.elements.SimpleEdge;
 import de.imise.tool3lgm.metamodel.service.node.IheInterface;
-import de.imise.tool3lgm.metamodel.service.node.IheTransaction;;
+import de.imise.tool3lgm.metamodel.service.node.IheTransaction;
 
 /**
  * @author AXS (05.02.2019)
@@ -18,7 +18,7 @@ public abstract class IheInterface_IheTransaction_Edge extends SimpleEdge {
 
     public static final EdgeCardinality SCARD = ZERO_UNLIMITED;
 
-    public static final EdgeCardinality ECARD = ONE_ONE;
+    public static final EdgeCardinality ECARD = ZERO_ONE;
 
     public static final Class<? extends ModelElement> ETCL = IheTransaction.class;
 
