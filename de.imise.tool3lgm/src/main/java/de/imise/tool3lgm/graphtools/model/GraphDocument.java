54f0be80edb71e6adacc48df913dcab353904519
Alexander Strübing <alexander.struebing@gmail.com>
2022-03-07 12:40:51 +0100

    fix #551 Undo-Redo works for Changes of UserField Values

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/model/GraphDocument.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/model/GraphDocument.java
@@ -4881,15 +4881,21 @@ public abstract class GraphDocument extends ElementSelectionContext implements G
      * @param value
      * @param pid
      */
-    public void setUserFieldValue(final ModelElement me, final UserField userField, final String value, final int pid) {
+    public void setUserFieldValue(final ModelElement me, final UserField userField, String value, final int pid) {
         start_transaction(pid);
-        String newValue = getParseSaveString(value, true);
+        if (userField.hasClassfificationStyle()) {
+            value = value.trim();
+        }
+        //String newValue = getParseSaveString(value, true);
         String oldValue = me.getValue(userField);
-        oldValue = getParseSaveString(oldValue, true);
-        addRedoCommandOrReplace(pid, newValue, MODEL_ACTION_SET_USER_FIELD_VALUE, me, userField);
+        if (UserField.EMPTY_STRING.equals(oldValue)) {
+            oldValue = "";
+        }
+        //oldValue = getParseSaveString(oldValue, true);
+        addRedoCommandOrReplace(pid, value, MODEL_ACTION_SET_USER_FIELD_VALUE, me, userField);
         addUndoCommandIfNotExist(pid, oldValue, MODEL_ACTION_SET_USER_FIELD_VALUE, me, userField);
-        newValue = getDecodedParseSaveString(value);
-        me.setUserFieldInputValue(userField, newValue);
+        //newValue = getDecodedParseSaveString(value);
+        me.setUserFieldInputValue(userField, value);
         finish_transaction(pid, USER_FIELD_VALUE_CHANGED);
     }
 
