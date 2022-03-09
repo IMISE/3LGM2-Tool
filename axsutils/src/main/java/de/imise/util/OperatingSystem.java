7fac3f86be4b9ec1e268977c70c8d60d31d52104
Alexander Strübing <alexander.struebing@gmail.com>
2022-03-09 20:15:27 +0100

    refs #570 (Should) Fix Crash on M1 Macs (untested)
    The old Apple Library  "AppleJavaExtensions" that works fine for Intel Macs to set the Dock icon of the applications seems to crash the app on M1 Macs. Now the icon swintch from java default icon to our tool icon is disabled.

--- axsutils/src/main/java/de/imise/util/OperatingSystem.java
+++ axsutils/src/main/java/de/imise/util/OperatingSystem.java
@@ -12,4 +12,20 @@ public class OperatingSystem {
         return System.getProperty("os.name", "").toUpperCase().startsWith("MAC");
     }
 
+    /**
+     * @return
+     */
+    public static boolean isARMArchtecture() {
+        String property = System.getProperty("os.arch", "").toUpperCase();
+        return property.contains("ARM");
+
+    }
+
+    /**
+     * @return <code>true</code> if the operating system is Mac OS.
+     */
+    public static boolean isWindowsOs() {
+        return System.getProperty("os.name", "").toUpperCase().contains("WINDOWS");
+    }
+
 }
