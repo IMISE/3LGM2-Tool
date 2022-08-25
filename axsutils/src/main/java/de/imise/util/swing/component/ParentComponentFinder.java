1c0027396fd43203d893c0b89015a6f3a85a4f18
Alexander Strübing <alexander.struebing@gmail.com>
2022-08-25 18:39:41 +0200

    Add Util to Get Swing Parent by Name

--- axsutils/src/main/java/de/imise/util/swing/component/ParentComponentFinder.java
+++ axsutils/src/main/java/de/imise/util/swing/component/ParentComponentFinder.java
@@ -1,5 +1,7 @@
 package de.imise.util.swing.component;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 import java.awt.Component;
 
 import javax.swing.JDialog;
@@ -18,12 +20,29 @@ public class ParentComponentFinder {
      * @param parentType
      * @return
      */
-    @SuppressWarnings("unchecked") //it's checked!
     public static final <T> T getParent(final Component component, final Class<T> parentType) {
+        return getParent(component, parentType, null);
+    }
+
+    /**
+     * Returns the parent with the given type
+     *
+     * @param <T>
+     * @param component
+     * @param parentType
+     * @param parentSimpleClassNamePattern
+     * @return
+     */
+    @SuppressWarnings("unchecked") //it's checked!
+    public static final <T> T getParent(final Component component, final Class<T> parentType, String parentSimpleClassNamePattern) {
         Component parent = component.getParent();
         while (parent != null) {
             Class<? extends Component> parentClass = parent.getClass();
-            if (parentType.isAssignableFrom(parentClass)) {
+            if (parentType != null && parentType.isAssignableFrom(parentClass)) {
+                if (isNullOrEmpty(parentSimpleClassNamePattern) || parentClass.getSimpleName().matches(parentSimpleClassNamePattern)) {
+                    return (T) parent;
+                }
+            } else if (parentType == null && (isNullOrEmpty(parentSimpleClassNamePattern) || parentClass.getSimpleName().matches(parentSimpleClassNamePattern))) {
                 return (T) parent;
             }
             parent = parent.getParent();
@@ -39,6 +58,17 @@ public class ParentComponentFinder {
      */
     public static final <T> boolean hasParent(final Component component, final Class<T> parentType) {
         T parent = getParent(component, parentType);
+        return parent != null;
+    }
+
+    /**
+     * @param <T>
+     * @param component
+     * @param parentSimpleClassNamePattern
+     * @return
+     */
+    public static final <T> boolean hasParentWithName(final Component component, final String parentSimpleClassNamePattern) {
+        T parent = getParent(component, null, parentSimpleClassNamePattern);
         return parent != null;
     }
 
