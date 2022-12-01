3d50716626663a5dd93d372ed3a941d25a383094
Alexander Strübing <alexander.struebing@gmail.com>
2022-12-01 20:41:47 +0100

    Change Group Dialog Tab Name for Parts

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/Group.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/metamodel/elements/Group.java
@@ -1,5 +1,7 @@
 package de.imise.tool3lgm.graphtools.metamodel.elements;
 
+import static de.imise.tool3lgm.graphtools.dialog.element.panel.PanelLabelOption.LABEL_LAST_EDGE_CONNECTION_NAME;
+
 import de.imise.tool3lgm.graphtools.dialog.element.ElementPropertyDialog;
 
 public class Group extends Node {
@@ -7,6 +9,7 @@ public class Group extends Node {
     @Override
     public ElementPropertyDialog createPropertyDialog() {
         ElementPropertyDialog dialog = super.createPropertyDialog();
+        dialog.addEdgePanel(LABEL_LAST_EDGE_CONNECTION_NAME, LABEL_LAST_EDGE_CONNECTION_NAME, Group_HasPartEdge.class);
         return dialog;
     }
 
