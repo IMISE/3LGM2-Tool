a0aec1e794fed87704124931fe3a6c4b6fa3dce1
Alexander Strübing <alexander.struebing@gmail.com>
2023-01-05 13:01:22 +0100

    Add ModelBrowser Tootips for (Sub)Models and Folder

--- de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/view/tooltip/LGMToolTipProvider.java
+++ de.imise.tool3lgm/src/main/java/de/imise/tool3lgm/graphtools/view/tooltip/LGMToolTipProvider.java
@@ -1,5 +1,7 @@
 package de.imise.tool3lgm.graphtools.view.tooltip;
 
+import java.io.File;
+
 import javax.swing.JComponent;
 
 import com.google.common.base.Strings;
@@ -8,6 +10,8 @@ import de.imise.tool3lgm.graphtools.ElementsNameBuilder;
 import de.imise.tool3lgm.graphtools.consistency.error.type.AbstractConsistencyError;
 import de.imise.tool3lgm.graphtools.metamodel.MetaModel;
 import de.imise.tool3lgm.graphtools.metamodel.elements.ModelElement;
+import de.imise.tool3lgm.graphtools.model.GDCollection;
+import de.imise.tool3lgm.graphtools.model.GraphDocument;
 import de.imise.tool3lgm.graphtools.view.container.ElementContainer;
 import de.imise.tool3lgm.graphtools.view.tree.node.LGMTreeNode;
 import de.imise.util.NamedObjectContainer;
@@ -54,29 +58,64 @@ public class LGMToolTipProvider implements ToolTipProvider {
             NamedObjectContainer<?> noc = (NamedObjectContainer<?>) o;
             o = noc.getObject();
         }
-        if (o instanceof ElementContainer) {
+        StringBuilder sb = new StringBuilder();
+        int initialLength = sb.length();
+
+        if (o instanceof ElementContainer) { // Elements
             ElementContainer ec = (ElementContainer) o;
             ModelElement me = ec.getElement();
             MetaModel metaModel = ec.getMetaModel();
             ElementsNameBuilder elementsNameBuilder = metaModel.getElementsNameBuilder();
-            StringBuilder sb = new StringBuilder("<html><p width=\"500\">");
             Class<? extends ModelElement> elementClass = me.getClass();
-            String displayableClassName = elementsNameBuilder.getDisplayableName(elementClass);
-            sb.append("<b>");
-            sb.append(displayableClassName);
-            sb.append("</b>");
+            appendBold(sb, elementsNameBuilder.getDisplayableName(elementClass));
             sb.append("<br><br>");
             sb.append(me.getClearName());
-            String description = me.getDescription();
-            if (!Strings.isNullOrEmpty(description)) {
-                sb.append("<br><br>");
-                description = HTMLConverter.getDecimalEncodedHTMLString(description, true);
-                sb.append(description);
+            appendDescription(sb, me.getDescription());
+        } else if (o instanceof GDCollection) { // Model = GDCollection
+            GDCollection gdcoll = (GDCollection) o;
+            String description = gdcoll.getDescription();
+            if (Strings.isNullOrEmpty(description)) {
+                description = gdcoll.getMainDoc().getDescription();
             }
-            sb.append("</p></html>");
-            return sb.toString();
+            appendBold(sb, o);
+            appendDescription(sb, description);
+        } else if (o instanceof GraphDocument) { // Submodel = GraphDocument
+            appendBold(sb, o);
+            appendDescription(sb, ((GraphDocument) o).getDescription());
+        } else if (o instanceof File) {
+            sb.append(o);
         }
-        return null;
+        if (sb.length() == initialLength) {
+            return null;
+        }
+
+        // limit width to 500px if tooltip if it contains more than 80 chars
+        sb.insert(0, sb.length() > 80 ? "<html><p width=\"500\">" : "<html><p>");
+
+        sb.append("</p></html>");
+        return sb.toString();
+    }
+
+    /**
+     * @param sb
+     * @param description
+     */
+    private static void appendDescription(StringBuilder sb, String description) {
+        if (!Strings.isNullOrEmpty(description)) {
+            sb.append("<br><br>");
+            description = HTMLConverter.getDecimalEncodedHTMLString(description, true);
+            sb.append(description);
+        }
+    }
+
+    /**
+     * @param sb
+     * @param line
+     */
+    private static void appendBold(StringBuilder sb, Object line) {
+        sb.append("<b>");
+        sb.append(line);
+        sb.append("</b>");
     }
 
 }
